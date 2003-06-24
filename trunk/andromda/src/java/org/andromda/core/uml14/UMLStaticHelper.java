6f13894918db41dd55a781f0b8a70394aa18cf7c
amowers <amowers@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2003-06-24 16:56:58 +0000

    bufix 759649, added support for primitive types in getFullyQualifiedName. now the method will return a full package name only if the type is not a primitive type.

--- trunk/andromda/src/java/org/andromda/core/uml14/UMLStaticHelper.java
+++ trunk/andromda/src/java/org/andromda/core/uml14/UMLStaticHelper.java
@@ -23,52 +23,52 @@ import org.omg.uml.foundation.core.TaggedValue;
  *
  *@author    Anthony Mowers
  */
-public class UMLStaticHelper extends UMLDefaultHelper implements ScriptHelper
-{
-	
+public class UMLStaticHelper extends UMLDefaultHelper implements ScriptHelper {
+    
+    /** 
+    * Returns the name of a model element fully qualified by the 
+    * name of the package that contains it. If the model element 
+    * is a primitive type it will return the primitive type itself. 
+    * 
+    *@param object model element 
+    *@return fully qualifed name 
+    */
+    public String getFullyQualifiedName(Object object) {
+        if ((object == null) || !(object instanceof ModelElement)) {
+            return null;
+        }
 
-	/**
-	 * Returns the name of a model element fully qualified by the
-     * name of the package that contains it.
-	 *
-	 *@param  object  model element
-	 *@return fully qualifed name  
-	 */
-	public String getFullyQualifiedName(Object object)
-	{
-		if ((object == null) || !(object instanceof ModelElement))
-		{
-			return null;
-		}
+        ModelElement modelElement = (ModelElement) object;
 
-		ModelElement modelElement = (ModelElement) object;
+        String fullName = modelElement.getName();
 
-		String fullName = modelElement.getName();
-		String packageName = getPackageName(modelElement);
-		fullName =
-			"".equals(packageName) ? fullName : packageName + "." + fullName;
+        if (StringUtilsHelper.isPrimitiveType(fullName)) {
+            return fullName;
+        }
 
-		return fullName;
-	}
+        String packageName = getPackageName(modelElement);
+        fullName =
+            "".equals(packageName) ? fullName : packageName + "." + fullName;
 
-	/**
-	 * Returns the collection of taggedValues for a given modelElement
+        return fullName;
+    }
+
+    /**
+     * Returns the collection of taggedValues for a given modelElement
      * 
-	 * @param object model element
-	 * @return Collection of org.omg.uml.foundation.core.TaggedValue
+     * @param object model element
+     * @return Collection of org.omg.uml.foundation.core.TaggedValue
      * 
-	 */
-	public Collection getTaggedValues(Object object)
-	{
-		if ((object == null) || !(object instanceof ModelElement))
-		{
-			return Collections.EMPTY_LIST;
-		}
+     */
+    public Collection getTaggedValues(Object object) {
+        if ((object == null) || !(object instanceof ModelElement)) {
+            return Collections.EMPTY_LIST;
+        }
 
-		ModelElement modelElement = (ModelElement) object;
+        ModelElement modelElement = (ModelElement) object;
 
-		return modelElement.getTaggedValue();
-	}
+        return modelElement.getTaggedValue();
+    }
 
     /**
      * Searches a collection of tag values for one with a particular
@@ -79,27 +79,23 @@ public class UMLStaticHelper extends UMLDefaultHelper implements ScriptHelper
      * 
      * @return value of tag, null if tag not found
      */
-    public String findTagValue(Collection taggedValues, String tagName)
-    {    
-        for (Iterator i = taggedValues.iterator(); i.hasNext(); )
-        {
-            TaggedValue taggedValue = (TaggedValue)i.next();
+    public String findTagValue(Collection taggedValues, String tagName) {
+        for (Iterator i = taggedValues.iterator(); i.hasNext();) {
+            TaggedValue taggedValue = (TaggedValue) i.next();
             String tgvName = getName(taggedValue);
-            
-            if (tagName.equals(tgvName))
-            {
+
+            if (tagName.equals(tgvName)) {
                 Iterator it = taggedValue.getDataValue().iterator();
-                if (it.hasNext())
-                {
+                if (it.hasNext()) {
                     return it.next().toString();
                 }
                 return null;
             }
         }
-        
+
         return null;
     }
-    
+
     /**
      * Searches for and returns the value of a given tag on
      * the specified model element.
@@ -109,173 +105,151 @@ public class UMLStaticHelper extends UMLDefaultHelper implements ScriptHelper
      * @return String value of tag, <b>null</b> if tag not found
      * 
      */
-    public String findTagValue(
-        ModelElement modelElement, String tagName)
-    {
+    public String findTagValue(ModelElement modelElement, String tagName) {
         return findTagValue(getTaggedValues(modelElement), tagName);
     }
-    
-	/**
-	 * Returns Association information from the perspective of
+
+    /**
+     * Returns Association information from the perspective of
      * a particular end of the association.
      * 
      * <p>The returned object can answers information about
      * whether the assocation is one2many, many2one, ...
      * </p>
      * 
-	 * @param object assocation end
-	 * @return DirectionalAssociationEnd directional association data
+     * @param object assocation end
+     * @return DirectionalAssociationEnd directional association data
      * 
-	 */
-	public DirectionalAssociationEnd getAssociationData(Object object)
-	{
-		if ((object == null) || !(object instanceof AssociationEnd))
-		{
-			return null;
-		}
-		
-		AssociationEnd ae = (AssociationEnd)object;
-		
-		return new DirectionalAssociationEnd(ae);
-	}
+     */
+    public DirectionalAssociationEnd getAssociationData(Object object) {
+        if ((object == null) || !(object instanceof AssociationEnd)) {
+            return null;
+        }
 
+        AssociationEnd ae = (AssociationEnd) object;
 
+        return new DirectionalAssociationEnd(ae);
+    }
 
-		
-	/**
-	 * Searches the given class feature (operation or attribute) for
+    /**
+     * Searches the given class feature (operation or attribute) for
      * the specified tag.
      * 
      * <p>If the follow boolean is set to true then the search will
      * continue from the class feature to the class itself and then
      * up the class hiearchy.</p>
      * 
-	 * @param feature attribute or operation object
-	 * @param tagName name of the tag to search for
-	 * @param follow <b>true</b> if search should follow inheritance
+     * @param feature attribute or operation object
+     * @param tagName name of the tag to search for
+     * @param follow <b>true</b> if search should follow inheritance
      * hierarchy
-	 * @return String value of tag, <b>null</b> if tag not found
+     * @return String value of tag, <b>null</b> if tag not found
      * 
-	 */
-	public String findTagValue(
-		StructuralFeature feature, String tagName, boolean follow)
-	{
-        if (feature == null) return null;
-        
-		String value = findTagValue(feature,tagName);
-		ModelElement element = feature.getType();
-		while ( ( value == null ) && (element != null) ) 
-		{
-			value = findTagValue(element,tagName);
-			element = getGeneralization(element);
-		}
-			
-		
-		return value;
-	}
- 
-	/**
-	 * Returns the collection of dependencies for a given model element.
+     */
+    public String findTagValue(
+        StructuralFeature feature,
+        String tagName,
+        boolean follow) {
+        if (feature == null)
+            return null;
+
+        String value = findTagValue(feature, tagName);
+        ModelElement element = feature.getType();
+        while ((value == null) && (element != null)) {
+            value = findTagValue(element, tagName);
+            element = getGeneralization(element);
+        }
+
+        return value;
+    }
+
+    /**
+     * Returns the collection of dependencies for a given model element.
      * 
-	 * <p>Abstraction/Interface implements dependencies will not be 
+     * <p>Abstraction/Interface implements dependencies will not be 
      * included in this collection.</b>
      * 
-	 *@param  object  model element
-	 *@return Collection of org.omg.uml.foundation.core.Dependency
-	 */
-	public Collection getDependencies(Object object)
-	{
-		if ((object == null) || !(object instanceof ModelElement))
-		{
-			return Collections.EMPTY_LIST;
-		}
+     *@param  object  model element
+     *@return Collection of org.omg.uml.foundation.core.Dependency
+     */
+    public Collection getDependencies(Object object) {
+        if ((object == null) || !(object instanceof ModelElement)) {
+            return Collections.EMPTY_LIST;
+        }
 
-		ModelElement modelElement = (ModelElement) object;
+        ModelElement modelElement = (ModelElement) object;
 
-		Collection clientDependencies =
-			model.getCore().getAClientClientDependency().getClientDependency(
-				modelElement);
+        Collection clientDependencies =
+            model.getCore().getAClientClientDependency().getClientDependency(
+                modelElement);
 
-		return new FilteredCollection(clientDependencies)
-		{
-			protected boolean accept(Object object)
-			{
-				return 
-					(object instanceof Dependency) && !(object instanceof Abstraction);
-			}
-		};
-	}
+        return new FilteredCollection(clientDependencies) {
+            protected boolean accept(Object object) {
+                return (object instanceof Dependency)
+                    && !(object instanceof Abstraction);
+            }
+        };
+    }
 
-	/**
-	 *  Gets the attributes of the specified Classifier object.
-	 *
-	 *@param  object  Classifier object
-	 *@return  Collection of org.omg.uml.foundation.core.Attribute
-	 */
-	public Collection getAttributes(Object object)
-	{
-		if ((object == null) || !(object instanceof Classifier))
-		{
-			return Collections.EMPTY_LIST;
-		}
+    /**
+     *  Gets the attributes of the specified Classifier object.
+     *
+     *@param  object  Classifier object
+     *@return  Collection of org.omg.uml.foundation.core.Attribute
+     */
+    public Collection getAttributes(Object object) {
+        if ((object == null) || !(object instanceof Classifier)) {
+            return Collections.EMPTY_LIST;
+        }
 
-		Classifier classifier = (Classifier) object;
-		Collection features = new FilteredCollection(classifier.getFeature())
-		{
-			protected boolean accept(Object object)
-			{
-				return object instanceof Attribute;
-			}
-		};
+        Classifier classifier = (Classifier) object;
+        Collection features = new FilteredCollection(classifier.getFeature()) {
+            protected boolean accept(Object object) {
+                return object instanceof Attribute;
+            }
+        };
 
-		return features;
-	}
+        return features;
+    }
 
-	/**
-	 *  Gets the operations of the specified Classifier object.
-	 *
-	 *@param  object  Classifier object
-	 *@return  Collection of org.omg.uml.foundation.core.Operation
-	 */
-	public Collection getOperations(Object object)
-	{
-		if ((object == null) || !(object instanceof Classifier))
-		{
-			return Collections.EMPTY_LIST;
-		}
+    /**
+     *  Gets the operations of the specified Classifier object.
+     *
+     *@param  object  Classifier object
+     *@return  Collection of org.omg.uml.foundation.core.Operation
+     */
+    public Collection getOperations(Object object) {
+        if ((object == null) || !(object instanceof Classifier)) {
+            return Collections.EMPTY_LIST;
+        }
 
-		Classifier classifier = (Classifier) object;
-		Collection features = new FilteredCollection(classifier.getFeature())
-		{
-			protected boolean accept(Object object)
-			{
-				return object instanceof Operation;
-			}
-		};
+        Classifier classifier = (Classifier) object;
+        Collection features = new FilteredCollection(classifier.getFeature()) {
+            protected boolean accept(Object object) {
+                return object instanceof Operation;
+            }
+        };
 
-		return features;
-	}
+        return features;
+    }
 
-	/**
-	 * Gets the assocation ends that are attached to the specified
+    /**
+     * Gets the assocation ends that are attached to the specified
      * Classifier object.
-	 *
-	 *@param  object  Classifier object
-	 *@return  Collection of org.omg.uml.foundation.core.AssociationEnd
-	 */
-	public Collection getAssociationEnds(Object object)
-	{
-		if ((object == null) || !(object instanceof Classifier))
-		{
-			return Collections.EMPTY_LIST;
-		}
+     *
+     *@param  object  Classifier object
+     *@return  Collection of org.omg.uml.foundation.core.AssociationEnd
+     */
+    public Collection getAssociationEnds(Object object) {
+        if ((object == null) || !(object instanceof Classifier)) {
+            return Collections.EMPTY_LIST;
+        }
 
-		Classifier classifier = (Classifier) object;
-		return model.getCore().getAParticipantAssociation().getAssociation(
-			classifier);
-	}
+        Classifier classifier = (Classifier) object;
+        return model.getCore().getAParticipantAssociation().getAssociation(
+            classifier);
+    }
 
-    
     /**
      * Returns the generalization/superclass for the given model generalizable
      * model element (i.e. Class).
@@ -303,77 +277,66 @@ public class UMLStaticHelper extends UMLDefaultHelper implements ScriptHelper
         return null;
     }
 
-	
-	
-	/**
-	 * Returns the collection of interfaces implemented by the given
+    /**
+     * Returns the collection of interfaces implemented by the given
      * Classifier object.
      * 
-	 * @param object  Class
-	 * @return Collection of Interfaces
-	 */
-	public Collection getAbstractions(Object object)
-	{
-		if ((object == null) || !(object instanceof Classifier))
-		{
-			return Collections.EMPTY_LIST;
-		}
+     * @param object  Class
+     * @return Collection of Interfaces
+     */
+    public Collection getAbstractions(Object object) {
+        if ((object == null) || !(object instanceof Classifier)) {
+            return Collections.EMPTY_LIST;
+        }
 
-		ModelElement modelElement = (ModelElement) object;
+        ModelElement modelElement = (ModelElement) object;
 
-		Collection clientDependencies =
-			model.getCore().getAClientClientDependency().getClientDependency(
-				modelElement);
+        Collection clientDependencies =
+            model.getCore().getAClientClientDependency().getClientDependency(
+                modelElement);
 
-		return new FilteredCollection(clientDependencies)
-		{
-			public boolean add(Object object)
-			{
-				Abstraction abstraction = (Abstraction) object;
-				return super.add(abstraction.getSupplier().iterator().next());
-			}
+        return new FilteredCollection(clientDependencies) {
+            public boolean add(Object object) {
+                Abstraction abstraction = (Abstraction) object;
+                return super.add(abstraction.getSupplier().iterator().next());
+            }
 
-			protected boolean accept(Object object)
-			{
-				return object instanceof Abstraction;
-			}
-		};
-	}
+            protected boolean accept(Object object) {
+                return object instanceof Abstraction;
+            }
+        };
+    }
 
-	/**
-	 *  Filters a collection of objects so that the collection
+    /**
+     *  Filters a collection of objects so that the collection
      * contains only those objects that pass the 'accept' test.
      * 
      * <p>It is useful for filtering the results of a query.</p>
-	 *
-	 *@author    Anthony Mowers
-	 */
-	private abstract static class FilteredCollection extends Vector
-	{
-		/**
-		 *  Constructor for the FilterCollection object
-		 *
-		 *@param  c  Description of the Parameter
-		 */
-		public FilteredCollection(Collection c)
-		{
-			for (Iterator i = c.iterator(); i.hasNext();)
-			{
-				Object object = i.next();
-				if (accept(object))
-				{
-					add(object);
-				}
-			}
-		}
+     *
+     *@author    Anthony Mowers
+     */
+    private abstract static class FilteredCollection extends Vector {
+        /**
+         *  Constructor for the FilterCollection object
+         *
+         *@param  c  Description of the Parameter
+         */
+        public FilteredCollection(Collection c) {
+            for (Iterator i = c.iterator(); i.hasNext();) {
+                Object object = i.next();
+                if (accept(object)) {
+                    add(object);
+                }
+            }
+        }
 
-		/**
-		 *  Description of the Method
-		 *
-		 *@param  object  Description of the Parameter
-		 *@return         Description of the Return Value
-		 */
-		protected abstract boolean accept(Object object);
-	}
+        /**
+         *  Description of the Method
+         *
+         *@param  object  Description of the Parameter
+         *@return         Description of the Return Value
+         */
+        protected abstract boolean accept(Object object);
+    }
 
 }
