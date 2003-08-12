4256e7759ff266cc3f67de00286f316baa9b2c19
mattes3 <mattes3@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2003-08-12 13:32:16 +0000

    Macro libraries can now be specified in a cartridge descriptor.

--- trunk/andromda/src/java/org/andromda/cartridges/interfaces/ICartridgeDescriptor.java
+++ trunk/andromda/src/java/org/andromda/cartridges/interfaces/ICartridgeDescriptor.java
@@ -44,10 +44,19 @@ public interface ICartridgeDescriptor {
     /**
      * Returns the list of templates configured in this cartridge.
      * 
-     * @return List the template lists
+     * @return List the template list
+     * @see TemplateConfiguration
      */
     public List getTemplateConfigurations();
-    
+
+    /**
+     * Returns the list of macro libraries with commonly used
+     * scripting engine macros.
+     * 
+     * @return List the list of macros
+     */
+    public List getMacroLibraries();
+        
     /**
      * Gets the URL where this descriptor data came from.
      * 
