f5b1ab3e8d994b88148237b619e8f8a902af2320
mattes3 <mattes3@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2003-10-26 12:26:58 +0000

    Deleted some unused imports.

--- andromda/src/java/org/andromda/cartridges/interfaces/DefaultAndroMDACartridge.java
+++ andromda/src/java/org/andromda/cartridges/interfaces/DefaultAndroMDACartridge.java
@@ -22,7 +22,6 @@ import org.apache.commons.collections.ExtendedProperties;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 /**
