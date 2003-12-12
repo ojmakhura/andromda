5bdce2ba92becf73555b6d259f752a9d330ad620
mattes3 <mattes3@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2003-12-12 14:30:29 +0000

    Now methods are treated uniformly, regardless of whether they come from decorated interfaces, from UML operations or from UML associations.

--- cartridges/andromda-meta/src/java/org/andromda/cartridges/meta/MethodDescriptor.java
+++ cartridges/andromda-meta/src/java/org/andromda/cartridges/meta/MethodDescriptor.java
@@ -1,23 +1,19 @@
 package org.andromda.cartridges.meta;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 
 /**
- * Represents a method in Java.
+ * Represents a method in a programming language.
  */
 public class MethodDescriptor implements Comparable
 {
-    private Method method;
-	private String interfaceName;
+    private MethodData method;
 	
     /**
      * Creates a method descriptor from the properties of a method. 
      */
-    public MethodDescriptor(Method m, String interfaceName)
+    public MethodDescriptor(MethodData m)
     {
         this.method = m;
-        this.interfaceName = interfaceName;
     }
 
 	/**
@@ -25,7 +21,7 @@ public class MethodDescriptor implements Comparable
 	 */
 	public String getDeclaration(boolean suppressAbstractDeclaration)
 	{
-		return buildMethodDeclaration(method, suppressAbstractDeclaration);
+		return method.buildMethodDeclaration(suppressAbstractDeclaration);
 	}
 
 	/**
@@ -34,8 +30,9 @@ public class MethodDescriptor implements Comparable
 	 */
 	public boolean hasReturnType()
 	{
-		return (method.getReturnType() != null)
-			&& !("void".equals(typename(method.getReturnType())));
+		String returnTypeName = method.getReturnTypeName();
+        return (returnTypeName != null)
+			&& !("void".equals(returnTypeName));
 	}
 
 	/**
@@ -44,103 +41,31 @@ public class MethodDescriptor implements Comparable
 	 */
 	public String getMethodCall()
 	{
-		return buildMethodCall(method);
+		return method.buildMethodCall();
 	}
 
+    /**
+     * Returns a signature which can be used as a key into a map.
+     * Consists of the return type, the name and the f.q. types
+     * of the arguments.
+     * 
+     * @return String the key that identifies this method
+     */
+    public String getCharacteristicKey()
+    {
+        return method.buildCharacteristicKey();
+    }
+    
 	/**
 	 * Returns the name of the interface in which the method was declared.
      * @return name
      */
     public String getParentInterfaceName()
 	{
-		return interfaceName;
+		return method.getInterfaceName();
 	}
 	
 	
-    /**
-     * Builds a string representing a call to the method.
-     * @param m the method
-     * @return String how a call would look like
-     */
-    private String buildMethodCall(Method m)
-    {
-        Class returntype = m.getReturnType();
-        Class parameters[] = m.getParameterTypes();
-        Class exceptions[] = m.getExceptionTypes();
-
-        String call = m.getName() + " (";
-
-        for (int i = 0; i < parameters.length; i++)
-        {
-            if (i > 0)
-                call += ", ";
-            call += "p" + i;
-        }
-        call += ")";
-        return call;
-    }
-
-    /**
-     * Builds a string representing a declaration for this method.
-     * @param m the method
-     * @return String the declaration
-     */
-    private String buildMethodDeclaration(Method m, boolean suppressAbstractDeclaration)
-    {
-        Class returntype = m.getReturnType();
-        Class parameters[] = m.getParameterTypes();
-        Class exceptions[] = m.getExceptionTypes();
-
-        String declaration =
-            modifiers(m.getModifiers() & ~(suppressAbstractDeclaration ? Modifier.ABSTRACT : 0))
-                + ((returntype != null) ? typename(returntype) + " " : "")
-                + m.getName()
-                + "(";
-
-        for (int i = 0; i < parameters.length; i++)
-        {
-            if (i > 0)
-                declaration += ", ";
-            declaration += typename(parameters[i]);
-            declaration += " p" + i;
-        }
-        declaration += ")";
-
-        if (exceptions.length > 0)
-        {
-            declaration += " throws ";
-            for (int i = 0; i < exceptions.length; i++)
-            {
-                if (i > 0)
-                    declaration += ", ";
-                declaration += typename(exceptions[i]);
-            }
-        }
-
-        return declaration;
-    }
-
-    /** Return the name of an interface or primitive type, handling arrays. */
-    private static String typename(Class t)
-    {
-        String brackets = "";
-        while (t.isArray())
-        {
-            brackets += "[]";
-            t = t.getComponentType();
-        }
-        return t.getName() + brackets;
-    }
-
-    /** Return a string version of modifiers, handling spaces nicely. */
-    private static String modifiers(int m)
-    {
-        if (m == 0)
-            return "";
-        else
-            return Modifier.toString(m) + " ";
-    }
-
     /* (non-Javadoc)
      * @see java.lang.Comparable#compareTo(java.lang.Object)
      */
