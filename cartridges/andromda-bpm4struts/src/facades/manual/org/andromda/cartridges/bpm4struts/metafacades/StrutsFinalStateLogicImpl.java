0f886120bb7672ff4ceead31c783861111c614a2
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-03-29 02:00:50 +0000

    if a final state does not have a name, or a corresponding use-case is not found, it will resolve to the first use-case in the application (the one with the FrontEndApplication stereotype)

--- cartridges/andromda-bpm4struts/src/facades/manual/org/andromda/cartridges/bpm4struts/metafacades/StrutsFinalStateLogicImpl.java
+++ cartridges/andromda-bpm4struts/src/facades/manual/org/andromda/cartridges/bpm4struts/metafacades/StrutsFinalStateLogicImpl.java
@@ -2,6 +2,8 @@ package org.andromda.cartridges.bpm4struts.metafacades;
 
 import org.andromda.cartridges.bpm4struts.Bpm4StrutsProfile;
 import org.andromda.metafacades.uml.UseCaseFacade;
+import org.andromda.metafacades.uml.ModelElementFacade;
+import org.andromda.core.metafacade.MetafacadeBase;
 
 import java.util.Collection;
 import java.util.Iterator;
@@ -17,6 +19,7 @@ public class StrutsFinalStateLogicImpl
         implements org.andromda.cartridges.bpm4struts.metafacades.StrutsFinalState
 {
     private String fullPath = null;
+    private Object targetUseCase = null;
 
     // ---------------- constructor -------------------------------
     
@@ -36,20 +39,33 @@ public class StrutsFinalStateLogicImpl
 
     protected Object handleGetTargetUseCase()
     {
+        if (Bpm4StrutsProfile.ENABLE_CACHE && targetUseCase != null) return targetUseCase;
+
+        Object useCaseObject = null;
         final String name = getName();
+
         if (name != null)
         {
             final Collection useCases = getModel().getAllUseCases();
-            for (Iterator iterator = useCases.iterator(); iterator.hasNext();)
+            for (Iterator iterator = useCases.iterator(); (useCaseObject==null && iterator.hasNext());)
             {
                 UseCaseFacade useCase = (UseCaseFacade) iterator.next();
                 if (useCase instanceof StrutsUseCase)
                 {
                     if (name.equalsIgnoreCase(useCase.getName()))
-                        return useCase;
+                        useCaseObject = useCase;
                 }
             }
         }
-        return null;
+
+        final Collection allUseCases = getModel().getAllUseCases();
+        for (Iterator iterator = allUseCases.iterator(); (useCaseObject==null && iterator.hasNext());)
+        {
+            ModelElementFacade facade = (ModelElementFacade) iterator.next();
+            if (facade.hasStereotype(Bpm4StrutsProfile.STEREOTYPE_APPLICATION))
+                useCaseObject = facade;
+        }
+
+        return targetUseCase = useCaseObject;
     }
 }
