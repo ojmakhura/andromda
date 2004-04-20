56f240c3762d2bcc8e5a178f7a8b7d4d08f3c216
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-04-20 22:36:30 +0000

    add an option to not cartridges logging when disovering cartridges (need this in the maven plugin)

--- trunk/andromda/src/java/org/andromda/core/cartridge/CartridgeFinder.java
+++ trunk/andromda/src/java/org/andromda/core/cartridge/CartridgeFinder.java
@@ -19,7 +19,7 @@ import org.apache.log4j.Logger;
  * @author    <a href="http://www.mbohlen.de">Matthias Bohlen</a>
  * @author    Chad Brandon
  * @since     April 1, 2003
- * @version   $Revision: 1.4 $
+ * @version   $Revision: 1.5 $
  */
 public class CartridgeFinder
 {
@@ -41,14 +41,28 @@ public class CartridgeFinder
 	    }
 	    return instance;
 	}
+    
+    /**
+     * Discovers and initializes all <code>AndroMDACartridge</code>
+     * objects on the current classpath.  If the cartridge
+     * with the given name is already registered in the 
+     * ComponentContainer, it will not be registered again.
+     */
+    public void discoverCartridges()
+    {
+       this.discoverCartridges(true); 
+    }
 
 	/**
 	 * Discovers and initializes all <code>AndroMDACartridge</code>
 	 * objects on the current classpath.  If the cartridge
 	 * with the given name is already registered in the 
 	 * ComponentContainer, it will not be registered again.
+     * 
+     * @param showCartridges if true then the cartridge found will
+     *        logged, otherwise nothing will be shown.
 	 */
-    public void discoverCartridges() 
+    public void discoverCartridges(boolean showCartridges) 
     {         
         URL cartridgeUris[]= ResourceFinder.findResources(resourceName);
 
@@ -63,7 +77,9 @@ public class CartridgeFinder
             String name = descriptor.getCartridgeName();
 			
 			if (!ComponentContainer.instance().isRegistered(name)) {
-				logger.info("found cartridge --> '" + name + "'");
+                if (showCartridges) {
+				    logger.info("found cartridge --> '" + name + "'");
+                }
 			    ComponentContainer.instance().registerComponent(name, cartridge);
             }
         } 
