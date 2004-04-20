23b817fcbcf6655617889d56cd756d72745d474d
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-04-20 00:06:04 +0000

    - Add exception generation to the java cartridge and add support for exception   references.  Modify the samples so to use the generated exceptions - Move initialization scripts into more appropriate "conf" directory (since they configure the database).

--- metafacades/uml/common/src/java/org/andromda/metafacades/uml/UMLProfile.java
+++ metafacades/uml/common/src/java/org/andromda/metafacades/uml/UMLProfile.java
@@ -31,9 +31,33 @@ public class UMLProfile {
 	public static final String STEREOTYPE_SERVICE = "Service";
     
     /**
-     * Represents an exception.
+     * The base exception stereotype.  If a model element is 
+     * stereotyped with this (or one of its sub stereotypes), 
+     * then it is excepted that a cartridge will generate an exception
      */
     public static final String STEREOTYPE_EXCEPTION = "Exception";
+    
+    /**
+     * Represents exceptions thrown during normal application processing
+     * (such as business exceptions).  It extends the base exception stereotype.
+     */
+    public static final String STEREOTYPE_APPLICATION_EXCEPTION = "ApplicationException";
+    
+    /**
+     * Represents unexpected exceptions that can occur during application
+     * processing.  This that a caller isn't expected to handle.
+     */
+    public static final String STEREOTYPE_UNEXPECTED_EXCEPTION = "UnexpectedException";
+    
+    /**
+     * Represents a reference to an exception model element. 
+     * Model dependencies to unstereotyped exception model elements 
+     * can be stereotyped with this.  This allows the user to create 
+     * a custom exception class since the exception itself will not 
+     * be generated but the references to it will be (i.e. the throws
+     * clause within an operation).
+     */
+    public static final String STEREOTYPE_EXCEPTION_REF = "ExceptionRef";
 	
 	/* ----------------- Tagged Values -------------------- */
 	
