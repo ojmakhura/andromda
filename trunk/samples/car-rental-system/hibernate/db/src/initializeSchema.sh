6b67fe6351c6d29e864b040d19ea0de6cf641e81
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-04-22 00:31:00 +0000

    remove unneeded files (since they are in conf now)

--- trunk/samples/car-rental-system/hibernate/db/src/initializeSchema.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-java -classpath $JBOSS_HOME/server/default/lib/hsqldb.jar org.hsqldb.util.ScriptTool -driver org.hsqldb.jdbcDriver -url jdbc:hsqldb:hsql: -database //localhost:1701 -script ../../build/hibernate-schema-initialize.sql
