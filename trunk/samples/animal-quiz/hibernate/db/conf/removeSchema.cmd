a20e9aa7910714254e0745b1f8d9690de4aed81b
mattes3 <mattes3@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-04-28 21:33:39 +0000

    Fixed wrong path names in database init scripts.

--- trunk/samples/animal-quiz/hibernate/db/conf/removeSchema.cmd
+++ trunk/samples/animal-quiz/hibernate/db/conf/removeSchema.cmd
@@ -1,1 +1,9 @@
-java -classpath %JBOSS_HOME%\server\default\lib\hsqldb.jar org.hsqldb.util.ScriptTool -driver org.hsqldb.jdbcDriver -url jdbc:hsqldb:hsql: -database //localhost:1701 -script ../../sar/target/schemas/hibernate-schema-remove.sql
+rem
+rem This script does something to the HSQL database which is part of the
+rem JBoss distribution. The script is _not_ meant to run from the source tree
+rem but from the same directory as the *.sql files that the build script
+rem has generated.
+rem
+rem Matthias Bohlen
+rem
+java -classpath %JBOSS_HOME%\server\default\lib\hsqldb.jar org.hsqldb.util.ScriptTool -driver org.hsqldb.jdbcDriver -url jdbc:hsqldb:hsql: -database //localhost:1701 -script hibernate-schema-remove.sql
