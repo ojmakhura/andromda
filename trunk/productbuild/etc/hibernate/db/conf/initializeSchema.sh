d88e27b426aafa261ac7b59d549b6633c484f8e8
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-05-03 21:51:41 +0000

    check in new versions from Matthias's fixes

--- trunk/productbuild/etc/hibernate/db/conf/initializeSchema.sh
+++ trunk/productbuild/etc/hibernate/db/conf/initializeSchema.sh
@@ -1,3 +1,10 @@
 #! /bin/sh
-
-java -classpath $JBOSS_HOME/server/default/lib/hsqldb.jar org.hsqldb.util.ScriptTool -driver org.hsqldb.jdbcDriver -url jdbc:hsqldb:hsql: -database //localhost:1701 -script ../../sar/target/schemas/hibernate-schema-initialize.sql
+#
+# This script does something to the HSQL database which is part of the
+# JBoss distribution. The script is _not_ meant to run from the source tree
+# but from the same directory as the *.sql files that the build script
+# has generated.
+#
+# Matthias Bohlen
+#
+java -classpath $JBOSS_HOME/server/default/lib/hsqldb.jar org.hsqldb.util.ScriptTool -driver org.hsqldb.jdbcDriver -url jdbc:hsqldb:hsql: -database //localhost:1701 -script hibernate-schema-initialize.sql
