1f7dab454f2080add502ac096483434008102dba
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-05-03 20:53:59 +0000

    bad boy

--- productbuild/etc/hibernate/db-conf/removeSchema.cmd
+++
@@ -1,1 +0,0 @@
-java -classpath %JBOSS_HOME%\server\default\lib\hsqldb.jar org.hsqldb.util.ScriptTool -driver org.hsqldb.jdbcDriver -url jdbc:hsqldb:hsql: -database //localhost:1701 -script ../../sar/target/schemas/hibernate-schema-remove.sql
