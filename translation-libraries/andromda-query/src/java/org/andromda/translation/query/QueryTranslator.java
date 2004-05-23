dc9bc74bc6e76b76eab7d15017d65ad07ee656ac
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-05-23 21:29:24 +0000

    updated to reflect changes in grammar (OCL collection operations)

--- translation-libraries/andromda-query/src/java/org/andromda/translation/query/QueryTranslator.java
+++ translation-libraries/andromda-query/src/java/org/andromda/translation/query/QueryTranslator.java
@@ -1,23 +1,17 @@
 package org.andromda.translation.query;
 
-import java.util.List;
-
 import org.andromda.core.translation.BaseTranslator;
 import org.andromda.core.translation.TranslationUtils;
-import org.andromda.core.translation.node.AFeatureCall;
-import org.andromda.core.translation.node.ALogicalExpressionTail;
-import org.andromda.core.translation.node.AOperationContextDeclaration;
-import org.andromda.core.translation.node.APropertyCallExpression;
-import org.andromda.core.translation.node.ARelationalExpressionTail;
-import org.andromda.core.translation.node.AStandardDeclarator;
-import org.andromda.core.translation.node.PRelationalExpression;
+import org.andromda.core.translation.node.*;
 import org.andromda.core.translation.syntax.VariableDeclaration;
 import org.andromda.core.translation.syntax.impl.ConcreteSyntaxUtils;
 import org.apache.commons.lang.StringUtils;
 
+import java.util.List;
+
 /**
- * Performs translation to the following:   
- * <ul> 
+ * Performs translation to the following:
+ * <ul>
  *     <li>
  *          Hibernate-QL
  *     </li>
@@ -45,14 +39,14 @@ public class QueryTranslator extends BaseTranslator {
 		this.selectClause = new StringBuffer();
         this.declaratorCtr = 0;
 	}
-    
+
     /**
      * Stores the name of the initial declarator.
      */
     private short declaratorCtr;
-    
+
     /**
-     * True/false whether or not its the initial declarator. 
+     * True/false whether or not its the initial declarator.
      * We care about this because we must know what the first one is
      * for differentiating between the first declarator (the beginning of the query)
      * and any other declarators (the connecting associations).
@@ -62,10 +56,10 @@ public class QueryTranslator extends BaseTranslator {
         this.declaratorCtr++;
         return isInitialDeclarator;
     }
-    
+
 	/**
 	 * Returns true if the name is one of the arguments.
-	 * 
+	 *
 	 * @return boolean true if its an argument
 	 */
 	protected boolean isArgument(String name) {
@@ -74,8 +68,8 @@ public class QueryTranslator extends BaseTranslator {
 			isArgument = this.argumentNames.contains(name);
 		}
 		return isArgument;
-    }	
-     
+    }
+
 	/**
 	 * Gets the information about the operation which is the context of this
 	 * expression.
@@ -91,7 +85,7 @@ public class QueryTranslator extends BaseTranslator {
 
 	/**
 	 * Helps out with 'includesAll', replaces the {1} in the expression fragment
-     * when a declarator is encountered (i.e. '| <variable name>') 
+     * when a declarator is encountered (i.e. '| <variable name>')
 	 */
 	public void inAStandardDeclarator(AStandardDeclarator declarator) {
 		String methodName = "inAStandardDeclarator";
@@ -102,19 +96,19 @@ public class QueryTranslator extends BaseTranslator {
 					+ " with declarator --> "
 					+ declarator);
 		}
-	
+
         String temp = this.selectClause.toString();
 
-        String declaratorName = 
+        String declaratorName =
         	ConcreteSyntaxUtils.getVariableDeclarations(declarator)[0].getName();
-       
+
         //by default we'll assume we're replacing the {1} arg.
         short replacement = 1;
         if (this.isInitialDeclarator()) {
         	//handle differently if its the initial declarator, replacement is {0}
             replacement = 0;
         }
-        
+
 		//now replace {1} reference
 		temp =
 			TranslationUtils.replacePattern(
@@ -123,14 +117,14 @@ public class QueryTranslator extends BaseTranslator {
 				declaratorName);
 		this.selectClause = new StringBuffer(temp);
 	}
-    
+
     /**
      * Override to handle any propertyCall expressions (
      * i.e. exists(<expression>), select(<expression>, etc.)
      */
     public void inAPropertyCallExpression(APropertyCallExpression expression) {
         //System.out.println("property call expression: " + expression);
-        this.handleTranslationFragment(expression);     
+        this.handleTranslationFragment(expression);
     }
 
 	/**
@@ -151,7 +145,7 @@ public class QueryTranslator extends BaseTranslator {
 	 * Checks to see if the replacement is an argument and if so replaces the
 	 * {index} in the fragment with the 'argument' fragment from the template.
 	 * Otherwise replaces the {index} with the passed in replacement value.
-	 * 
+	 *
 	 * @param fragment the fragment to perform replacement.
 	 * @param replacement the replacement string
 	 * @param index the index in the string to replace.
@@ -187,7 +181,7 @@ public class QueryTranslator extends BaseTranslator {
 		String existingExpression =
 			StringUtils.trimToEmpty(
 				this.getExpression().getTranslatedExpression());
-        
+
 		if (StringUtils.isNotEmpty(selectClauseTail)
 			&& StringUtils.isNotEmpty(existingExpression)) {
 			this.selectClause.append(" ");
@@ -199,14 +193,14 @@ public class QueryTranslator extends BaseTranslator {
 			0,
 			selectClause.toString());
 	}
-   
-    
+
+
     /*------------------------- Handler methods ---------------------------------------*/
 
     /*------------------------- PropertyCallExpression Handler methods ---------------------*/
-    
+
     public void handleSubSelect(String translation, Object node) {
-        
+
         APropertyCallExpression propertyCallExpression = (APropertyCallExpression)node;
 
         String primaryExpression =
@@ -215,73 +209,73 @@ public class QueryTranslator extends BaseTranslator {
         //set the association which the 'includesAll' indicates (which
         //is the first feature call from the list of feature calls)
         translation = this.replaceFragment(
-            translation, 
+            translation,
             TranslationUtils.trimToEmpty(primaryExpression),
             0);
 
         this.selectClause.append(" ");
-        this.selectClause.append(translation);    	
+        this.selectClause.append(translation);
     }
-    
+
     public void handleIsLike(String translation, Object node) {
-        
+
         APropertyCallExpression propertyCallExpression = (APropertyCallExpression)node;
-        List featureCalls = ConcreteSyntaxUtils.getFeatureCalls(propertyCallExpression);        
-        AFeatureCall featureCall = (AFeatureCall)featureCalls.get(0);
-        
-        List params =
-            ConcreteSyntaxUtils.getParameters(featureCall);
+        List featureCalls = ConcreteSyntaxUtils.getFeatureCalls(propertyCallExpression);
+
+        System.out.println("featureCalls = " + featureCalls);
+
+        List params = params = ConcreteSyntaxUtils.getParameters((ADotFeatureCall)featureCalls.get(0));
 
         translation = this.replaceFragment(
-                translation, 
-                TranslationUtils.deleteWhitespace(params.get(0)), 
+                translation,
+                TranslationUtils.deleteWhitespace(params.get(0)),
                 0);
         translation = this.replaceFragment(
-                translation, 
-                TranslationUtils.deleteWhitespace(params.get(1)), 
+                translation,
+                TranslationUtils.deleteWhitespace(params.get(1)),
                 1);
 
         if (StringUtils.isNotEmpty(this.getExpression().getTranslatedExpression())) {
             this.getExpression().appendSpaceToTranslatedExpression();
         }
-        this.getExpression().appendToTranslatedExpression(translation);        
-        
+        this.getExpression().appendToTranslatedExpression(translation);
+
     }
-    
+
     public void handleSelect(String translation, Object node) {
         this.selectClause.append(translation);
     }
-    
+
     public void handleExists(String translation, Object node) {
 
         APropertyCallExpression propertyCallExpression = (APropertyCallExpression)node;
-        List featureCalls = ConcreteSyntaxUtils.getFeatureCalls(propertyCallExpression); 
-        AFeatureCall featureCall = (AFeatureCall)featureCalls.get(0);
+        List featureCalls = ConcreteSyntaxUtils.getFeatureCalls(propertyCallExpression);
+        ADotFeatureCall featureCall = (ADotFeatureCall)featureCalls.get(0);
 
         String primaryExpression =
             ConcreteSyntaxUtils.getPrimaryExpression(propertyCallExpression);
-        
+
         if (logger.isDebugEnabled()) {
             logger.debug("primaryExpression --> '" + primaryExpression + "'");
         }
 
-        String parameters = 
+        String parameters =
             StringUtils.deleteWhitespace(
-                ConcreteSyntaxUtils.getParametersAsString(featureCall));        
-        
+                ConcreteSyntaxUtils.getParametersAsString(featureCall));
+
         translation = this.replaceFragment(
-        	translation, 
-            primaryExpression, 
+        	translation,
+            primaryExpression,
             1);
         translation = this.replaceFragment(
-            translation, 
-            parameters, 
+            translation,
+            parameters,
             0);
 
         this.getExpression().appendSpaceToTranslatedExpression();
         this.getExpression().appendToTranslatedExpression(translation);
     }
-    
+
     public void handleIsEmpty(String translation, Object node) {
 
         APropertyCallExpression propertyCallExpression = (APropertyCallExpression)node;
@@ -291,26 +285,26 @@ public class QueryTranslator extends BaseTranslator {
         if (logger.isDebugEnabled()) {
             logger.debug("primaryExpression --> '" + primaryExpression + "'");
         }
-        
+
     	translation = this.replaceFragment(translation, primaryExpression, 0);
-        
+
         this.getExpression().appendSpaceToTranslatedExpression();
         this.getExpression().appendToTranslatedExpression(translation);
-        
+
     }
-    
+
     /*------------------------- Logical Expression Handler (and, or, xor, etc.) ----------------------*/
-  
+
     public void handleLogicalExpression(String translation, Object node) {
         this.getExpression().appendSpaceToTranslatedExpression();
         this.getExpression().appendToTranslatedExpression(translation);
     }
-    
+
     /*------------------------- Relational Expression Handler (=, <, >, >=, etc.) --------------------*/
-    
+
     public void handleRelationalExpression(String translation, Object node) {
-        
-        ARelationalExpressionTail relationalExpressionTail =  
+
+        ARelationalExpressionTail relationalExpressionTail =
             (ARelationalExpressionTail)node;
 
         String[] leftAndRightExpressions =
@@ -327,7 +321,7 @@ public class QueryTranslator extends BaseTranslator {
 
         this.getExpression().appendSpaceToTranslatedExpression();
         this.getExpression().appendToTranslatedExpression(translation);
-        
+
     }
-    
+
 }
