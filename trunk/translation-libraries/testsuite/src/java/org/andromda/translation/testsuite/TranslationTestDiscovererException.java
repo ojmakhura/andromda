07644e5122e1c7978698cab9cdd4faf18c1da2d9
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-05-27 00:50:51 +0000

    add better finding ability to the translation test suite

---
+++ trunk/translation-libraries/testsuite/src/java/org/andromda/translation/testsuite/TranslationTestDiscovererException.java
@@ -0,0 +1,38 @@
+package org.andromda.translation.testsuite;
+
+
+/**
+ * Any unchecked exception that will be thrown when an unexpected 
+ * error occurs during TranslationTestDiscoverer processing. 
+ */
+public class TranslationTestDiscovererException extends RuntimeException {
+
+    /**
+     * Constructs an instance of TranslationTestDiscovererException.
+     * 
+     * @param th
+     */
+	public TranslationTestDiscovererException(Throwable th){
+		super(th);
+	}
+	
+    /**
+     * Constructs an instance of TranslationTestDiscovererException.
+     * 
+     * @param msg
+     */
+	public TranslationTestDiscovererException(String msg) {
+		super(msg);	
+	} 
+	
+    /**
+     * Constructs an instance of TranslationTestDiscovererException.
+     * 
+     * @param msg
+     * @param th
+     */
+	public TranslationTestDiscovererException(String msg, Throwable th) {
+		super(msg, th);	
+	}   
+	   
+}
