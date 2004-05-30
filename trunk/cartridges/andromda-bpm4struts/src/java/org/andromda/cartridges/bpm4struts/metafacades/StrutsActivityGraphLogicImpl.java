9820457446d69c5ca5cc5d932c7f150dcb17be2c
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-05-30 23:30:11 +0000

    remove an unnecessary else clause

--- trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsActivityGraphLogicImpl.java
+++ trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsActivityGraphLogicImpl.java
@@ -57,28 +57,26 @@ public class StrutsActivityGraphLogicImpl
         {
             return contextElement;
         }
-        else
+        
+        /*
+         * for those tools not supporting setting the context of an activity graph (such as Poseidon)
+         * an alternative is implemented: a tagged value on the controller, specifying the name of the use-case
+         */
+        
+        final String useCaseName = getUseCase().getName();
+
+        // loop over the controllers, looking for the tagged value matching this activity graph's use-case name
+        Collection allClasses = getModel().getRootPackage().getClasses();
+        for (Iterator classIterator = allClasses.iterator(); classIterator.hasNext();)
         {
-            /*
-             * for those tools not supporting setting the context of an activity graph (such as Poseidon)
-             * an alternative is implemented: a tagged value on the controller, specifying the name of the use-case
-             */
-
-            final String useCaseName = getUseCase().getName();
-
-            // loop over the controllers, looking for the tagged value matching this activity graph's use-case name
-            Collection allClasses = getModel().getRootPackage().getClasses();
-            for (Iterator classIterator = allClasses.iterator(); classIterator.hasNext();)
+            ModelElementFacade element = (ModelElementFacade) classIterator.next();
+            if (element.hasStereotype(Bpm4StrutsProfile.STEREOTYPE_CONTROLLER))
             {
-                ModelElementFacade element = (ModelElementFacade) classIterator.next();
-                if (element.hasStereotype(Bpm4StrutsProfile.STEREOTYPE_CONTROLLER))
+                Object value = element.findTaggedValue(Bpm4StrutsProfile.TAGGED_VALUE_USE_CASE);
+                String taggedValue = value==null?null:value.toString();
+                if (useCaseName.equalsIgnoreCase(taggedValue))
                 {
-                    Object value = element.findTaggedValue(Bpm4StrutsProfile.TAGGED_VALUE_USE_CASE);
-                    String taggedValue = value==null?null:value.toString();
-                    if (useCaseName.equalsIgnoreCase(taggedValue))
-                    {
-                        return element;
-                    }
+                    return element;
                 }
             }
         }
