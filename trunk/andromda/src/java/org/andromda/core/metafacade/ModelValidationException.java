4d3d9697aea89a6f0d7ad4eb0df2e4f0218395b6
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-06-06 02:04:47 +0000

    ModelValidationException extends runtime now

--- trunk/andromda/src/java/org/andromda/core/metafacade/ModelValidationException.java
+++ trunk/andromda/src/java/org/andromda/core/metafacade/ModelValidationException.java
@@ -1,13 +1,24 @@
 package org.andromda.core.metafacade;
 
 /**
- * This exception should be thrown when any model validation error
- * occurs.
+ * This exception should be thrown when 
+ * any model validation error occurs.
  */
-public class ModelValidationException extends Exception
+public class ModelValidationException extends RuntimeException
 {
-    public ModelValidationException(String modelElementClassName, String modelElementName, String message)
+    /**
+     * Constructs a new instance of the ModelValidationException 
+     * from a <code>modelElementClass</code>, 
+     * <code>modelElementName</code> and the actual <code>message</code>.
+     * @param modelElementClass the class name of the model element where
+     *        the validation exception occurred.
+     * @param modelElementName the name of the model element to validate.
+     * @param message the message to give.
+     */
+    public ModelValidationException(Class modelElementClass, String modelElementName, String message)
     {
-        super(modelElementClassName + " [" + modelElementName + "] : " + message);
+        super(modelElementClass.getName() 
+            + " [" + modelElementName 
+            + "] : " + message);
     }
 }
