46984b71a8d9cd63d4cd76d3162ea130e23fe9b4
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-06-13 00:12:16 +0000

    implemented a few more methods and added Javadoc

--- translation-libraries/andromda-validation/src/java/org/andromda/translation/validation/OCLCollections.java
+++ translation-libraries/andromda-validation/src/java/org/andromda/translation/validation/OCLCollections.java
@@ -1,30 +1,45 @@
 package org.andromda.translation.validation;
 
+import org.apache.commons.collections.bag.HashBag;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Bag;
+import org.apache.commons.collections.SetUtils;
+import org.apache.commons.collections.Predicate;
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.Closure;
+
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
-
-import org.apache.commons.collections.Bag;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.SetUtils;
-import org.apache.commons.collections.bag.HashBag;
+import java.util.LinkedList;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Random;
 
 public final class OCLCollections
 {
+    /**
+     * Counts the number of occurrences of the argument item in the source collection.
+     */
     public static int count(Collection collection, Object item)
     {
         return CollectionUtils.cardinality(item, collection);
     }
 
+    /**
+     * Return true if the object is not an element of the collection, false otherwise.
+     */
     public static boolean excludes(Collection collection, Object item)
     {
         return !collection.contains(item);
     }
 
+    /**
+     * Returns true if all elements of the parameter collection are not present in the current
+     * collection, false otherwise.
+     */
     public static boolean excludesAll(Collection collection, Collection items)
     {
         for (Iterator iterator = items.iterator(); iterator.hasNext();)
@@ -35,51 +50,87 @@ public final class OCLCollections
         return true;
     }
 
+    /**
+     * Returns true if the object is an element of the collection, false otherwise.
+     */
     public static boolean includes(Collection collection, Object item)
     {
         return collection.contains(item);
     }
 
+    /**
+     * Returns true if all elements of the parameter collection are
+     * present in the current collection, false otherwise.
+     */
     public static boolean includesAll(Collection collection, Collection items)
     {
         return collection.containsAll(items);
     }
 
+    /**
+     * Returns true if the collection contains no elements, false otherwise.
+     */
     public static boolean isEmpty(Collection collection)
     {
         return (collection == null) || (collection.isEmpty());
     }
 
+    /**
+     * Returns true if the argument is <code>null</code>, false otherwise.
+     */
     public static boolean isEmpty(Object object)
     {
         return object == null;
     }
 
+    /**
+     * Returns true if the argument is either <code>null</code> or only contains whitespace characters, false otherwise.
+     */
     public static boolean isEmpty(String string)
     {
         return (string == null) || (string.trim().length() == 0);
     }
 
+    /**
+     * Returns true if the collection contains one or more elements, false otherwise.
+     */
     public static boolean notEmpty(Collection collection)
     {
         return (collection != null) && (!isEmpty(collection));
     }
 
+    /**
+     * Returns true if the argument is not <code>null</code>, false otherwise.
+     */
     public static boolean notEmpty(Object object)
     {
         return object != null;
     }
 
+    /**
+     * Returns true if the argument is neither <code>null</code>
+     * nor only contains whitespace characters, false otherwise.
+     */
     public static boolean notEmpty(String string)
     {
         return (string != null) && (string.trim().length() > 0);
     }
 
+    /**
+     * Returns the number of elements in the collection.
+     */
     public static int size(Collection collection)
     {
         return collection.size();
     }
 
+    /**
+     * Returns the sum of all the element in the collections. Every element must extend java.lang.Number or
+     * this method will throw an exception.
+     *
+     * @param collection a collection containing only classes extending java.lang.Number
+     * @return the sum of all the elements in the collection
+     */
     public static double sum(Collection collection)
     {
         double sum = 0;
@@ -97,56 +148,100 @@ public final class OCLCollections
         return sum;
     }
 
-    public static boolean append(List collection, Object item)
+    /**
+     * Appends the item to the list.
+     *
+     * @return true if the operation was a success
+     */
+    public static boolean append(List list, Object item)
     {
-        return collection.add(item);
+        return list.add(item);
     }
 
-    public static Object prepend(List collection, Object item)
+    /**
+     * Insert the item into the first position of the list.
+     *
+     * @return the element previously at the first position
+     */
+    public static Object prepend(List list, Object item)
     {
-        return collection.set(0, item);
+        return list.set(0, item);
     }
 
+    /**
+     * Appends the item to the bag.
+     *
+     * @return true if the operation was a success
+     */
     public static boolean append(Bag collection, Object item)
     {
         return collection.add(item);
     }
 
+    /**
+     * Returns the argument as a bag.
+     */
     public static Bag asBag(Collection collection)
     {
         return new HashBag(collection);
     }
 
+    /**
+     * Returns the argument as an ordered set.
+     */
     public static Set asOrderedSet(Collection collection)
     {
         return SetUtils.orderedSet(new TreeSet(collection));
     }
 
+    /**
+     * Returns the argument as a list.
+     */
     public static List asSequence(Collection collection)
     {
         return new LinkedList(collection);
     }
 
+    /**
+     * Returns the argument as a set.
+     */
     public static Set asSet(Collection collection)
     {
         return new HashSet(collection);
     }
 
-    public static Object at(List collection, int index)
+    /**
+     * Returns the element at the specified index in the argument list.
+     */
+    public static Object at(List list, int index)
     {
-        return collection.get(index);
+        return list.get(index);
     }
 
+    /**
+     * Removes all occurrences of the item in the source collection.
+     *
+     * @return true if one or more elements were removed
+     */
     public static boolean excluding(Collection collection, Object item)
     {
         return collection.remove(item);
     }
 
+    /**
+     * Adds the item to the list
+     *
+     * @return true if the element was added
+     */
     public static boolean including(Collection collection, Object item)
     {
         return collection.add(item);
     }
 
+    /**
+     * Recursively flattens this collection, this method returns a Collection containing no nested Collection
+     * instances.
+     */
     public static Collection flatten(Collection collection)
     {
         Collection flattenedCollection = new LinkedList();
@@ -163,38 +258,58 @@ public final class OCLCollections
         return flattenedCollection;
     }
 
+    /**
+     * Returns the index in this list of the first occurrence of the specified element, or -1
+     * if this list does not contain this element. More formally, returns the lowest index i
+     * such that (o==null ? get(i)==null : o.equals(get(i))), or -1 if there is no such index.
+     */
     public static int indexOf(List collection, Object item)
     {
         return collection.indexOf(item);
     }
 
-    public static Object insertAt(List collection, int index, Object item)
+    /**
+     * Insert the item at the specified index into the collection.
+     */
+    public static void insertAt(List collection, int index, Object item)
     {
-        return collection.set(index, item);
+        collection.add(index, item);
     }
 
+    /**
+     * Returns the collection of elements common in both argument collections.
+     */
     public static Collection intersection(Collection first, Collection second)
     {
         return CollectionUtils.intersection(first, second);
     }
 
+    /**
+     * Returns the union of both collections into a single collection.
+     */
     public static Collection union(Collection first, Collection second)
     {
         return CollectionUtils.union(first, second);
     }
 
+    /**
+     * Returns the last element in the collection.
+     */
     public static Object last(List collection)
     {
         return (collection.isEmpty()) ? null : collection.get(collection.size()-1);
     }
 
+    /**
+     * Returns those element that are contained in only one of both collections.
+     */ 
     public static Collection symmetricDifference(Collection first, Collection second)
     {
         return CollectionUtils.disjunction(first, second);
     }
 
     /**
-     * todo: implement
+     * @todo: implement
      */
     public static Set subOrderedSet(Set collection)
     {
@@ -202,7 +317,7 @@ public final class OCLCollections
     }
 
     /**
-     * todo: implement
+     * @todo: implement
      */
     public static List subSequence(List collection)
     {
@@ -210,23 +325,28 @@ public final class OCLCollections
     }
 
     /**
-     * todo: implement
+     * Returns a random element from the collection for which the argument expression evaluates true.
      */
-    public static Collection any(Collection collection)
+    public static Object any(Collection collection, Predicate predicate)
     {
-        throw new UnsupportedOperationException(OCLCollections.class.getName()+".any");
+        final List selectedElements = new ArrayList(select(collection, predicate));
+        Random random = new Random(System.currentTimeMillis());
+        return selectedElements.isEmpty()
+            ? null
+            : selectedElements.get(random.nextInt(selectedElements.size()));
     }
 
     /**
-     * todo: implement
+     * Returns the collection of Objects that results from executing the transformer on each individual
+     * element in the source collection.
      */
-    public static Collection collect(Collection collection)
+    public static Collection collect(Collection collection, Transformer transformer)
     {
-        throw new UnsupportedOperationException(OCLCollections.class.getName()+".collect");
+        return CollectionUtils.collect(collection, transformer);
     }
 
     /**
-     * todo: implement
+     * @todo: implement
      */
     public static Collection collectNested(Collection collection)
     {
@@ -234,31 +354,37 @@ public final class OCLCollections
     }
 
     /**
-     * todo: implement
+     * Returns true if a predicate is true for at least one element of a collection.
+     * <p>
+     * A null collection or predicate returns false.
      */
-    public static Collection exists(Collection collection)
+    public static boolean exists(Collection collection, Predicate predicate)
     {
-        throw new UnsupportedOperationException(OCLCollections.class.getName()+".exists");
+        return CollectionUtils.exists(collection, predicate);
     }
 
     /**
-     * todo: implement
+     * Executes the given closure on each element in the collection.
+     * <p>
+     * If the input collection or closure is null, there is no change made.
      */
-    public static Collection forAll(Collection collection)
+    public static void forAll(Collection collection, Closure closure)
     {
-        throw new UnsupportedOperationException(OCLCollections.class.getName()+".forAll");
+        CollectionUtils.forAllDo(collection, closure);
     }
 
     /**
-     * todo: implement
+     * Returns <code>true</code> if the expression has a unique value for each
+     * element in the source collection.
+     * @todo: implement
      */
-    public static Collection isUnique(Collection collection)
+    public static Collection isUnique(Collection collection, OCLExpression expression)
     {
         throw new UnsupportedOperationException(OCLCollections.class.getName()+".isUnique");
     }
 
     /**
-     * todo: implement
+     * @todo: implement
      */
     public static Collection iterate(Collection collection)
     {
@@ -269,12 +395,12 @@ public final class OCLCollections
      * Returns <code>true</true> when the argument expression evaluates true for one and only
      * one element in the collection. Returns <code>false</code> otherwise.
      */
-    public static boolean one(Collection collection, OCLExpression expression)
+    public static boolean one(Collection collection, Predicate predicate)
     {
         boolean found = false;
         for (Iterator iterator = collection.iterator(); iterator.hasNext();)
         {
-            if (expression.evaluate(iterator.next()))
+            if (predicate.evaluate(iterator.next()))
             {
                 if (found) return false; else found = true;
             }
@@ -283,27 +409,28 @@ public final class OCLCollections
     }
 
     /**
-     * todo: implement
+     * Returns a subcollection of the source collection containing all elements for which
+     * the expression evaluates <code>false</code>.
      */
-    public static Collection reject(Collection collection)
+    public static Collection reject(Collection collection, Predicate predicate)
     {
-        throw new UnsupportedOperationException(OCLCollections.class.getName()+".reject");
+        return CollectionUtils.selectRejected(collection, predicate);
     }
 
     /**
-     * todo: implement
+     * Returns a subcollection of the source collection containing all elements for which
+     * the expression evaluates <code>true</code>.
      */
-    public static Collection select(Collection collection)
+    public static Collection select(Collection collection, Predicate predicate)
     {
-        throw new UnsupportedOperationException(OCLCollections.class.getName()+".select");
+        return CollectionUtils.select(collection, predicate);
     }
 
     /**
-     * todo: implement
+     * @todo: implement
      */
     public static Collection sortedBy(Collection collection)
     {
         throw new UnsupportedOperationException(OCLCollections.class.getName()+".sortedBy");
     }
-
 }
