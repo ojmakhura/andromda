41610910f90c4a8dea71fd1c173ba42eadf92bbe
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-07-01 22:24:59 +0000

    Format

--- trunk/metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/UMLModelAccessFacade.java
+++ trunk/metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/UMLModelAccessFacade.java
@@ -15,28 +15,29 @@ import org.omg.uml.UmlPackage;
 import org.omg.uml.foundation.core.ModelElement;
 
 /**
- * Contains a UML model, follows the ModelAccessFacade interface
- * and can therefore be processed by AndroMDA.
- *
- * @author <a href="http://www.mbohlen.de">Matthias Bohlen</a>
+ * Contains a UML model, follows the ModelAccessFacade interface and can
+ * therefore be processed by AndroMDA.
+ * 
+ * @author <a href="http://www.mbohlen.de">Matthias Bohlen </a>
  * @author Chad Brandon
  */
-public class UMLModelAccessFacade implements ModelAccessFacade
+public class UMLModelAccessFacade
+    implements ModelAccessFacade
 {
     private Logger logger = Logger.getLogger(UMLModelAccessFacade.class);
-    
+
     private UmlPackage model;
-    
+
     /**
      * @see org.andromda.core.metafacade.ModelAccessFacade#setModel(java.lang.Object)
      */
-    public void setModel(Object model) 
+    public void setModel(Object model)
     {
-        final String methodName = "UMLModelFacade.setModel";
+        final String methodName = "UMLModelAccessFacade.setModel";
         ExceptionUtils.checkNull(methodName, "model", model);
         ExceptionUtils.checkAssignable(
-            methodName, 
-            UmlPackage.class, 
+            methodName,
+            UmlPackage.class,
             "modelElement",
             model.getClass());
         this.model = (UmlPackage)model;
@@ -55,11 +56,11 @@ public class UMLModelAccessFacade implements ModelAccessFacade
      */
     public String getName(Object modelElement)
     {
-        final String methodName = "UMLModelFacade.getName";
+        final String methodName = "UMLModelAccessFacade.getName";
         ExceptionUtils.checkNull(methodName, "modelElement", modelElement);
         ExceptionUtils.checkAssignable(
-            methodName, 
-            ModelElementFacade.class, 
+            methodName,
+            ModelElementFacade.class,
             "modelElement",
             modelElement.getClass());
         return ((ModelElementFacade)modelElement).getName();
@@ -70,11 +71,11 @@ public class UMLModelAccessFacade implements ModelAccessFacade
      */
     public String getPackageName(Object modelElement)
     {
-        final String methodName = "UMLModelFacade.getPackageName";
+        final String methodName = "UMLModelAccessFacade.getPackageName";
         ExceptionUtils.checkNull(methodName, "modelElement", modelElement);
         ExceptionUtils.checkAssignable(
-            methodName, 
-            ModelElementFacade.class, 
+            methodName,
+            ModelElementFacade.class,
             "modelElement",
             modelElement.getClass());
         return ((ModelElementFacade)modelElement).getPackageName();
@@ -88,43 +89,50 @@ public class UMLModelAccessFacade implements ModelAccessFacade
         Collection stereoTypeNames = new Vector();
         if (modelElement instanceof ModelElement)
         {
-            ModelElement m = (ModelElement) modelElement;
+            ModelElement m = (ModelElement)modelElement;
             Collection stereotypes = m.getStereotype();
             for (Iterator i = stereotypes.iterator(); i.hasNext();)
             {
-                ModelElement stereotype = (ModelElement) i.next();
+                ModelElement stereotype = (ModelElement)i.next();
                 stereoTypeNames.add(stereotype.getName());
             }
         }
 
         return stereoTypeNames;
     }
-    
+
     /**
      * @see edu.duke.dcri.mda.model.ModelAccessFacade#findByStereotype(java.lang.String)
      */
-    public Collection findByStereotype(String stereotype) 
+    public Collection findByStereotype(String stereotype)
     {
-        final String methodName = "UMLModelFacade.findByStereotype";
+        final String methodName = "UMLModelAccessFacade.findByStereotype";
         Collection modelElements = new ArrayList();
         stereotype = StringUtils.trimToEmpty(stereotype);
-        if (StringUtils.isNotEmpty(stereotype)) {
-            if (this.model != null) {
-                Collection underlyingElements = 
-                    model.getCore().getModelElement().refAllOfType();
-                if (underlyingElements != null || !underlyingElements.isEmpty()) {
+        if (StringUtils.isNotEmpty(stereotype))
+        {
+            if (this.model != null)
+            {
+                Collection underlyingElements = model.getCore()
+                    .getModelElement().refAllOfType();
+                if (underlyingElements != null || !underlyingElements.isEmpty())
+                {
                     Iterator elementIt = underlyingElements.iterator();
-                    while (elementIt.hasNext()) {
+                    while (elementIt.hasNext())
+                    {
                         ModelElement element = (ModelElement)elementIt.next();
-                        Collection stereotypeNames = this.getStereotypeNames(element);
-                        if (stereotypeNames != null && stereotypeNames.contains(stereotype)) {
+                        Collection stereotypeNames = this
+                            .getStereotypeNames(element);
+                        if (stereotypeNames != null
+                            && stereotypeNames.contains(stereotype))
+                        {
                             modelElements.add(element);
                         }
                     }
                 }
                 if (logger.isDebugEnabled())
-                    logger.debug("completed " + methodName 
-                        + " with " + modelElements.size() + " modelElements");
+                    logger.debug("completed " + methodName + " with "
+                        + modelElements.size() + " modelElements");
             }
         }
         return modelElements;
@@ -133,7 +141,7 @@ public class UMLModelAccessFacade implements ModelAccessFacade
     /**
      * @see org.andromda.core.metafacade.ModelAccessFacade#getModelElements()
      */
-    public Collection getModelElements() 
+    public Collection getModelElements()
     {
         return MetafacadeFactory.getInstance().createMetafacades(
             this.model.getCore().getModelElement().refAllOfType());
