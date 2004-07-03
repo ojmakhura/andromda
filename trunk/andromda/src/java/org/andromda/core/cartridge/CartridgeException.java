26b5e852f3745361013a99bde94162d3fec3d133
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-07-03 18:40:46 +0000

    cleanup formatting

--- trunk/andromda/src/java/org/andromda/core/cartridge/CartridgeException.java
+++ trunk/andromda/src/java/org/andromda/core/cartridge/CartridgeException.java
@@ -3,11 +3,11 @@ package org.andromda.core.cartridge;
 /**
  * This exception is thrown when a special situation is encountered within an
  * AndroMDA cartridge.
- *
- * @author <a href="http://www.mbohlen.de">Matthias Bohlen</a>
- *
+ * 
+ * @author <a href="http://www.mbohlen.de">Matthias Bohlen </a>
  */
-public class CartridgeException extends RuntimeException
+public class CartridgeException
+    extends RuntimeException
 {
 
     /**
@@ -20,28 +20,35 @@ public class CartridgeException extends RuntimeException
 
     /**
      * Constructor for CartridgeException.
+     * 
      * @param arg0
      */
-    public CartridgeException(String arg0)
+    public CartridgeException(
+        String arg0)
     {
         super(arg0);
     }
 
     /**
      * Constructor for CartridgeException.
+     * 
      * @param arg0
      * @param arg1
      */
-    public CartridgeException(String arg0, Throwable arg1)
+    public CartridgeException(
+        String arg0,
+        Throwable arg1)
     {
         super(arg0, arg1);
     }
 
     /**
      * Constructor for CartridgeException.
+     * 
      * @param arg0
      */
-    public CartridgeException(Throwable arg0)
+    public CartridgeException(
+        Throwable arg0)
     {
         super(arg0);
     }
--- trunk/andromda/src/java/org/andromda/core/cartridge/TemplateModelElements.java
+++ trunk/andromda/src/java/org/andromda/core/cartridge/TemplateModelElements.java
@@ -10,147 +10,160 @@ import org.andromda.core.common.ExceptionUtils;
 import org.apache.commons.lang.StringUtils;
 
 /**
- * Defines the &lt;modelElements/&gt;
- * element within a &lt;template/&gt; 
- * within an XML cartridge descriptor.
+ * Defines the &lt;modelElements/&gt; element within a &lt;template/&gt; within
+ * an XML cartridge descriptor.
  * 
  * @see org.andromda.core.cartridge.TemplateConfiguration
  * @see org.andromda.core.cartridge.TemplateModelElement
- * 
  * @author Chad Brandon
  */
-public class TemplateModelElements {
-    
+public class TemplateModelElements
+{
+
     private String variable;
     private Map modelElements = null;
-     
-    public TemplateModelElements() {
-        this.modelElements = new HashMap();   
+
+    public TemplateModelElements()
+    {
+        this.modelElements = new HashMap();
     }
-    
+
     /**
-     * The variable name to make the model element 
-     * available to the template engine. For example if you
-     * have the modelElement &lt;&lt;entity&gt;&gt;
-     * defined within your &lt;&lt;modelElements&gt;&gt; element
-     * you may want to define this value as <code>entity</code>.
-     * If on the other hand the outputToSingleFile
-     * flag is set to true you'd probably want to
-     * make it available as <code>entities</code>. 
+     * The variable name to make the model element available to the template
+     * engine. For example if you have the modelElement &lt;&lt;entity&gt;&gt;
+     * defined within your &lt;&lt;modelElements&gt;&gt; element you may want to
+     * define this value as <code>entity</code>. If on the other hand the
+     * outputToSingleFile flag is set to true you'd probably want to make it
+     * available as <code>entities</code>.
      * 
      * @return Returns the variable.
      */
-    public String getVariable() {
+    public String getVariable()
+    {
         return variable;
     }
-    
+
     /**
      * @param variable The variable to set.
      */
-    public void setVariable(String variable) {
+    public void setVariable(String variable)
+    {
         final String methodName = "TemplateModelElements.setVariable";
         variable = StringUtils.trimToEmpty(variable);
         ExceptionUtils.checkEmpty(methodName, "variable", variable);
         this.variable = variable;
     }
-    
+
     /**
-     * Adds a modelElement to the Map of <code>modelElements</code>
-     * and keys it by <code>stereotype</code>. The <code>stereotype</code>
-     * of the modelElement must be defined.
+     * Adds a modelElement to the Map of <code>modelElements</code> and keys
+     * it by <code>stereotype</code>. The <code>stereotype</code> of the
+     * modelElement must be defined.
+     * 
      * @param modelElement the new TemplateModelElement to add.
      */
-    public void addModelElement(TemplateModelElement modelElement) {
+    public void addModelElement(TemplateModelElement modelElement)
+    {
         final String methodName = "TemplateModelElements.addModelElement";
         ExceptionUtils.checkNull(methodName, "modelElement", modelElement);
-        String stereotype = StringUtils.trimToEmpty(modelElement.getStereotype());
-        ExceptionUtils.checkEmpty(methodName, "modelElement.stereotype", stereotype);
+        String stereotype = StringUtils.trimToEmpty(modelElement
+            .getStereotype());
+        ExceptionUtils.checkEmpty(
+            methodName,
+            "modelElement.stereotype",
+            stereotype);
         modelElements.put(stereotype, modelElement);
     }
-    
+
     /**
-     * Retrieves the TemplateModelElement
-     * having the given <code>stereotypeName</code>.  
+     * Retrieves the TemplateModelElement having the given
+     * <code>stereotypeName</code>.
      * 
      * @param stereotypeName the name of the modelElement's stereotype
      */
-    public TemplateModelElement getModelElement(String stereotypeName) {
+    public TemplateModelElement getModelElement(String stereotypeName)
+    {
         final String methodName = "TemplateModelElements.getModelElement";
         stereotypeName = StringUtils.trimToEmpty(stereotypeName);
-        ExceptionUtils.checkEmpty(methodName, "stereotypeName", stereotypeName);       
+        ExceptionUtils.checkEmpty(methodName, "stereotypeName", stereotypeName);
         return (TemplateModelElement)this.modelElements.get(stereotypeName);
     }
-    
+
     /**
-     * Retrieves the model elements bound to the stereotype
-     * having the given <code>stereotypeName</code>.  
+     * Retrieves the model elements bound to the stereotype having the given
+     * <code>stereotypeName</code>.
      * 
-     * @param stereotypeName the name of the stereotype 
+     * @param stereotypeName the name of the stereotype
      */
-    public Collection getModelElements(String stereotypeName) {    
+    public Collection getModelElements(String stereotypeName)
+    {
         Collection modelElements = null;
         TemplateModelElement stereotype = this.getModelElement(stereotypeName);
-        if (stereotype != null) {
+        if (stereotype != null)
+        {
             modelElements = stereotype.getModelElements();
         }
         return modelElements;
     }
-    
+
     /**
-     * Gets all model elements for each TemplateModelElement
-     * belonging to this TemplateModelElements instance.
+     * Gets all model elements for each TemplateModelElement belonging to this
+     * TemplateModelElements instance.
+     * 
      * @return Collection of all model elements.
      */
-    public Collection getAllModelElements() {
-    	Collection allModelElements = new ArrayList();
+    public Collection getAllModelElements()
+    {
+        Collection allModelElements = new ArrayList();
         Iterator stereotypeNames = this.stereotypeNames();
-        while (stereotypeNames.hasNext()) {
-        	String name = (String)stereotypeNames.next();
+        while (stereotypeNames.hasNext())
+        {
+            String name = (String)stereotypeNames.next();
             allModelElements.addAll(getModelElements(name));
         }
         return allModelElements;
     }
-    
+
     /**
-     * Retrieves the variable for the given 
-     * <code>stereotypeName</code>.  First it checks
-     * to see if a variable has been defined for the stereotype
-     * wit the <code>stereotypeName</code>, if it can't find
-     * that, it uses the default <code>variable</code> defined
-     * for this TemplateModelElements object.
+     * Retrieves the variable for the given <code>stereotypeName</code>.
+     * First it checks to see if a variable has been defined for the stereotype
+     * wit the <code>stereotypeName</code>, if it can't find that, it uses
+     * the default <code>variable</code> defined for this
+     * TemplateModelElements object.
      * 
-     * @param stereotypeName the name of the stereotype of which
-     *        to retrieve the variable
+     * @param stereotypeName the name of the stereotype of which to retrieve the
+     *        variable
      */
-    protected String getVariable(String stereotypeName) {
+    protected String getVariable(String stereotypeName)
+    {
         stereotypeName = StringUtils.trimToEmpty(stereotypeName);
         String variable = (String)modelElements.get(stereotypeName);
-        if (StringUtils.isEmpty(variable)) {
-        	variable = this.variable;
+        if (StringUtils.isEmpty(variable))
+        {
+            variable = this.variable;
         }
         return variable;
     }
-    
+
     /**
-     * Returns an Iterator containing all stereotype
-     * names for the modelElement stereotypes of this
-     * instance.
+     * Returns an Iterator containing all stereotype names for the modelElement
+     * stereotypes of this instance.
      * 
      * @return Iterator
      */
-    protected Iterator stereotypeNames() {
-    	return this.modelElements.keySet().iterator();
+    protected Iterator stereotypeNames()
+    {
+        return this.modelElements.keySet().iterator();
     }
-        
+
     /**
-     * Returns true if this instance
-     * has no <code>modelElements</code>
-     * stored within it.
+     * Returns true if this instance has no <code>modelElements</code> stored
+     * within it.
      * 
      * @return true/false
      */
-    protected boolean isEmpty() {
-    	return this.modelElements.isEmpty();
+    protected boolean isEmpty()
+    {
+        return this.modelElements.isEmpty();
     }
-    
+
 }
