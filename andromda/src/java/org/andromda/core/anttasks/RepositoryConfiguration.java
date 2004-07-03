f9301d057bac2204e7de7f6276b610d6cddd950f
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-07-03 18:39:15 +0000

    cleanup formatting a bit

--- andromda/src/java/org/andromda/core/anttasks/RepositoryConfiguration.java
+++ andromda/src/java/org/andromda/core/anttasks/RepositoryConfiguration.java
@@ -4,33 +4,29 @@ import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.Path;
 
 /**
+ * This class implements the <code>&lt;repository&gt;</code> tag which can
+ * used within the ant <code>&lt;andromda&gt;</code> tag to configure androMDA
+ * to configure the object model repository. Currently its used as a way of
+ * specifying a <code>&lt;moduleSearchPath/&gt;</code>.
  * 
- * This class implements the <code>&lt;repository&gt;</code> tag
- * which can used within the ant <code>&lt;andromda&gt;</code> tag
- * to configure androMDA to configure the object model repository.
- * 
- * Currently its used as a way of specifying a 
- * <code>&lt;moduleSearchPath/&gt;</code>.
- * 
- * @author <A HREF="http://www.amowers.com">Anthony Mowers</A>
+ * @author <A HREF="http://www.amowers.com">Anthony Mowers </A>
  * @author Chad Brandon
- *  
  */
 public class RepositoryConfiguration
 {
-    
     private Project project;
     private Path moduleSearchPath = null;
 
-    public RepositoryConfiguration(Project project)
+    public RepositoryConfiguration(
+        Project project)
     {
         this.project = project;
     }
 
     /**
-     * Handles the nested &lt;moduleSearchPath&gt; element.
-     * The user can specify her own search path for submodels
-     * of the models that she is going to process.
+     * Handles the nested &lt;moduleSearchPath&gt; element. The user can specify
+     * her own search path for submodels of the models that she is going to
+     * process.
      * 
      * @return Path the module search path
      */
