50102656d99cba0d173f93ea0153e0b4d090c4a8
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-07-05 02:41:58 +0000

    Add wildcard matching for resources (no more reg expression matching)

---
+++ trunk/andromda/src/test/java/org/andromda/core/common/PathMatcherTest.java
@@ -0,0 +1,65 @@
+package org.andromda.core.common;
+
+import junit.framework.TestCase;
+
+/**
+ * JUnit tests for org.andromda.common.PathMatter
+ * 
+ * @author Chad Brandon
+ */
+public class PathMatcherTest
+    extends TestCase
+{
+    public void testWildcardMatch()
+    {
+        String path = "org/andromda/some/file/Test.java";
+        assertTrue(PathMatcher.wildcardMatch(
+            path, 
+            "**/*.java"));
+        assertFalse(PathMatcher.wildcardMatch(
+            path,
+            "*.java"));
+        assertTrue(PathMatcher.wildcardMatch(
+            path,
+            "*/*/*/*/*.java"));
+        assertFalse(PathMatcher.wildcardMatch(
+            path,
+            "*/*/*.java"));
+        assertTrue(PathMatcher.wildcardMatch(
+            path,
+            "**/*Test.java"));
+        assertTrue(PathMatcher.wildcardMatch(
+            path,
+            "**/*Tes*.java"));
+        assertFalse(PathMatcher.wildcardMatch(
+            path,
+            "**/*TestFile*.java"));  
+        assertFalse(PathMatcher.wildcardMatch(
+            path,
+            "**/.java")); 
+        
+        path = "Test.java";
+        assertTrue(PathMatcher.wildcardMatch(
+            path,
+            "*.java"));        
+        assertTrue(PathMatcher.wildcardMatch(
+            path,
+            "**/*.java"));
+        
+        
+        assertTrue(PathMatcher.wildcardMatch(
+            path,
+            "*.java"));
+        assertTrue(PathMatcher.wildcardMatch(
+            path,
+            "**.java"));
+        assertFalse(PathMatcher.wildcardMatch(
+            path,
+            "***/*.java"));
+        
+        path = "org/Test.java";
+        assertFalse(PathMatcher.wildcardMatch(
+            path,
+            "*.java"));
+    }
+}
