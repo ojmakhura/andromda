48d0e8bf90ed47b694adc28292882bbba4da308c
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-07-09 19:19:31 +0000

    add getEntityReferences()

--- trunk/metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/ServiceFacadeLogicImpl.java
+++ trunk/metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/ServiceFacadeLogicImpl.java
@@ -1,20 +1,49 @@
 package org.andromda.metafacades.uml14;
 
+import java.util.Collection;
+
+import org.andromda.metafacades.uml.DependencyFacade;
+import org.andromda.metafacades.uml.FilteredCollection;
+import org.andromda.metafacades.uml.UMLProfile;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Transformer;
 
 /**
- * 
- *
  * Metaclass facade implementation.
- *
  */
 public class ServiceFacadeLogicImpl
-       extends ServiceFacadeLogic
-       implements org.andromda.metafacades.uml.ServiceFacade
+    extends ServiceFacadeLogic
+    implements org.andromda.metafacades.uml.ServiceFacade
 {
     // ---------------- constructor -------------------------------
-    
-    public ServiceFacadeLogicImpl (java.lang.Object metaObject, String context)
+
+    public ServiceFacadeLogicImpl(
+        java.lang.Object metaObject,
+        String context)
     {
-        super (metaObject, context);
+        super(metaObject, context);
+    }
+
+    /**
+     * @see org.andromda.metafacades.uml.ServiceFacade#getEntityReferences()
+     */
+    public Collection handleGetEntityReferences()
+    {
+        Collection refs = new FilteredCollection(this.getDependencies())
+        {
+            public boolean evaluate(Object object)
+            {
+                return ((DependencyFacade)object)
+                    .hasStereotype(UMLProfile.STEREOTYPE_ENTITY_REF);
+            }
+        };
+        CollectionUtils.transform(refs, new Transformer()
+        {
+            public Object transform(Object object)
+            {
+                return ((DependencyFacade)object).getTargetElement();
+            }
+        });
+        return refs;
     }
 }
