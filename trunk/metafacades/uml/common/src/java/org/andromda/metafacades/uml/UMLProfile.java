a44cb1cdc56ca3173a6c77596c8e2cfbab5b2a9b
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-07-10 14:23:44 +0000

    add file I forgot to check int with last change

--- trunk/metafacades/uml/common/src/java/org/andromda/metafacades/uml/UMLProfile.java
+++ trunk/metafacades/uml/common/src/java/org/andromda/metafacades/uml/UMLProfile.java
@@ -72,6 +72,11 @@ public class UMLProfile
     public static final String STEREOTYPE_EXCEPTION_REF = umlProfile
         .getString("STEREOTYPE_EXCEPTION_REF");
 
+    /**
+     * Represents a reference to an entity. 
+     */
+    public static final String STEREOTYPE_ENTITY_REF = umlProfile.getString("STEREOTYPE_ENTITY_REF");
+    
     /* ----------------- Tagged Values -------------------- */
 
     /**
