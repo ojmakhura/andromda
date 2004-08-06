6c79779a6b79e80857efb8fafd0960e536f913eb
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-08-06 01:40:37 +0000

    add hasWrapper() method that checks to see if the language mapped type has a wrapper defined with it.  The wrapper will be found in the wrapper-mappings file.  Also improve isCollectionType and isDateType so that they are ONLY using model types.

--- trunk/metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/UMLMetafacadeGlobals.java
+++ trunk/metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/UMLMetafacadeGlobals.java
@@ -11,4 +11,22 @@ public class UMLMetafacadeGlobals
      * The seperator used for package seperation in the generated metafacades.
      */
     public static final char PACKAGE_SEPERATOR = '.';
+    
+    /**
+     * Used to identify collection types in the model, any other type
+     * that will be identified as a collection must specialize this type.
+     */
+    public static final String COLLECTION_TYPE_NAME = "datatype.Collection";
+    
+    /**
+     * Used to identify a list type in the model, any other type
+     * that will be identified as a list must specialize this type.
+     */
+    public static final String LIST_TYPE_NAME = "datatype.List";
+    
+    /**
+     * Used to identify date types in the model, any other type
+     * that will be identified as a date must specialize this type.
+     */
+    public static final String DATE_TYPE_NAME = "datatype.Date";
 }
