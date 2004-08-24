1b10bdebab04467b1c0c0082809f8a3378f39f6e
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-08-24 02:15:02 +0000

    added openWindow()

--- trunk/cartridges/andromda-bpm4struts/src/resources/layout-common.js
+++ trunk/cartridges/andromda-bpm4struts/src/resources/layout-common.js
@@ -15,3 +15,23 @@ function getDiv(divID)
 
     return false;
 }
+
+function openWindow(href,name,centered,attributes,width,height)
+{
+    var w = (width) ? width : 400;
+    var h = (height) ? height : 300;
+
+    var windowWidth = (document.all) ? document.body.clientWidth : window.outerWidth;
+    var windowHeight = (document.all) ? document.body.clientHeight : window.outerHeight;
+
+    x = (windowWidth-w) / 2;
+    y = (windowHeight-h) / 2;
+
+    var properties = (attributes)
+        ? "toolbar=yes,directories=yes,status=yes,menubar=yes,copyhistory=yes,location=yes"
+        : "toolbar=no,directories=no,status=no,menubar=no,copyhistory=no,location=no";
+
+    properties = "width="+w+",height="+h+",top="+y+",left="+x+",scrollbars=yes,resizable=yes," + properties;
+
+    window.open(href,name,properties);
+}
