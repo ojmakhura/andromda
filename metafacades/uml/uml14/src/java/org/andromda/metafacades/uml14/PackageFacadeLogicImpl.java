8882cd4968f7c3374323e6f4f07f78d72790305f
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-09-04 09:28:32 +0000

    implemented 'getOwnedElements'

--- metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/PackageFacadeLogicImpl.java
+++ metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/PackageFacadeLogicImpl.java
@@ -1,7 +1,5 @@
 package org.andromda.metafacades.uml14;
 
-import java.util.Collection;
-
 import org.andromda.core.metafacade.MetafacadeFactory;
 import org.andromda.metafacades.uml.FilteredCollection;
 import org.andromda.metafacades.uml.ModelElementFacade;
@@ -11,18 +9,19 @@ import org.apache.commons.lang.StringUtils;
 import org.omg.uml.UmlPackage;
 import org.omg.uml.foundation.core.UmlClass;
 
+import java.util.Collection;
+
 /**
  * Metaclass facade implementation.
  */
 public class PackageFacadeLogicImpl
-    extends PackageFacadeLogic
-    implements org.andromda.metafacades.uml.PackageFacade
+        extends PackageFacadeLogic
+        implements org.andromda.metafacades.uml.PackageFacade
 {
     // ---------------- constructor -------------------------------
 
-    public PackageFacadeLogicImpl(
-        org.omg.uml.modelmanagement.UmlPackage metaObject,
-        String context)
+    public PackageFacadeLogicImpl(org.omg.uml.modelmanagement.UmlPackage metaObject,
+                                  String context)
     {
         super(metaObject, context);
     }
@@ -60,26 +59,29 @@ public class PackageFacadeLogicImpl
      */
     protected Collection handleGetModelElements()
     {
-        return ((UmlPackage)MetafacadeFactory.getInstance().getModel()
-            .getModel()).getCore().getModelElement().refAllOfType();
+        return ((UmlPackage) MetafacadeFactory.getInstance().getModel()
+                .getModel()).getCore().getModelElement().refAllOfType();
     }
 
     /**
      * @see org.andromda.metafacades.uml.ModelFacade#findModelElement(java.lang.String)
      */
-    public ModelElementFacade handleFindModelElement(
-        final String fullyQualifiedName)
+    public ModelElementFacade handleFindModelElement(final String fullyQualifiedName)
     {
-        return (ModelElementFacade)CollectionUtils.find(
-            this.getModelElements(),
-            new Predicate()
-            {
-                public boolean evaluate(Object object)
+        return (ModelElementFacade) CollectionUtils.find(this.getModelElements(),
+                new Predicate()
                 {
-                    return ModelElementFacade.class.isAssignableFrom(object.getClass()) &&  
-                        ((ModelElementFacade)object).getFullyQualifiedName(true)
-                        .equals(StringUtils.trimToEmpty(fullyQualifiedName));
-                }
-            });
+                    public boolean evaluate(Object object)
+                    {
+                        return ModelElementFacade.class.isAssignableFrom(object.getClass()) &&
+                                ((ModelElementFacade) object).getFullyQualifiedName(true)
+                                .equals(StringUtils.trimToEmpty(fullyQualifiedName));
+                    }
+                });
+    }
+
+    protected Collection handleGetOwnedElements()
+    {
+        return metaObject.getOwnedElement();
     }
 }
