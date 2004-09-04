14989656cd26e51213a251d1b7d556222cff79b9
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-09-04 09:24:33 +0000

    no message

---
+++ trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsPackageLogicImpl.java
@@ -0,0 +1,41 @@
+package org.andromda.cartridges.bpm4struts.metafacades;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.bpm4struts.metafacades.StrutsPackage.
+ *
+ * @see org.andromda.cartridges.bpm4struts.metafacades.StrutsPackage
+ */
+public class StrutsPackageLogicImpl
+        extends StrutsPackageLogic
+        implements org.andromda.cartridges.bpm4struts.metafacades.StrutsPackage
+{
+    // ---------------- constructor -------------------------------
+
+    public StrutsPackageLogicImpl(Object metaObject, String context)
+    {
+        super(metaObject, context);
+    }
+
+    public java.util.Collection handleGetStrutsUseCases()
+    {
+        Collection useCases = new ArrayList();
+
+        Collection ownedElements = getOwnedElements();
+        for (Iterator elementIterator = ownedElements.iterator(); elementIterator.hasNext();)
+        {
+            Object object = elementIterator.next();
+            if (object instanceof StrutsUseCase)
+            {
+                useCases.add(object);
+            }
+        }
+
+        return useCases;
+    }
+
+}
