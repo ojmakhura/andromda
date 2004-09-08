fd716f170bffd215ab4d296eedda3f511db52b4a
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-09-08 15:24:15 +0000

    added a test for crunch()

--- trunk/andromda/src/test/java/org/andromda/core/common/StringUtilsHelperTest.java
+++ trunk/andromda/src/test/java/org/andromda/core/common/StringUtilsHelperTest.java
@@ -13,12 +13,12 @@ public class StringUtilsHelperTest extends TestCase
     {
         final String[][] fixture = new String[][]
         {
-            new String[] { "ejb", "Ejb" },
-            new String[] { "EJB", "EJB" },
-            new String[] { "an EJB class", "An EJB class" },
-            new String[] { "an EJB Class", "An EJB Class" },
-            new String[] { "HibernateEntity", "HibernateEntity" },
-            new String[] { "Hibernate Entity", "Hibernate Entity" }
+            new String[]{"ejb", "Ejb"},
+            new String[]{"EJB", "EJB"},
+            new String[]{"an EJB class", "An EJB class"},
+            new String[]{"an EJB Class", "An EJB Class"},
+            new String[]{"HibernateEntity", "HibernateEntity"},
+            new String[]{"Hibernate Entity", "Hibernate Entity"}
         };
 
         for (int i = 0; i < fixture.length; i++)
@@ -32,12 +32,12 @@ public class StringUtilsHelperTest extends TestCase
     {
         final String[][] fixture = new String[][]
         {
-            new String[] { "ejb", "ejb" },
-            new String[] { "EJB", "eJB" },
-            new String[] { "an EJB class", "an EJB class" },
-            new String[] { "an EJB Class", "an EJB Class" },
-            new String[] { "HibernateEntity", "hibernateEntity" },
-            new String[] { "Hibernate Entity", "hibernate Entity" }
+            new String[]{"ejb", "ejb"},
+            new String[]{"EJB", "eJB"},
+            new String[]{"an EJB class", "an EJB class"},
+            new String[]{"an EJB Class", "an EJB Class"},
+            new String[]{"HibernateEntity", "hibernateEntity"},
+            new String[]{"Hibernate Entity", "hibernate Entity"}
         };
 
         for (int i = 0; i < fixture.length; i++)
@@ -51,11 +51,11 @@ public class StringUtilsHelperTest extends TestCase
     {
         final String[][] fixture = new String[][]
         {
-            new String[] { "EntityHibernate", "Hibernate", "EJB", "EntityEJB" },
-            new String[] { "EntityHibernate", "Hibernate", "Hibernate", "EntityHibernate" },
-            new String[] { "EntityHibernate", "hibernate", "EJB", "EntityHibernate" },
-            new String[] { "EntityHibernate", "Entity", "EJB", "EntityHibernate" },
-            new String[] { "EntityHibernate", "ernate", "qwErty", "EntityHibqwErty" }
+            new String[]{"EntityHibernate", "Hibernate", "EJB", "EntityEJB"},
+            new String[]{"EntityHibernate", "Hibernate", "Hibernate", "EntityHibernate"},
+            new String[]{"EntityHibernate", "hibernate", "EJB", "EntityHibernate"},
+            new String[]{"EntityHibernate", "Entity", "EJB", "EntityHibernate"},
+            new String[]{"EntityHibernate", "ernate", "qwErty", "EntityHibqwErty"}
         };
 
         for (int i = 0; i < fixture.length; i++)
@@ -70,13 +70,13 @@ public class StringUtilsHelperTest extends TestCase
     {
         final String[][] fixture = new String[][]
         {
-            new String[] { "ejb", "Ejb" },
-            new String[] { "EJB", "EJB" },
-            new String[] { "an EJB class", "AnEJBClass" },
-            new String[] { "an EJB Class", "AnEJBClass" },
-            new String[] { "HibernateEntity", "HibernateEntity" },
-            new String[] { "Hibernate Entity", "HibernateEntity" },
-            new String[] { "Welcome... to the jungle (Guns \'n\' Roses)", "WelcomeToTheJungleGunsNRoses" }
+            new String[]{"ejb", "Ejb"},
+            new String[]{"EJB", "EJB"},
+            new String[]{"an EJB class", "AnEJBClass"},
+            new String[]{"an EJB Class", "AnEJBClass"},
+            new String[]{"HibernateEntity", "HibernateEntity"},
+            new String[]{"Hibernate Entity", "HibernateEntity"},
+            new String[]{"Welcome... to the jungle (Guns \'n\' Roses)", "WelcomeToTheJungleGunsNRoses"}
         };
 
         for (int i = 0; i < fixture.length; i++)
@@ -90,13 +90,13 @@ public class StringUtilsHelperTest extends TestCase
     {
         final String[][] fixture = new String[][]
         {
-            new String[] { "ejb", "ejb" },
-            new String[] { "EJB", "eJB" },
-            new String[] { "an EJB class", "anEJBClass" },
-            new String[] { "an EJB Class", "anEJBClass" },
-            new String[] { "HibernateEntity", "hibernateEntity" },
-            new String[] { "Hibernate Entity", "hibernateEntity" },
-            new String[] { "Welcome... to the jungle (Guns \'n\' Roses)", "welcomeToTheJungleGunsNRoses" }
+            new String[]{"ejb", "ejb"},
+            new String[]{"EJB", "eJB"},
+            new String[]{"an EJB class", "anEJBClass"},
+            new String[]{"an EJB Class", "anEJBClass"},
+            new String[]{"HibernateEntity", "hibernateEntity"},
+            new String[]{"Hibernate Entity", "hibernateEntity"},
+            new String[]{"Welcome... to the jungle (Guns \'n\' Roses)", "welcomeToTheJungleGunsNRoses"}
         };
 
         for (int i = 0; i < fixture.length; i++)
@@ -110,13 +110,13 @@ public class StringUtilsHelperTest extends TestCase
     {
         final String[][] fixture = new String[][]
         {
-            new String[] { "ejb", "ejb" },
-            new String[] { "EJB", "ejb" },
-            new String[] { "an EJB class", "an-ejb-class" },
-            new String[] { "an EJB Class", "an-ejb-class" },
-            new String[] { "HibernateEntity", "hibernate-entity" },
-            new String[] { "Hibernate Entity", "hibernate-entity" },
-            new String[] { "Welcome... to the jungle (Guns \'n\' Roses)", "welcome-to-the-jungle-guns-n-roses" }
+            new String[]{"ejb", "ejb"},
+            new String[]{"EJB", "ejb"},
+            new String[]{"an EJB class", "an-ejb-class"},
+            new String[]{"an EJB Class", "an-ejb-class"},
+            new String[]{"HibernateEntity", "hibernate-entity"},
+            new String[]{"Hibernate Entity", "hibernate-entity"},
+            new String[]{"Welcome... to the jungle (Guns \'n\' Roses)", "welcome-to-the-jungle-guns-n-roses"}
         };
 
         for (int i = 0; i < fixture.length; i++)
@@ -130,12 +130,12 @@ public class StringUtilsHelperTest extends TestCase
     {
         final String[][] fixture = new String[][]
         {
-            new String[] { "ejb", "ejb" },
-            new String[] { "EJB", "ejb" },
-            new String[] { "an EJB class", "an.ejb.class" },
-            new String[] { "an EJB Class", "an.ejb.class" },
-            new String[] { "HibernateEntity", "hibernate.entity" },
-            new String[] { "Hibernate Entity", "hibernate.entity" }
+            new String[]{"ejb", "ejb"},
+            new String[]{"EJB", "ejb"},
+            new String[]{"an EJB class", "an.ejb.class"},
+            new String[]{"an EJB Class", "an.ejb.class"},
+            new String[]{"HibernateEntity", "hibernate.entity"},
+            new String[]{"Hibernate Entity", "hibernate.entity"}
         };
 
         for (int i = 0; i < fixture.length; i++)
@@ -149,12 +149,12 @@ public class StringUtilsHelperTest extends TestCase
     {
         final String[][] fixture = new String[][]
         {
-            new String[] { "ejb", "Ejb" },
-            new String[] { "EJB", "EJB" },
-            new String[] { "an EJB class", "An EJB class" },
-            new String[] { "an EJB Class", "An EJB Class" },
-            new String[] { "HibernateEntity", "Hibernate Entity" },
-            new String[] { "Hibernate Entity", "Hibernate Entity" }
+            new String[]{"ejb", "Ejb"},
+            new String[]{"EJB", "EJB"},
+            new String[]{"an EJB class", "An EJB class"},
+            new String[]{"an EJB Class", "An EJB Class"},
+            new String[]{"HibernateEntity", "Hibernate Entity"},
+            new String[]{"Hibernate Entity", "Hibernate Entity"}
         };
 
         for (int i = 0; i < fixture.length; i++)
@@ -184,36 +184,36 @@ public class StringUtilsHelperTest extends TestCase
     {
         final String[][] fixture = new String[][]
         {
-            new String[] { "a", "an a" },
-            new String[] { "b", "a b" },
-            new String[] { "c", "a c" },
-            new String[] { "d", "a d" },
-            new String[] { "e", "an e" },
-            new String[] { "f", "a f" },
-            new String[] { "g", "a g" },
-            new String[] { "h", "a h" },
-            new String[] { "i", "an i" },
-            new String[] { "j", "a j" },
-            new String[] { "k", "a k" },
-            new String[] { "l", "a l" },
-            new String[] { "m", "a m" },
-            new String[] { "n", "a n" },
-            new String[] { "o", "an o" },
-            new String[] { "p", "a p" },
-            new String[] { "q", "a q" },
-            new String[] { "r", "a r" },
-            new String[] { "s", "a s" },
-            new String[] { "t", "a t" },
-            new String[] { "u", "a u" },
-            new String[] { "v", "a v" },
-            new String[] { "w", "a w" },
-            new String[] { "x", "a x" },
-            new String[] { "y", "a y" },
-            new String[] { "z", "a z" },
-            new String[] { "elephant", "an elephant" },
-            new String[] { "cat", "a cat" },
-            new String[] { "horse", "a horse" },
-            new String[] { "building", "a building" }
+            new String[]{"a", "an a"},
+            new String[]{"b", "a b"},
+            new String[]{"c", "a c"},
+            new String[]{"d", "a d"},
+            new String[]{"e", "an e"},
+            new String[]{"f", "a f"},
+            new String[]{"g", "a g"},
+            new String[]{"h", "a h"},
+            new String[]{"i", "an i"},
+            new String[]{"j", "a j"},
+            new String[]{"k", "a k"},
+            new String[]{"l", "a l"},
+            new String[]{"m", "a m"},
+            new String[]{"n", "a n"},
+            new String[]{"o", "an o"},
+            new String[]{"p", "a p"},
+            new String[]{"q", "a q"},
+            new String[]{"r", "a r"},
+            new String[]{"s", "a s"},
+            new String[]{"t", "a t"},
+            new String[]{"u", "a u"},
+            new String[]{"v", "a v"},
+            new String[]{"w", "a w"},
+            new String[]{"x", "a x"},
+            new String[]{"y", "a y"},
+            new String[]{"z", "a z"},
+            new String[]{"elephant", "an elephant"},
+            new String[]{"cat", "a cat"},
+            new String[]{"horse", "a horse"},
+            new String[]{"building", "a building"}
         };
 
         for (int i = 0; i < fixture.length; i++)
@@ -227,19 +227,19 @@ public class StringUtilsHelperTest extends TestCase
     {
         final String[][] fixture = new String[][]
         {
-            new String[] { "", "" },
-            new String[] { "", "" },
-            new String[] { " ", "" },
-            new String[] { "\n", "" },
-            new String[] { " \t ", "" },
-            new String[] { "null", "null" },
-            new String[] { "\r\ntest \nthis\n", "test this" },
-            new String[] { "word", "word" },
-            new String[] { " horse ", "horse" },
-            new String[] { " clean me   up   ", "clean me up" },
-            new String[] { "\n\n\r\n\n\n   ", "" },
-            new String[] { "This is\na multiline\n\n?", "This is a multiline ?" },
-            new String[] { "This is \na multiline\n\n?", "This is a multiline ?" }
+            new String[]{"", ""},
+            new String[]{"", ""},
+            new String[]{" ", ""},
+            new String[]{"\n", ""},
+            new String[]{" \t ", ""},
+            new String[]{"null", "null"},
+            new String[]{"\r\ntest \nthis\n", "test this"},
+            new String[]{"word", "word"},
+            new String[]{" horse ", "horse"},
+            new String[]{" clean me   up   ", "clean me up"},
+            new String[]{"\n\n\r\n\n\n   ", ""},
+            new String[]{"This is\na multiline\n\n?", "This is a multiline ?"},
+            new String[]{"This is \na multiline\n\n?", "This is a multiline ?"}
         };
 
         for (int i = 0; i < fixture.length; i++)
@@ -248,11 +248,32 @@ public class StringUtilsHelperTest extends TestCase
             assertEquals(StringUtilsHelper.toSingleLine(strings[0]), strings[1]);
         }
     }
-    
+
     public void testRemoveLastOccurence()
     {
         String testString = "This is a test string ending with a comma";
         String someString = testString + ",  ";
         assertEquals(StringUtilsHelper.removeLastOccurrence(someString, ","), testString + "  ");
+    }
+
+    public void testCrunch()
+    {
+/*
+        final String[][] fixture = new String[][]
+        {
+            new String[] { "", "" },
+            new String[] { "               ", " " },
+            new String[] { "AaaaaBbbbbCccccDdddd", "ABCDdddd" },
+            new String[] { "ThisIsAnother long filename", "TIA l f" },
+            new String[] { "this is a file", "t i a file" }
+        };
+
+        for (int i = 0; i < fixture.length; i++)
+        {
+            String[] strings = fixture[i];
+            System.out.println(strings[0]+ "::"+strings[1]+" --> "+StringUtilsHelper.crunch(strings[0], 10));
+            assertEquals(StringUtilsHelper.crunch(strings[0], 10), strings[1]);
+        }
+*/
     }
 }
