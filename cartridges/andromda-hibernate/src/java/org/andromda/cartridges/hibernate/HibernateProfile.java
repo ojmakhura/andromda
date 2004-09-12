2d49260b3c996ab70c2f06beddc668bd45f5dcba
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-09-12 16:22:03 +0000

    support for unique and indexed properties

--- cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/HibernateProfile.java
+++ cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/HibernateProfile.java
@@ -4,20 +4,24 @@ import org.andromda.metafacades.uml.UMLProfile;
 
 /**
  * The Hibernate profile. Contains
- * the profile information (tagged values, and stereotypes) 
+ * the profile information (tagged values, and stereotypes)
  * for the Hibernate cartridge.
- * 
+ *
  * @author Chad Brandon
  */
-public class HibernateProfile extends UMLProfile {
-	
-	/* ----------------- Stereotypes -------------------- */
-	
-	
-	/* ----------------- Tagged Values -------------------- */
-	
-	public static final String TAGGEDVALUE_HIBERNATE_QUERY = "@andromda.hibernate.query";
-    
+public class HibernateProfile extends UMLProfile
+{
+
+    /* ----------------- Stereotypes -------------------- */
+
+
+    /* ----------------- Tagged Values -------------------- */
+
+    public static final String TAGGEDVALUE_HIBERNATE_QUERY = "@andromda.hibernate.query";
+
+    public static final String TAGGEDVALUE_HIBERNATE_INDEX = "@andromda.hibernate.index";
+    public static final String TAGGEDVALUE_HIBERNATE_UNIQUE = "@andromda.hibernate.unique";
+
     public static final String TAGGEDVALUE_EJB_VIEWTYPE = "@andromda.ejb.viewType";
-	
+
 }
