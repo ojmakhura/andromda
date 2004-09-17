1969a7fc88a6739152c4d9e6a0e40a3e5cfcab12
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-09-17 21:43:34 +0000

    updated auto-deploying of samples

--- productbuild/etc/nightly-build/buildAll.sh
+++ productbuild/etc/nightly-build/buildAll.sh
@@ -11,14 +11,12 @@ nice -n 19 ant dist -logger org.apache.tools.ant.listener.MailLogger
 nice -n 19 ant docs -logger org.apache.tools.ant.listener.MailLogger
 
 # deploy the samples to the JBoss server
-set ONLINE_STORE=~/nightly/build/working/andromda-all/samples/online-store/web/target/online-store-sample-web.war
-set ANIMAL_QUIZ=~/nightly/build/working/andromda-all/samples/animal-quiz/app/target/animal-quiz-app-3.0M3-SNAPSHOT.ear
+~/stop_jboss.sh
 
-if [ -f $ONLINE_STORE ] && [ -f $ANIMAL_QUIZ ]; then
-    ~/stop_jboss.sh
-    rm ~/deploy/animal-quiz-*.ear
-    rm ~/deploy/online-store-*.war
-    cp $ONLINE_STORE ~/deploy
-    cp $ANIMAL_QUIZ ~/deploy
-    ~/start_jboss.sh
-fi
+rm ~/deploy/animal-quiz-*.ear
+rm ~/deploy/online-store-*.ear
+
+cp ~/maven/repository/andromda/samples/ears/online-store-app-3.0M3-SNAPSHOT.ear ~/deploy
+cp ~/maven/repository/andromda/samples/ears/animal-quiz-app-3.0M3-SNAPSHOT.ear ~/deploy
+
+~/start_jboss.sh
