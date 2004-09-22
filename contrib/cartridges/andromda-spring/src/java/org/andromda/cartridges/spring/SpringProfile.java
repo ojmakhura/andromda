81acbe11cfe4ffce4939fcafa1c5abd17d322b4f
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-09-22 17:48:15 +0000

    Add ability to specify the hibernateGeneratorClass

--- contrib/cartridges/andromda-spring/src/java/org/andromda/cartridges/spring/SpringProfile.java
+++ contrib/cartridges/andromda-spring/src/java/org/andromda/cartridges/spring/SpringProfile.java
@@ -16,6 +16,14 @@ public class SpringProfile extends UMLProfile {
 	
 	/* ----------------- Tagged Values -------------------- */
 	
+    /**
+     * Stores a hibernate query.
+     */
 	public static final String TAGGEDVALUE_HIBERNATE_QUERY = "@andromda.hibernate.query";	
+    
+    /**
+     * Stores the hibernate generator class.
+     */
+    public static final String TAGGEDVALUE_HIBERNATE_GENERATOR_CLASS = "@andromda.hibernate.generator.class";
 	
 }
