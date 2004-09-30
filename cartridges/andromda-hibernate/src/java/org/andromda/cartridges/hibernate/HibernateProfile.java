547f1f201345aa4555e77ac0d558b297e4af682c
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-09-30 17:53:08 +0000

    refactor index and unique tagged values into a HibernateEntityAttribute

--- cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/HibernateProfile.java
+++ cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/HibernateProfile.java
@@ -17,11 +17,24 @@ public class HibernateProfile extends UMLProfile
 
     /* ----------------- Tagged Values -------------------- */
 
+    /**
+     * Stores a hibernate query string
+     */
     public static final String TAGGEDVALUE_HIBERNATE_QUERY = "@andromda.hibernate.query";
 
+    /**
+     * Stores the name of an index for an hibernate entity attribute.
+     */
     public static final String TAGGEDVALUE_HIBERNATE_INDEX = "@andromda.hibernate.index";
+    
+    /**
+     * Stores whether or not the column is unique.
+     */
     public static final String TAGGEDVALUE_HIBERNATE_UNIQUE = "@andromda.hibernate.unique";
 
+    /**
+     * Stores the viewtype of the Hibernate Session EJB.
+     */
     public static final String TAGGEDVALUE_EJB_VIEWTYPE = "@andromda.ejb.viewType";
 
 }
