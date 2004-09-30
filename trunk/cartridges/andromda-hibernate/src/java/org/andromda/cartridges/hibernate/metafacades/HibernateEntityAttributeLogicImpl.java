9e7756af7d134edd88441ec0035280150722a137
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-09-30 17:53:08 +0000

    refactor index and unique tagged values into a HibernateEntityAttribute

---
+++ trunk/cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/metafacades/HibernateEntityAttributeLogicImpl.java
@@ -0,0 +1,52 @@
+package org.andromda.cartridges.hibernate.metafacades;
+
+import org.andromda.cartridges.hibernate.HibernateProfile;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * MetafacadeLogic implementation for
+ * org.andromda.cartridges.hibernate.metafacades.HibernateEntityAttribute.
+ * 
+ * @see org.andromda.cartridges.hibernate.metafacades.HibernateEntityAttribute
+ */
+public class HibernateEntityAttributeLogicImpl
+    extends HibernateEntityAttributeLogic
+    implements
+    org.andromda.cartridges.hibernate.metafacades.HibernateEntityAttribute
+{
+    // ---------------- constructor -------------------------------
+
+    public HibernateEntityAttributeLogicImpl(
+        Object metaObject,
+        String context)
+    {
+        super(metaObject, context);
+    }
+
+    /**
+     * @see org.andromda.cartridges.hibernate.metafacades.HibernateEntityAttribute#isUnique()
+     */
+    public boolean handleIsUnique()
+    {
+        boolean unique = false;
+        Object value = this
+            .findTaggedValue(HibernateProfile.TAGGEDVALUE_HIBERNATE_UNIQUE);
+        if (value != null)
+        {
+            unique = Boolean.valueOf(StringUtils.trimToEmpty(value.toString()))
+                .booleanValue();
+        }
+        return unique;
+    }
+
+    /**
+     * @see org.andromda.cartridges.hibernate.metafacades.HibernateEntityAttribute#getIndex()
+     */
+    public java.lang.String handleGetIndex()
+    {
+        String index = (String)this
+            .findTaggedValue(HibernateProfile.TAGGEDVALUE_HIBERNATE_INDEX);
+        return index != null ? StringUtils.trimToEmpty(index) : null;
+    }
+
+}
