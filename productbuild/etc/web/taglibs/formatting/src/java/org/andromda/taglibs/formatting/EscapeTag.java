28b144a0c7b42b6a0ceba776a9db87548ec03405
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-10-01 14:58:22 +0000

    no message

---
+++ productbuild/etc/web/taglibs/formatting/src/java/org/andromda/taglibs/formatting/EscapeTag.java
@@ -0,0 +1,56 @@
+package org.andromda.taglibs.formatting;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+import java.io.IOException;
+
+public class EscapeTag extends BodyTagSupport
+{
+    private String language = null;
+
+    public String getLanguage()
+    {
+        return language;
+    }
+
+    public void setLanguage(String language)
+    {
+        this.language = language;
+    }
+
+    public int doAfterBody() throws JspException
+    {
+        final String bodyText = getBodyContent().getString();
+
+        String escapedString = null;
+
+        if ("html".equalsIgnoreCase(language))
+        {
+            escapedString = StringEscapeUtils.escapeHtml(bodyText);
+        }
+        else if ("javascript".equalsIgnoreCase(language))
+        {
+            escapedString = StringEscapeUtils.escapeJavaScript(bodyText);
+        }
+        else if ("java".equalsIgnoreCase(language))
+        {
+            escapedString = StringEscapeUtils.escapeJava(bodyText);
+        }
+        else
+        {
+            escapedString = bodyText;
+        }
+
+        try
+        {
+            this.getPreviousOut().print(escapedString);
+        }
+        catch (IOException e)
+        {
+            throw new JspException("Unable to print out escaped body text");
+        }
+        return super.doAfterBody();
+    }
+}
