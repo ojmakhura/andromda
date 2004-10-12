fabe1e006e8894644091f8c994a147ff1b3d88cd
skulawik <skulawik@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-10-12 13:00:24 +0000

    Fixed the "is"-getter problem with ejb cartridge

--- cartridges/andromda-ejb/src/java/org/andromda/cartridges/ejb/metafacades/EJBEntityAttributeFacadeLogicImpl.java
+++ cartridges/andromda-ejb/src/java/org/andromda/cartridges/ejb/metafacades/EJBEntityAttributeFacadeLogicImpl.java
@@ -1,6 +1,8 @@
 package org.andromda.cartridges.ejb.metafacades;
 
 import org.andromda.cartridges.ejb.EJBProfile;
+import org.apache.commons.lang.StringUtils;
+import org.omg.uml.foundation.core.Attribute;
 
 /**
  * MetafacadeLogic implementation for
@@ -8,26 +10,22 @@ import org.andromda.cartridges.ejb.EJBProfile;
  * 
  * @see org.andromda.cartridges.ejb.metafacades.EJBEntityAttributeFacade
  */
-public class EJBEntityAttributeFacadeLogicImpl
-    extends EJBEntityAttributeFacadeLogic
-    implements org.andromda.cartridges.ejb.metafacades.EJBEntityAttributeFacade
-{
-    // ---------------- constructor -------------------------------
+public class EJBEntityAttributeFacadeLogicImpl extends EJBEntityAttributeFacadeLogic implements
+		org.andromda.cartridges.ejb.metafacades.EJBEntityAttributeFacade {
+	// ---------------- constructor -------------------------------
 
-    public EJBEntityAttributeFacadeLogicImpl(
-        Object metaObject,
-        String context)
-    {
-        super(metaObject, context);
-    }
+	public EJBEntityAttributeFacadeLogicImpl(Object metaObject, String context) {
+		super(metaObject, context);
+	}
 
-    /**
-     * @see org.andromda.cartridges.ejb.metafacades.EJBEntityAttributeFacade#getTransactionType()
-     */
-    public java.lang.String handleGetTransactionType()
-    {
-        return (String)this.findTaggedValue(
-            EJBProfile.TAGGEDVALUE_EJB_TRANSACTION_TYPE,
-            true);
-    }
+	/**
+	 * @see org.andromda.cartridges.ejb.metafacades.EJBEntityAttributeFacade#getTransactionType()
+	 */
+	public java.lang.String handleGetTransactionType() {
+		return (String) this.findTaggedValue(EJBProfile.TAGGEDVALUE_EJB_TRANSACTION_TYPE, true);
+	}
+
+	public java.lang.String getGetterName() {
+		return "get" + StringUtils.capitalize(((Attribute)metaObject).getName());
+	}
 }
