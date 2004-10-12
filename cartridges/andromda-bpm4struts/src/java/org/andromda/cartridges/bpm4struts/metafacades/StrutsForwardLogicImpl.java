ee0be223daf1a664be51540a67a51da2019fb274
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-10-12 15:26:26 +0000

    public handlers have been moved to protected added actions member

--- cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsForwardLogicImpl.java
+++ cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsForwardLogicImpl.java
@@ -2,10 +2,7 @@ package org.andromda.cartridges.bpm4struts.metafacades;
 
 import org.andromda.cartridges.bpm4struts.Bpm4StrutsProfile;
 import org.andromda.core.common.StringUtilsHelper;
-import org.andromda.metafacades.uml.EventFacade;
-import org.andromda.metafacades.uml.GuardFacade;
-import org.andromda.metafacades.uml.PseudostateFacade;
-import org.andromda.metafacades.uml.StateVertexFacade;
+import org.andromda.metafacades.uml.*;
 
 import java.util.*;
 
@@ -31,39 +28,39 @@ public class StrutsForwardLogicImpl
     // concrete business methods that were declared
     // abstract in class StrutsForward ...
 
-    public String handleGetGuardName()
+    protected String handleGetGuardName()
     {
         final GuardFacade guard = getGuard();
         return (guard == null) ? null : guard.getName();
     }
 
-    public boolean handleIsTargettingActionState()
+    protected boolean handleIsTargettingActionState()
     {
         return getTarget() instanceof StrutsActionState;
     }
 
-    public boolean handleIsTargettingFinalState()
+    protected boolean handleIsTargettingFinalState()
     {
         return getTarget() instanceof StrutsFinalState;
     }
 
-    public boolean handleIsTargettingDecisionPoint()
+    protected boolean handleIsTargettingDecisionPoint()
     {
         final StateVertexFacade target = getTarget();
         return target instanceof PseudostateFacade && ((PseudostateFacade) target).isDecisionPoint();
     }
 
-    public boolean handleIsTargettingPage()
+    protected boolean handleIsTargettingPage()
     {
         return getTarget() instanceof StrutsJsp;
     }
 
-    public java.lang.String handleGetForwardName()
+    protected java.lang.String handleGetForwardName()
     {
         return StringUtilsHelper.toResourceMessageKey(resolveName());
     }
 
-    public java.lang.String handleGetForwardPath()
+    protected java.lang.String handleGetForwardPath()
     {
         final StateVertexFacade target = getTarget();
         if (isTargettingPage())
@@ -78,12 +75,12 @@ public class StrutsForwardLogicImpl
             return null;
     }
 
-    public String handleGetActionMethodName()
+    protected String handleGetActionMethodName()
     {
         return StringUtilsHelper.lowerCamelCaseName(resolveName());
     }
 
-    public String handleGetTargetNameKey()
+    protected String handleGetTargetNameKey()
     {
         if (isTargettingPage())
         {
@@ -112,22 +109,22 @@ public class StrutsForwardLogicImpl
         return forwardName;
     }
 
-    public boolean handleIsExitingPage()
+    protected boolean handleIsExitingPage()
     {
         return getSource() instanceof StrutsJsp;
     }
 
-    public boolean handleIsSuccessMessagesPresent()
+    protected boolean handleIsSuccessMessagesPresent()
     {
         return getSuccessMessages().isEmpty() == false;
     }
 
-    public boolean handleIsWarningMessagesPresent()
+    protected boolean handleIsWarningMessagesPresent()
     {
         return getWarningMessages().isEmpty() == false;
     }
 
-    public String handleGetMessageKey()
+    protected String handleGetMessageKey()
     {
         String messageKey = getStrutsActivityGraph().getUseCase().getName();
         return StringUtilsHelper.toResourceMessageKey(messageKey);
@@ -148,17 +145,15 @@ public class StrutsForwardLogicImpl
         return messages;
     }
 
-    public Map handleGetSuccessMessages()
+    protected Map handleGetSuccessMessages()
     {
         return getMessages(getMessageKey() + ".success.", Bpm4StrutsProfile.TAGGED_VALUE_ACTION_SUCCES_MESSAGE);
     }
 
-    public Map handleGetWarningMessages()
+    protected Map handleGetWarningMessages()
     {
         return getMessages(getMessageKey() + ".warning.", Bpm4StrutsProfile.TAGGED_VALUE_ACTION_WARNING_MESSAGE);
     }
-
-    // ------------- relations ------------------
 
     protected java.util.Collection handleGetForwardParameters()
     {
@@ -175,5 +170,48 @@ public class StrutsForwardLogicImpl
     {
         Object graph = getSource().getActivityGraph();
         return (graph instanceof StrutsActivityGraph) ? graph : null;
+    }
+
+    protected Collection handleGetActions()
+    {
+        Collection actions = null;
+
+        if (this instanceof StrutsAction) // @todo this is not so nice because StrutsAction extends StrutsForward, solution would be to override in StrutsAction
+        {
+            actions = Collections.singletonList(this);
+        }
+        else
+        {
+            StateVertexFacade vertex = getSource();
+            if (vertex instanceof StrutsJsp)
+            {
+                StrutsJsp jsp = (StrutsJsp)vertex;
+                actions = jsp.getActions();
+            }
+            else if (vertex instanceof StrutsActionState)
+            {
+                StrutsActionState actionState = (StrutsActionState)vertex;
+                actions = actionState.getContainerActions();
+            }
+            else if (vertex instanceof PseudostateFacade)
+            {
+                PseudostateFacade pseudostate = (PseudostateFacade)vertex;
+                if (pseudostate.isInitialState())
+                {
+                    actions = Collections.EMPTY_LIST;
+                }
+                else
+                {
+                    actions = new ArrayList();
+                    Collection incomingForwards = pseudostate.getIncoming();
+                    for (Iterator forwardIterator = incomingForwards.iterator(); forwardIterator.hasNext();)
+                    {
+                        StrutsForward forward = (StrutsForward) forwardIterator.next();
+                        actions.addAll(forward.getActions());
+                    }
+                }
+            }
+        }
+        return actions;
     }
 }
