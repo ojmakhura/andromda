59b3419ef9c2631f8e0db4e628c9255ce2ff5382
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-10-22 09:01:29 +0000

    added the 'link' input type TAGGED_VALUE is now TAGGEDVALUE

--- trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsProfile.java
+++ trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsProfile.java
@@ -14,57 +14,55 @@ public class Bpm4StrutsProfile
 
     /* ----------------- Tagged Values -------------------- */
 
-    public static final String TAGGED_VALUE_ACTION_TYPE = "@andromda.struts.action.type";
-    public static final String TAGGED_VALUE_ACTION_RESETTABLE = "@andromda.struts.action.resettable";
-    public static final String TAGGED_VALUE_ACTION_SUCCES_MESSAGE = "@andromda.struts.action.success.message";
-    public static final String TAGGED_VALUE_ACTION_WARNING_MESSAGE = "@andromda.struts.action.warning.message";
-    public static final String TAGGED_VALUE_ACTION_TABINDEX = "@andromda.struts.action.tabindex";
+    public static final String TAGGEDVALUE_ACTION_TYPE = "@andromda.struts.action.type";
+    public static final String TAGGEDVALUE_ACTION_RESETTABLE = "@andromda.struts.action.resettable";
+    public static final String TAGGEDVALUE_ACTION_SUCCES_MESSAGE = "@andromda.struts.action.success.message";
+    public static final String TAGGEDVALUE_ACTION_WARNING_MESSAGE = "@andromda.struts.action.warning.message";
+    public static final String TAGGEDVALUE_ACTION_TABINDEX = "@andromda.struts.action.tabindex";
 
-    public static final String TAGGED_VALUE_INPUT_REQUIRED = "@andromda.struts.view.field.required";
-    public static final String TAGGED_VALUE_INPUT_READONLY = "@andromda.struts.view.field.readonly";
-    public static final String TAGGED_VALUE_INPUT_FORMAT = "@andromda.struts.view.field.format";
-    public static final String TAGGED_VALUE_INPUT_TYPE = "@andromda.struts.view.field.type";
-    public static final String TAGGED_VALUE_INPUT_VALIDWHEN = "@andromda.struts.view.field.validwhen";
-    public static final String TAGGED_VALUE_INPUT_TABINDEX = "@andromda.struts.view.field.tabindex";
-    public static final String TAGGED_VALUE_INPUT_CALENDAR = "@andromda.struts.view.field.calendar";
-    public static final String TAGGED_VALUE_INPUT_TABLELINK = "@andromda.struts.view.field.tablelink";
+    public static final String TAGGEDVALUE_INPUT_REQUIRED = "@andromda.struts.view.field.required";
+    public static final String TAGGEDVALUE_INPUT_READONLY = "@andromda.struts.view.field.readonly";
+    public static final String TAGGEDVALUE_INPUT_FORMAT = "@andromda.struts.view.field.format";
+    public static final String TAGGEDVALUE_INPUT_TYPE = "@andromda.struts.view.field.type";
+    public static final String TAGGEDVALUE_INPUT_VALIDWHEN = "@andromda.struts.view.field.validwhen";
+    public static final String TAGGEDVALUE_INPUT_TABINDEX = "@andromda.struts.view.field.tabindex";
+    public static final String TAGGEDVALUE_INPUT_CALENDAR = "@andromda.struts.view.field.calendar";
+    public static final String TAGGEDVALUE_INPUT_TABLELINK = "@andromda.struts.view.field.tablelink";
 
-    public static final String TAGGED_VALUE_TABLE_COLUMNS = "@andromda.struts.view.table.columns";
-    public static final String TAGGED_VALUE_TABLE_MAXROWS = "@andromda.struts.view.table.maxrows";
-    public static final String TAGGED_VALUE_TABLE_EXPORT = "@andromda.struts.view.table.export";
-    public static final String TAGGED_VALUE_TABLE_SORTABLE = "@andromda.struts.view.table.sortable";
+    public static final String TAGGEDVALUE_TABLE_COLUMNS = "@andromda.struts.view.table.columns";
+    public static final String TAGGEDVALUE_TABLE_MAXROWS = "@andromda.struts.view.table.maxrows";
+    public static final String TAGGEDVALUE_TABLE_EXPORT = "@andromda.struts.view.table.export";
+    public static final String TAGGEDVALUE_TABLE_SORTABLE = "@andromda.struts.view.table.sortable";
 
-    public static final String TAGGED_VALUE_EXCEPTION_TYPE = "@andromda.struts.exception.type";
-    public static final String TAGGED_VALUE_CONTROLLER_USE_CASE = "@andromda.struts.controller.usecase";
-    public static final String TAGGED_VALUE_USECASE_ACTIVITY = "@andromda.struts.usecase.activity";
+    public static final String TAGGEDVALUE_EXCEPTION_TYPE = "@andromda.struts.exception.type";
+    public static final String TAGGEDVALUE_CONTROLLER_USE_CASE = "@andromda.struts.controller.usecase";
+    public static final String TAGGEDVALUE_USECASE_ACTIVITY = "@andromda.struts.usecase.activity";
 
     /* ----------------- Default Values ------------------- */
-    public static final String TAGGED_VALUE_INPUT_DEFAULT_REQUIRED = "true";
-    public static final String TAGGED_VALUE_EXCEPTION_DEFAULT_TYPE = "java.lang.Exception";
+    public static final String TAGGEDVALUE_INPUT_DEFAULT_REQUIRED = "true";
+    public static final String TAGGEDVALUE_EXCEPTION_DEFAULT_TYPE = "java.lang.Exception";
 
-    public static final String TAGGED_VALUE_ACTION_TYPE_HYPERLINK = "hyperlink";
-    public static final String TAGGED_VALUE_ACTION_TYPE_FORM = "form";
-    public static final String TAGGED_VALUE_ACTION_DEFAULT_TYPE = TAGGED_VALUE_ACTION_TYPE_FORM;
+    public static final String TAGGEDVALUE_ACTION_TYPE_HYPERLINK = "hyperlink";
+    public static final String TAGGEDVALUE_ACTION_TYPE_FORM = "form";
+    public static final String TAGGEDVALUE_ACTION_DEFAULT_TYPE = TAGGEDVALUE_ACTION_TYPE_FORM;
 
-    public static final String TAGGED_VALUE_INPUT_TYPE_TEXT = "text";
-    public static final String TAGGED_VALUE_INPUT_TYPE_TEXTAREA = "textarea";
-    public static final String TAGGED_VALUE_INPUT_TYPE_RADIO = "radio";
-    public static final String TAGGED_VALUE_INPUT_TYPE_CHECKBOX = "checkbox";
-    public static final String TAGGED_VALUE_INPUT_TYPE_HIDDEN = "hidden";
-    public static final String TAGGED_VALUE_INPUT_TYPE_SELECT = "select";
-    public static final String TAGGED_VALUE_INPUT_TYPE_PASSWORD = "password";
-    public static final String TAGGED_VALUE_INPUT_TYPE_MULTIBOX = "multibox";
-    public static final int TAGGED_VALUE_INPUT_TYPE_OPTION_DEFAULT_COUNT = 3;
-    public static final String TAGGED_VALUE_INPUT_DEFAULT_DATEFORMAT = "dd/MM/yyyy";
+    public static final String TAGGEDVALUE_INPUT_TYPE_TEXT = "text";
+    public static final String TAGGEDVALUE_INPUT_TYPE_TEXTAREA = "textarea";
+    public static final String TAGGEDVALUE_INPUT_TYPE_RADIO = "radio";
+    public static final String TAGGEDVALUE_INPUT_TYPE_CHECKBOX = "checkbox";
+    public static final String TAGGEDVALUE_INPUT_TYPE_HIDDEN = "hidden";
+    public static final String TAGGEDVALUE_INPUT_TYPE_SELECT = "select";
+    public static final String TAGGEDVALUE_INPUT_TYPE_PASSWORD = "password";
+    public static final String TAGGEDVALUE_INPUT_TYPE_MULTIBOX = "multibox";
+    public static final String TAGGEDVALUE_INPUT_TYPE_LINK = "link";
+    public static final int TAGGEDVALUE_INPUT_TYPE_OPTION_DEFAULT_COUNT = 3;
+    public static final String TAGGEDVALUE_INPUT_DEFAULT_DATEFORMAT = "dd/MM/yyyy";
 
-    public static final int TAGGED_VALUE_TABLE_MAXROWS_DEFAULT_COUNT = 15;
-    public static final boolean TAGGED_VALUE_TABLE_SORTABLE_DEFAULT_VALUE = true;
-    public static final boolean TAGGED_VALUE_TABLE_EXPORTABLE_DEFAULT_VALUE = true;
+    public static final int TAGGEDVALUE_TABLE_MAXROWS_DEFAULT_COUNT = 15;
+    public static final boolean TAGGEDVALUE_TABLE_SORTABLE_DEFAULT_VALUE = true;
+    public static final boolean TAGGEDVALUE_TABLE_EXPORTABLE_DEFAULT_VALUE = true;
 
-    public static final String TAGGED_VALUE_HYPERLINK = "hyperlinkModel";
-
-    /* --------------- Runtime properties ---------------- */
-    public static final boolean ENABLE_CACHE = true;
+    public static final String TAGGEDVALUE_HYPERLINK = "hyperlinkModel";
 
     private Bpm4StrutsProfile()
     {
