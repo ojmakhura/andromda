c3861ebac9fc151d45451b2b626d92d3268a61e2
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-10-24 22:27:11 +0000

    remove debug output I accidently checked in

--- trunk/repositories/mdr/src/java/org/andromda/repositories/mdr/MDRXmiReferenceResolverContext.java
+++ trunk/repositories/mdr/src/java/org/andromda/repositories/mdr/MDRXmiReferenceResolverContext.java
@@ -77,7 +77,7 @@ public class MDRXmiReferenceResolverContext
                 String modelUrlAsString = findModuleURL(suffix);
                 if (StringUtils.isNotBlank(modelUrlAsString))
                 {
-                    modelUrl = this.getValidURL(modelUrlAsString);
+                    modelUrl = getValidURL(modelUrlAsString);
                 }
                 if (modelUrl == null)
                 {
@@ -86,15 +86,8 @@ public class MDRXmiReferenceResolverContext
                 }
                 if (modelUrl == null)
                 {
-                    //check the directory on the same level
-                    System.out.println("suffixWithExtension: " + systemId);
-                    modelUrl = this.getValidURL(systemId);
-                }
-                if (modelUrl == null)
-                {
                     // Give up and let superclass deal with it.
-                    modelUrl = super.toURL("jar:file:" + suffix + "!/" + suffixWithExt);
-                    System.out.println("gett the model url!!!!: " +"jar:file:" + suffix + "!/" + suffixWithExt);
+                    modelUrl = super.toURL(systemId);
                 }
             }
             // if we've found the module model, log it
@@ -103,7 +96,6 @@ public class MDRXmiReferenceResolverContext
             if (modelUrl != null)
             {
                 AndroMDALogger.info("Referenced model --> '" + modelUrl + "'");
-                System.out.println("referenced model: " + modelUrl);
                 urlMap.put(suffixWithExt, modelUrl);
             }
         }
