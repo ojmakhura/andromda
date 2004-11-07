f8ce4cf972b998774ec5ec88fed0b45ab8fac415
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-11-07 20:03:21 +0000

    remove possiblity of null pointer occurring

--- trunk/metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/DependencyFacadeLogicImpl.java
+++ trunk/metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/DependencyFacadeLogicImpl.java
@@ -2,35 +2,28 @@ package org.andromda.metafacades.uml14;
 
 import org.apache.commons.lang.StringUtils;
 
-
 /**
- * 
- *
  * Metaclass facade implementation.
- *
  */
 public class DependencyFacadeLogicImpl
-       extends DependencyFacadeLogic
-       implements org.andromda.metafacades.uml.DependencyFacade
+    extends DependencyFacadeLogic
+    implements org.andromda.metafacades.uml.DependencyFacade
 {
     // ---------------- constructor -------------------------------
-    
-    public DependencyFacadeLogicImpl (org.omg.uml.foundation.core.Dependency metaObject, String context)
+
+    public DependencyFacadeLogicImpl(
+        org.omg.uml.foundation.core.Dependency metaObject,
+        String context)
     {
-        super (metaObject, context);
+        super(metaObject, context);
     }
-    
+
     /**
      * Gets the name in the following manner.
      * <ol>
-     *   <li>
-     *     If the dependency has a name return it.
-     *   </li>
-     *   <li>
-     *     If the dependency does <strong>NOT</strong> have
-     *     a name, get the target element's and return its name
-     *     uncapitalized.
-     *   </li>
+     * <li>If the dependency has a name return it.</li>
+     * <li>If the dependency does <strong>NOT </strong> have a name, get the
+     * target element's and return its name uncapitalized.</li>
      * </ol>
      * 
      * @see org.andromda.metafacades.uml.ModelElementFacade#getName()
@@ -38,13 +31,13 @@ public class DependencyFacadeLogicImpl
     public String getName()
     {
         String name = super.getName();
-        if (StringUtils.isBlank(name))
+        if (StringUtils.isBlank(name) && this.getTargetElement() != null)
         {
-           name = StringUtils.uncapitalize(this.getTargetElement().getName());    
+            name = StringUtils.uncapitalize(this.getTargetElement().getName());
         }
         return name;
     }
-    
+
     /**
      * @see org.andromda.metafacades.uml.DependencyFacade#getGetterName()
      */
@@ -60,13 +53,18 @@ public class DependencyFacadeLogicImpl
     {
         return "set" + StringUtils.capitalize(this.getName());
     }
-    
+
     /**
      * @see org.andromda.metafacades.uml.DependencyFacade#getTargetElement()
      */
     public Object handleGetTargetElement()
     {
-        return metaObject.getSupplier().iterator().next();
+        Object targetElement = null;
+        if (!metaObject.getSupplier().isEmpty())
+        {
+            targetElement = metaObject.getSupplier().iterator().next();
+        }
+        return targetElement;
     }
 
 }
