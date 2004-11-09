0e0406949e26d6b8db119fd9d8985e598f013c33
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-11-09 01:10:38 +0000

    add new @andromda.struts.action.redirect tagged value that allows us to define whether or not an action forward should be redirected.  Add tests and docs as well.

--- trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsForwardLogicImpl.java
+++ trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsForwardLogicImpl.java
@@ -1,8 +1,10 @@
 package org.andromda.cartridges.bpm4struts.metafacades;
 
+import org.andromda.cartridges.bpm4struts.Bpm4StrutsGlobals;
 import org.andromda.cartridges.bpm4struts.Bpm4StrutsProfile;
 import org.andromda.core.common.StringUtilsHelper;
 import org.andromda.metafacades.uml.*;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.*;
 
@@ -217,5 +219,21 @@ public class StrutsForwardLogicImpl
                 }
             }
         }
+    }
+
+    /**
+     * @see org.andromda.cartridges.bpm4struts.metafacades.StrutsForward#isRedirect()
+     */
+    public boolean handleIsRedirect()
+    {
+        String redirect = (String)this.getConfiguredProperty(
+            Bpm4StrutsGlobals.PROPERTY_DEFAULT_ACTION_REDIRECT);
+        Object value = this
+            .findTaggedValue(Bpm4StrutsProfile.TAGGEDVALUE_ACTION_REDIRECT);
+        if (value != null)
+        {
+            redirect = (String)value;
+        }
+        return Boolean.valueOf(StringUtils.trimToEmpty(redirect)).booleanValue();
     }
 }
