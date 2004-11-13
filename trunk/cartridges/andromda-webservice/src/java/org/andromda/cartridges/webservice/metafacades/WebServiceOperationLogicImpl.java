d7fb5452fd5702227fd581a82cadbc71c359230b
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-11-13 15:45:57 +0000

    add validation to make sure people are using array types and NOT collection types on their web services.

---
+++ trunk/cartridges/andromda-webservice/src/java/org/andromda/cartridges/webservice/metafacades/WebServiceOperationLogicImpl.java
@@ -0,0 +1,19 @@
+package org.andromda.cartridges.webservice.metafacades;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.webservice.metafacades.WebServiceOperation.
+ *
+ * @see org.andromda.cartridges.webservice.metafacades.WebServiceOperation
+ */
+public class WebServiceOperationLogicImpl
+       extends WebServiceOperationLogic
+       implements org.andromda.cartridges.webservice.metafacades.WebServiceOperation
+{
+    // ---------------- constructor -------------------------------
+
+    public WebServiceOperationLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+}
---
+++ trunk/cartridges/andromda-webservice/src/java/org/andromda/cartridges/webservice/metafacades/WebServiceParameterLogicImpl.java
@@ -0,0 +1,19 @@
+package org.andromda.cartridges.webservice.metafacades;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.webservice.metafacades.WebServiceParameter.
+ *
+ * @see org.andromda.cartridges.webservice.metafacades.WebServiceParameter
+ */
+public class WebServiceParameterLogicImpl
+       extends WebServiceParameterLogic
+       implements org.andromda.cartridges.webservice.metafacades.WebServiceParameter
+{
+    // ---------------- constructor -------------------------------
+
+    public WebServiceParameterLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+}
