ccf36a5ba4363612becc7e90206fa77288809451
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-11-14 20:13:30 +0000

    fix validation (validation was incorrectly validating non exposed operations)

--- trunk/cartridges/andromda-webservice/src/java/org/andromda/cartridges/webservice/metafacades/WebServiceOperationLogicImpl.java
+++ trunk/cartridges/andromda-webservice/src/java/org/andromda/cartridges/webservice/metafacades/WebServiceOperationLogicImpl.java
@@ -1,19 +1,33 @@
 package org.andromda.cartridges.webservice.metafacades;
 
+import org.andromda.metafacades.uml.UMLProfile;
 
 /**
- * MetafacadeLogic implementation for org.andromda.cartridges.webservice.metafacades.WebServiceOperation.
- *
+ * MetafacadeLogic implementation for
+ * org.andromda.cartridges.webservice.metafacades.WebServiceOperation.
+ * 
  * @see org.andromda.cartridges.webservice.metafacades.WebServiceOperation
  */
 public class WebServiceOperationLogicImpl
-       extends WebServiceOperationLogic
-       implements org.andromda.cartridges.webservice.metafacades.WebServiceOperation
+    extends WebServiceOperationLogic
+    implements
+    org.andromda.cartridges.webservice.metafacades.WebServiceOperation
 {
     // ---------------- constructor -------------------------------
 
-    public WebServiceOperationLogicImpl (Object metaObject, String context)
+    public WebServiceOperationLogicImpl(
+        Object metaObject,
+        String context)
     {
-        super (metaObject, context);
+        super(metaObject, context);
+    }
+
+    /**
+     * @see org.andromda.cartridges.webservice.metafacades.WebServiceOperation#isExposed()
+     */
+    protected boolean handleIsExposed()
+    {
+        return this.getOwner().hasStereotype(UMLProfile.STEREOTYPE_WEBSERVICE)
+            || this.hasStereotype(UMLProfile.STEREOTYPE_WEBSERVICE_OPERATION);
     }
 }
