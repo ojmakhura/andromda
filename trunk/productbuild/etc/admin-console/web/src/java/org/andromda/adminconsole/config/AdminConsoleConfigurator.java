1ea2aa98d8cdbe8765adbe1e8e87143f63b9435b
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-11-24 10:12:31 +0000

    added javadoc stabilization added convenience methods

--- trunk/productbuild/etc/admin-console/web/src/java/org/andromda/adminconsole/config/AdminConsoleConfigurator.java
+++ trunk/productbuild/etc/admin-console/web/src/java/org/andromda/adminconsole/config/AdminConsoleConfigurator.java
@@ -16,11 +16,18 @@ public class AdminConsoleConfigurator implements Serializable
     private final WidgetRenderer widgetRenderer = new WidgetRenderer();
 
     private List knownUrls = null;
+    private List knownTables = null;
 
     private final Map tableCache = new HashMap();
     private final Map columnCache = new HashMap();
     //private final Map jspCache = new HashMap(); // @todo: use
 
+    /**
+     * Constructs the configuration by first trying to load the file <code>admin-console.cfg.xml</code>
+     * from the classpath, it that one could not be found the file <code>default.cfg.xml</code> will be loaded.
+     *
+     * @throws Exception when the configuration could not be loaded
+     */
     public AdminConsoleConfigurator() throws Exception
     {
         configuration = loadConfiguration(FILE_NAME);
@@ -34,31 +41,81 @@ public class AdminConsoleConfigurator implements Serializable
         }
     }
 
+    /**
+     * Returns the list of URLs as configured.
+     * @return a List of String instances
+     */
     public List getKnownDatabaseUrls()
     {
         if (knownUrls == null)
         {
+            knownUrls = new ArrayList();
             ConsoleConfiguration consoleConfiguration = configuration.getConsoleConfiguration();
             if (consoleConfiguration != null)
             {
                 DatabaseUrls databaseUrls = consoleConfiguration.getDatabaseUrls();
                 if (databaseUrls != null)
                 {
-                    knownUrls = Arrays.asList(databaseUrls.getUrl());
+                    knownUrls.addAll(Arrays.asList(databaseUrls.getUrl()));
                 }
+            }
+            if (knownUrls == null)
+            {
+                knownUrls = Collections.EMPTY_LIST;
             }
         }
         return knownUrls;
     }
 
+    /**
+     * Returns true if tables that are not configured should also be accessible.
+     */
+    public boolean isUnconfiguredTablesAvailable()
+    {
+        // path cannot throw a NullPointerException, guaranteed by the XML Schema
+        return configuration.getTables().getAllowUnconfigured();
+    }
+
+    /**
+     * Returns true if the user is allowed to enter any arbitrary URL.
+     */
+    public boolean isArbitraryUrlAllowed()
+    {
+        // path cannot throw a NullPointerException, guaranteed by the XML Schema
+        return configuration.getConsoleConfiguration().getDatabaseUrls().getAllowUserSpecified();
+    }
+
+    /**
+     * Returns the list of tables names as configured.
+     *
+     * @return a List of String instances
+     */
+    public List getKnownTableNames()
+    {
+        if (knownTables == null)
+        {
+            knownTables = new ArrayList();
+            TableConfiguration[] tableConfiguration = configuration.getTables().getTableConfiguration();
+            for (int i = 0; i < tableConfiguration.length; i++)
+            {
+                TableConfiguration tableConfig = tableConfiguration[i];
+                knownTables.add(tableConfig.getName());
+            }
+        }
+        return knownTables;
+    }
+
+    /**
+     * Returns the configuration for the argument table.
+     */
     public TableConfiguration getConfiguration(Table table)
     {
         TableConfiguration tableConfiguration = (TableConfiguration) tableCache.get(table);
 
-        if (tableConfiguration == null)
+        if (tableConfiguration == null && configuration.getTables() != null)
         {
             final String tableName = table.getName();
-            TableConfiguration[] tableConfigurations = configuration.getTableConfiguration();
+            TableConfiguration[] tableConfigurations = configuration.getTables().getTableConfiguration();
             for (int i = 0; i < tableConfigurations.length && tableConfiguration==null; i++)
                 if (tableName.equals(tableConfigurations[i].getName()))
                     tableConfiguration = tableConfigurations[i];
@@ -72,6 +129,9 @@ public class AdminConsoleConfigurator implements Serializable
         return tableConfiguration;
     }
 
+    /**
+     * Returns the configuration for the argument column.
+     */
     public ColumnConfiguration getConfiguration(Column column)
     {
         ColumnConfiguration columnConfiguration = (ColumnConfiguration) columnCache.get(column);
@@ -160,9 +220,7 @@ public class AdminConsoleConfigurator implements Serializable
 
                 if (column.isForeignKeyColumn())
                 {
-                    boolean resolveForeignKeys = this.configuration.getConsoleConfiguration().getResolveForeignKeys();
-
-                    if (resolveForeignKeys)
+                    if (configuration.getResolveFk())
                     {
                         ForeignKeyColumn foreignKeyColumn = (ForeignKeyColumn) column;
                         PrimaryKeyColumn primaryKeyColumn = foreignKeyColumn.getImportedKeyColumn();
@@ -223,17 +281,32 @@ public class AdminConsoleConfigurator implements Serializable
         return displayJsp;
     }
 
+    /**
+     * Returns a JSP fragment suitable for an input field for updating the argument column.
+     *
+     * @param custom A custom set of attributes that will be included in the generated fragment.
+     */
     public String getUpdateJsp(Column column, String parameterName, RowData rowData, String custom)
     {
         ColumnConfiguration configuration = getConfiguration(column);
         return getJsp(column, parameterName, rowData.get(column.getName()), !configuration.getUpdateable(), custom);
     }
 
+    /**
+     * Returns a JSP fragment suitable for an input field for inserting the argument column.
+     *
+     * @param custom A custom set of attributes that will be included in the generated fragment.
+     */
     public String getInsertJsp(Column column, String parameterName, String custom)
     {
         return getJsp(column, parameterName, "", false, custom);
     }
 
+    /**
+     * Returns a JSP fragment suitable for an input field for updating the argument column. The value can be set.
+     *
+     * @param custom A custom set of attributes that will be included in the generated fragment.
+     */
     public String getInsertJsp(Column column, String parameterName, Object value, String custom)
     {
         return getJsp(column, parameterName, (value==null)?"":value, false, custom);
@@ -258,6 +331,7 @@ public class AdminConsoleConfigurator implements Serializable
         catch(Exception e)
         {
             // do nothing, let this method silently return
+            e.printStackTrace();
         }
         finally
         {
@@ -270,6 +344,11 @@ public class AdminConsoleConfigurator implements Serializable
             {
                 throw new IOException("Resources could not properly be closed");
             }
+        }
+
+        if (adminConsole != null && adminConsole.isValid() == false)
+        {
+            throw new RuntimeException("Loaded configuration file violates its XML Schema");
         }
         return adminConsole;
     }
