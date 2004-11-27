bff39b50b103580f4a8a7e5d58cac8761553dd83
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-11-27 00:37:54 +0000

    complete rewrite because of the chicken-and-the-egg problem; foreign keys/primary keys in this case (the exported keys from the perspective of the primary key are added in a later step, not recursively)

--- trunk/productbuild/etc/admin-console/core/src/java/org/andromda/adminconsole/db/impl/PrimaryKeyColumnImpl.java
+++ trunk/productbuild/etc/admin-console/core/src/java/org/andromda/adminconsole/db/impl/PrimaryKeyColumnImpl.java
@@ -1,6 +1,5 @@
 package org.andromda.adminconsole.db.impl;
 
-import org.andromda.adminconsole.db.Column;
 import org.andromda.adminconsole.db.ForeignKeyColumn;
 import org.andromda.adminconsole.db.PrimaryKeyColumn;
 import org.andromda.adminconsole.db.Table;
@@ -8,25 +7,17 @@ import org.andromda.adminconsole.db.Table;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class PrimaryKeyColumnImpl extends ColumnImpl implements PrimaryKeyColumn
 {
     private String primaryKeyName = null;
-//    private final List exportedKeyColumns = new ArrayList();
+    private final List exportedKeyColumns = new ArrayList();
 
     public PrimaryKeyColumnImpl(Table table, String name, int sqlType)
     {
         super(table, name, sqlType);
-        this.refresh(true);
-    }
-
-    public PrimaryKeyColumnImpl(Table table, String name, int sqlType, ForeignKeyColumn[] exportedKeyColumns)
-    {
-        super(table, name, sqlType);
-//        this.exportedKeyColumns.addAll(Arrays.asList(exportedKeyColumns));
-        this.refresh(false);
+        this.loadMetaData();
     }
 
     public String getPrimaryKeyName()
@@ -34,98 +25,51 @@ public class PrimaryKeyColumnImpl extends ColumnImpl implements PrimaryKeyColumn
         return primaryKeyName;
     }
 
-/*
     public ForeignKeyColumn[] getExportedKeyColumns()
     {
         return (ForeignKeyColumn[]) exportedKeyColumns.toArray(new ForeignKeyColumn[exportedKeyColumns.size()]);
     }
-*/
 
-    private void refresh(boolean exportedKeys)
+    protected void addExportedKeyColumn(ForeignKeyColumn column)
     {
+        exportedKeyColumns.add(column);
+    }
+
+    private void loadMetaData()
+    {
+        ResultSet primaryKeys = null;
+
+        // PRIMARY KEY NAME
         try
         {
-            ResultSet resultSet = null;
+            // get the primary keys
+            primaryKeys = getMetaData().getPrimaryKeys(getCatalog(), getSchema(), getTable().getName());
 
-            // PRIMARY KEY NAME
-            try
+            String name = getName();
+            while (primaryKeys.next())
             {
-                resultSet = getMetaData().getPrimaryKeys(getCatalog(), getSchema(), getTable().getName());
-
-                String name = getName();
-                while (resultSet.next())
+                // if the column names match, get the primary key name
+                String keyName = primaryKeys.getString("COLUMN_NAME");
+                if (name.equals(keyName))
                 {
-                    String keyName = resultSet.getString("COLUMN_NAME");
-                    if (name.equals(keyName))
-                    {
-                        primaryKeyName = resultSet.getString("PK_NAME");
-                        break;
-                    }
+                    primaryKeyName = primaryKeys.getString("PK_NAME");
+                    // once we find one we can break from this loop
+                    break;
                 }
             }
-            finally
-            { close(resultSet); }
-
-/*
-            // EXPORTED KEY COLUMNS
-            if (exportedKeys)
-            {
-                try
-                {
-                    exportedKeyColumns.clear();
-
-                    Table table = getTable();
-                    resultSet = getMetaData().getExportedKeys(getCatalog(), getSchema(), table.getName());
-
-                    String name = getName();
-                    while (resultSet.next())
-                    {
-                        String keyName = resultSet.getString("PKCOLUMN_NAME");
-                        if (name.equals(keyName))
-                        {
-                            String targetTableName = resultSet.getString("FKTABLE_NAME");
-                            String targetTableColumnName = resultSet.getString("PKCOLUMN_NAME");
-
-                            Table targetTable = getTable().getDatabase().getPool().findTable(targetTableName);
-                            if (targetTable == null)
-                            {
-                                targetTable = new TableImpl(getTable().getDatabase(), targetTableName);
-                            }
-
-                            Column targetColumn = getTable().getDatabase().getPool().findColumn(targetTableName, targetTableColumnName);
-                            if (targetColumn == null)
-                            {
-                                ResultSet columnSet = getMetaData().getColumns(
-                                        getCatalog(), getSchema(), targetTableName, targetTableColumnName);
-                                if (columnSet.next())
-                                {
-                                    int sqlType = columnSet.getInt("DATA_TYPE");
-                                    targetColumn = new ForeignKeyColumnImpl(targetTable, targetTableColumnName, sqlType, this);
-                                }
-                                else
-                                {
-                                    throw new IllegalStateException("An exported key column was found but the " +
-                                            "column could not be loaded: "+targetTableColumnName);
-                                }
-                            }
-                            exportedKeyColumns.add(targetColumn);
-                        }
-                    }
-                }
-                finally
-                { close(resultSet); }
-            }
-*/
         }
         catch (SQLException e)
         {
-            throw new RuntimeException("Unable to refresh table: " + getName());
+            StringBuffer message = new StringBuffer();
+            message.append("Database error while loading metadata for table ");
+            message.append(getTable().getName());
+            message.append(" and column ");
+            message.append(getName());
+            throw new RuntimeException(message.toString(),e);
         }
-    }
-
-    public void refresh()
-    {
-        super.refresh();
-        this.refresh(true);
+        finally
+        {
+            close(primaryKeys);
+        }
     }
 }
