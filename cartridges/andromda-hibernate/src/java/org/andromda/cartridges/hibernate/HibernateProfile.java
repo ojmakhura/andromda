73597e6ffb0753226608bce8c43d69484ef7f411
amartinwest <amartinwest@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-12-06 11:26:54 +0000

    #HIB-6 Add hibernate inheritance support

--- cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/HibernateProfile.java
+++ cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/HibernateProfile.java
@@ -31,4 +31,25 @@ public class HibernateProfile extends UMLProfile
      * Stores the aggregation kind (lazy/eager) of the Hibernate Session EJB.
      */
     public static final String TAGGEDVALUE_HIBERNATE_LAZY = "@andromda.hibernate.lazy";
+
+	/**
+	 * Support for hibernate inheritance strategy, supported values are
+	 * <ul>
+	 * <li>class : one table per base class
+	 * <li>subclass : one table per subclass
+	 * <li>concrete : one table per class, subclasses may implement subclass or joined-subclass
+	 * <li>interface : generate interface and put attributes etc on subclasses
+	 * </ul>
+	 * See Hibernate documentation for specific details. 
+	 */
+	public static final String TAGGEDVALUE_HIBERNATE_INHERITANCE = "@andromda.hibernate.inheritance";
+	/** Value for one Table per root class */
+	public static final String TAGGEDVALUE_HIBERNATE_INHERITANCE_CLASS = "class";
+	/** Value for joined-subclass */
+	public static final String TAGGEDVALUE_HIBERNATE_INHERITANCE_SUBCLASS = "subclass";
+	/** Value for one Table per concrete class */
+	public static final String TAGGEDVALUE_HIBERNATE_INHERITANCE_CONRETE = "concrete";
+	/** Value make Entity an interface, delegate attributes to subclasses. */
+	public static final String TAGGEDVALUE_HIBERNATE_INHERITANCE_INTERFACE = "interface";
+
 }
