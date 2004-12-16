ffbcdd2cff947db32be8e1b74df768b6eb66dfc3
amartinwest <amartinwest@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-12-16 23:11:30 +0000

    Initial set of files

---
+++ trunk/productbuild/etc/cc-build/runcc.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+source set-env.sh
+
+cruisecontrol.sh $*
---
+++ trunk/productbuild/etc/cc-build/set-env.sh
@@ -0,0 +1,24 @@
+#!/bin/sh 
+
+
+if [[ -z $ANT_HOME || ! -d $ANT_HOME ]];
+then
+    export ANT_HOME=/usr/local/jakarta/apache-ant
+fi
+
+if [ ! -d $ANT_HOME ];then
+    echo "*** ANT_HOME:$ANT_HOME not found."
+    exit
+fi
+
+
+export MAVEN_OPTS="-XX:MaxPermSize=128m -Xmx512m"
+
+CVSHOST=cvs.sourceforge.net
+CVSROOTDIR=/cvsroot/andromda
+
+CVSROOT=:ext:${USER}@${CVSHOST}:${CVSROOTDIR}
+CVS_RSH=ssh
+export CVSROOT CVS_RSH
+~
+
