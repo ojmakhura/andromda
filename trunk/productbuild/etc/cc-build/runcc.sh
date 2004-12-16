16af39a73e20924e9e4c0aff73c10fe854e86e69
amartinwest <amartinwest@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-12-16 23:59:24 +0000

    More tweaks for new location.

--- trunk/productbuild/etc/cc-build/runcc.sh
+++ trunk/productbuild/etc/cc-build/runcc.sh
@@ -1,14 +1,40 @@
 #! /bin/sh
 
+me=`whoami`
+if [ $me != $CC_USER ]; then
+  echo "Must be run as $CC_USER"
+  exit
+fi
+
 # Runs a normal CC, expects config.xml
 basename=`basename $0`
 if [ $basename != $0 ]; then
    dirname=`dirname $0`
-else
-   dirname=`pwd`
+   cd $dirname 
 fi
-cd $dirname 
+# Get absolute directory
+dirname=`pwd`
+echo dirname $dirname
 
 source set-env.sh
 
-cruisecontrol.sh $*
+# cd to andromda-all
+cd ../..
+
+# save previous output
+mv --reply=y --backup=t nohup.out nohup.prev
+
+# For some reason cc/maven doesnt create it
+# automagically.
+
+mkdir -p ../logs/andromda-all                                                                    
+
+nohup cruisecontrol.sh -configfile $dirname/cruisecontrol.xml &
+pid=$!
+echo $pid >$dirname/pid
+echo "---------------------------------------"
+echo "Following log, Enter ctl-c to terminate"
+echo "---------------------------------------"
+tail -f nohup.out
+echo Done $0
+
