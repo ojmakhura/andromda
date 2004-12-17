05a840767d259cfab96562ba036c9f1c155f449e
amartinwest <amartinwest@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-12-17 00:01:31 +0000

    More tweaks

--- productbuild/etc/cc-build/run-cc-maven.sh
+++ productbuild/etc/cc-build/run-cc-maven.sh
@@ -1,26 +1,20 @@
 #! /bin/sh
 
-basename=`basename $0`
-if [ $basename != $0 ]; then
-   dirname=`dirname $0`
-else
-   dirname=`pwd`
-fi
-cd $dirname 
-source set-env.sh
-
 me=`whoami`
-                                                                                
 if [ $me != $CC_USER ]; then
   echo "Must be run as $CC_USER"
   exit
 fi
-                                                                                
+                                                                                                                                                                
 basename=`basename $0`
 if [ $basename != $0 ]; then
-  dirname=`dirname $0`
-  cd $dirname
+   dirname=`dirname $0`
+   cd $dirname
 fi
+# Get absolute directory
+dirname=`pwd`
+
+source set-env.sh
                                                                                 
 # save previous output
 mv --reply=y --backup=t nohup.out nohup.prev
