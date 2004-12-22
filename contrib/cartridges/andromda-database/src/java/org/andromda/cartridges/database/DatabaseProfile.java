a4ec17b41c4344d6d61d34100cc62aece9c2af47
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-12-22 17:34:05 +0000

    completely refactored the metamodel still not supported: compound primary keys and hibernate inheritance

---
+++ contrib/cartridges/andromda-database/src/java/org/andromda/cartridges/database/DatabaseProfile.java
@@ -0,0 +1,10 @@
+package org.andromda.cartridges.database;
+
+public final class DatabaseProfile
+{
+    public static final String INITIAL_LOAD_SIZE = "@andromda.persistence.initialload.size";
+    public static final int INITIAL_LOAD_SIZE_DEFAULT = 20;
+
+    public static final String INITIAL_LOAD_REFERENCES = "@andromda.persistence.initialload.references";
+    public static final int INITIAL_LOAD_REFERENCES_DEFAULT = 3;
+}
---
+++ contrib/cartridges/andromda-database/src/java/org/andromda/cartridges/database/metafacades/ForeignKeyColumnLogicImpl.java
@@ -0,0 +1,129 @@
+package org.andromda.cartridges.database.metafacades;
+
+import org.andromda.metafacades.uml.AssociationEndFacade;
+import org.andromda.metafacades.uml.EntityFacade;
+import org.andromda.metafacades.uml.EntityMetafacadeUtils;
+import org.andromda.cartridges.database.DatabaseProfile;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.database.metafacades.ForeignKeyColumn.
+ *
+ * @see org.andromda.cartridges.database.metafacades.ForeignKeyColumn
+ */
+public class ForeignKeyColumnLogicImpl
+       extends ForeignKeyColumnLogic
+       implements org.andromda.cartridges.database.metafacades.ForeignKeyColumn
+{
+    // ---------------- constructor -------------------------------
+
+    public ForeignKeyColumnLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+
+    /**
+     * @see org.andromda.cartridges.database.metafacades.ForeignKeyColumn#isCascadeDelete()
+     */
+    protected boolean handleIsCascadeDelete()
+    {
+        boolean isCascadeDelete = false;
+        isCascadeDelete = this.getOtherEnd().isComposition();
+        AssociationEndFacade otherEnd = this.getOtherEnd();
+        // check for assignable so we don't get class cast exceptions
+        if (otherEnd != null
+            && EntityFacade.class.isAssignableFrom(otherEnd.getType()
+                .getClass()))
+            if (otherEnd.isOne2One()
+                && ((EntityFacade)otherEnd.getType()).isChild())
+            {
+                isCascadeDelete = this.isComposition();
+            }
+        return isCascadeDelete;
+    }
+    
+    /**
+     * @see org.andromda.cartridges.database.metafacades.ForeignKeyColumn#getInitialLoadReferences()
+     */
+    protected int handleGetInitialLoadReferences()
+    {
+        int initialLoadReferences = 0;
+
+        try
+        {
+            String initialLoadReferencesString = (String)findTaggedValue(DatabaseProfile.INITIAL_LOAD_REFERENCES);
+            initialLoadReferences = Integer.parseInt(initialLoadReferencesString);
+        }
+        catch (Exception e)
+        {
+            initialLoadReferences = DatabaseProfile.INITIAL_LOAD_REFERENCES_DEFAULT;
+        }
+
+        return initialLoadReferences;
+    }
+    
+    /**
+     * @see org.andromda.cartridges.database.metafacades.ForeignKeyColumn#getTable()
+     */
+    protected java.lang.Object handleGetTable()
+    {
+        return getOtherEnd().getType();
+    }
+
+    /**
+     * @see org.andromda.cartridges.database.metafacades.ForeignKeyColumn#getImportedColumn()
+     */
+    protected java.lang.Object handleGetImportedTable()
+    {
+        return getType();
+    }
+
+    protected String handleGetInitialLoadValue(int index)
+    {
+        String initialLoadValue = null;
+
+        NonForeignKeyColumn importedColumn = getImportedTable().getPrimaryKeyColumn();
+
+        if (importedColumn != null)
+        {
+            initialLoadValue = importedColumn.getInitialLoadValue(index);
+        }
+
+        return initialLoadValue;
+    }
+
+    protected String handleGetConstraintName()
+    {
+        StringBuffer buffer = new StringBuffer();
+
+        buffer.append("FK");
+        buffer.append(getImportedTable().getTableName());
+        buffer.append(getTable().getTableName());
+
+        return EntityMetafacadeUtils.ensureMaximumNameLength(buffer.toString(), getMaxSqlNameLength());
+    }
+
+    protected String handleGetIndexName()
+    {
+        StringBuffer buffer = new StringBuffer();
+
+        buffer.append("IDX");
+        buffer.append(getImportedTable().getTableName());
+        buffer.append(getTable().getTableName());
+
+        return EntityMetafacadeUtils.ensureMaximumNameLength(buffer.toString(), getMaxSqlNameLength());
+    }
+
+    /**
+     * Gets the maximum name length SQL names may be
+     */
+    private Short getMaxSqlNameLength()
+    {
+        return Short.valueOf((String)this.getConfiguredProperty("maxSqlNameLength"));
+    }
+
+    protected Object handleGetImportedPrimaryKeyColumn()
+    {
+        return getImportedTable().getPrimaryKeyColumn();
+    }
+}
