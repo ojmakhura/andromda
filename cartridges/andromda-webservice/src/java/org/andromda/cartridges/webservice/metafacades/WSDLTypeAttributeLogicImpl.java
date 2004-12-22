31c412c8edd5e9af57a541cad4e697afec3bbdf1
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-12-22 17:01:38 +0000

    refactor out the defaultUse and defaultStyle into namespace properties (some minor code refactoring as well)

--- cartridges/andromda-webservice/src/java/org/andromda/cartridges/webservice/metafacades/WSDLTypeAttributeLogicImpl.java
+++ cartridges/andromda-webservice/src/java/org/andromda/cartridges/webservice/metafacades/WSDLTypeAttributeLogicImpl.java
@@ -1,28 +1,30 @@
 package org.andromda.cartridges.webservice.metafacades;
 
-
 /**
- * MetafacadeLogic implementation for org.andromda.cartridges.webservice.metafacades.WSDLTypeAttribute.
- *
+ * MetafacadeLogic implementation for
+ * org.andromda.cartridges.webservice.metafacades.WSDLTypeAttribute.
+ * 
  * @see org.andromda.cartridges.webservice.metafacades.WSDLTypeAttribute
  */
 public class WSDLTypeAttributeLogicImpl
-       extends WSDLTypeAttributeLogic
-       implements org.andromda.cartridges.webservice.metafacades.WSDLTypeAttribute
+    extends WSDLTypeAttributeLogic
+    implements org.andromda.cartridges.webservice.metafacades.WSDLTypeAttribute
 {
     // ---------------- constructor -------------------------------
 
-    public WSDLTypeAttributeLogicImpl (Object metaObject, String context)
+    public WSDLTypeAttributeLogicImpl(
+        Object metaObject,
+        String context)
     {
-        super (metaObject, context);
+        super(metaObject, context);
     }
 
     /**
      * @see org.andromda.cartridges.webservice.metafacades.WSDLTypeAttribute#isNillable()
      */
-    public boolean handleIsNillable() 
+    public boolean handleIsNillable()
     {
         return !this.isRequired();
     }
-    
+
 }
