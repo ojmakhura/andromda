8c4490604e871579b1ecef1f148bcef712ace8cb
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-12-27 21:28:41 +0000

    move some logic out of the templates for web service tests.

--- trunk/cartridges/andromda-webservice/src/java/org/andromda/cartridges/webservice/metafacades/WebServiceParameterLogicImpl.java
+++ trunk/cartridges/andromda-webservice/src/java/org/andromda/cartridges/webservice/metafacades/WebServiceParameterLogicImpl.java
@@ -1,5 +1,7 @@
 package org.andromda.cartridges.webservice.metafacades;
 
+import org.andromda.metafacades.uml.ClassifierFacade;
+
 /**
  * MetafacadeLogic implementation for
  * org.andromda.cartridges.webservice.metafacades.WebServiceParameter.
@@ -26,5 +28,43 @@ public class WebServiceParameterLogicImpl
     protected boolean handleIsNillable()
     {
         return !this.isRequired();
+    }
+
+    /**
+     * @see org.andromda.cartridges.webservice.metafacades.WebServiceParameter#getTestTypeName()
+     */
+    protected String handleGetTestTypeName()
+    {
+        String testTypeName = null;
+        ClassifierFacade type = this.getType();
+        if (type != null)
+        {
+            if (!type.isDataType()
+                && WSDLType.class.isAssignableFrom(type.getClass()))
+            {
+                WSDLType wsdlType = (WSDLType)type;
+                ClassifierFacade service = this.getOperation().getOwner();
+                if (service != null
+                    && WebService.class.isAssignableFrom(service.getClass()))
+                {
+                    WebService webService = (WebService)service;
+                    if (!webService.isRpcStyle() && wsdlType.isArrayType())
+                    {
+                        testTypeName = webService.getTestPackageName() + '.'
+                            + wsdlType.getArrayName();
+                    }
+                    else
+                    {
+                        testTypeName = webService.getTestPackageName() + '.'
+                            + wsdlType.getName();
+                    }
+                }
+            }
+            else
+            {
+                testTypeName = this.getType().getFullyQualifiedName();
+            }
+        }
+        return testTypeName;
     }
 }
