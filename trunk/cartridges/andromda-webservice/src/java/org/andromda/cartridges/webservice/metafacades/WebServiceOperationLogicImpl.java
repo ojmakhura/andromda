12a8e0f827b22f3edff0d927a65c9f2bdd9c7b3d
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-12-28 05:26:47 +0000

    improve tests, now have an abstract class that handles the axis specific stuff so that its hidden from the impl.

--- trunk/cartridges/andromda-webservice/src/java/org/andromda/cartridges/webservice/metafacades/WebServiceOperationLogicImpl.java
+++ trunk/cartridges/andromda-webservice/src/java/org/andromda/cartridges/webservice/metafacades/WebServiceOperationLogicImpl.java
@@ -1,6 +1,7 @@
 package org.andromda.cartridges.webservice.metafacades;
 
 import org.andromda.metafacades.uml.UMLProfile;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * MetafacadeLogic implementation for
@@ -29,5 +30,74 @@ public class WebServiceOperationLogicImpl
     {
         return this.getOwner().hasStereotype(UMLProfile.STEREOTYPE_WEBSERVICE)
             || this.hasStereotype(UMLProfile.STEREOTYPE_WEBSERVICE_OPERATION);
+    }
+
+    /**
+     * The prefix given to the test implementation operation names.
+     */
+    private static final String TEST_IMPLEMENTATION_OPERATION_NAME_PREFIX = "testImplementationOperationNamePrefix";
+    
+    /**
+     * The prefix given to the junit test operations.
+     */
+    private static final String TEST_NAME_PREFIX = "test";
+    
+    /**
+     * Gets the test implementation operation name prefix.
+     */
+    private String getTestImplementationOperationNamePrefix()
+    {
+        return String.valueOf(this
+            .getConfiguredProperty(TEST_IMPLEMENTATION_OPERATION_NAME_PREFIX));      
+    }
+
+    /**
+     * @see org.andromda.cartridges.webservice.metafacades.WebServiceOperation#getTestImplementationName()
+     */
+    protected String handleGetTestImplementationName()
+    {
+        return this.getTestImplementationOperationNamePrefix()
+            + StringUtils.capitalize(this.getTestName());
+    }
+
+    /**
+     * @see org.andromda.cartridges.webservice.metafacades.WebServiceOperation#getTestImplementationCall()
+     */
+    protected String handleGetTestImplementationCall()
+    {
+        return "this." + this.getTestImplementationSignature();
+    }
+
+    /**
+     * @see org.andromda.cartridges.webservice.metafacades.WebServiceOperation#getTestImplementationSignature()
+     */
+    protected String handleGetTestImplementationSignature()
+    {
+        return this.getTestImplementationOperationNamePrefix() 
+            + StringUtils.capitalize(this.getTestSignature());
+    }
+
+    /**
+     * @see org.andromda.cartridges.webservice.metafacades.WebServiceOperation#getTestName()
+     */
+    protected String handleGetTestName()
+    {
+        return TEST_NAME_PREFIX + StringUtils.capitalize(this.getName());
+    }
+
+    /**
+     * @see org.andromda.cartridges.webservice.metafacades.WebServiceOperation#getTestCall()
+     */
+    protected String handleGetTestCall()
+    {
+        return "this." + this.getSignature();
+    }
+
+    /**
+     * @see org.andromda.cartridges.webservice.metafacades.WebServiceOperation#getTestSignature()
+     */
+    protected String handleGetTestSignature()
+    {
+        return this.getTestName() + "()";
     }
 }
