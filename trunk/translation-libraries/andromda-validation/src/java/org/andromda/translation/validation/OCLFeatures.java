96c2f113192629fa0ef6e255a82687fb5405a817
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2004-12-29 23:45:10 +0000

    add support for "concat" (still needs ability to define quoted strings)

--- trunk/translation-libraries/andromda-validation/src/java/org/andromda/translation/validation/OCLFeatures.java
+++ trunk/translation-libraries/andromda-validation/src/java/org/andromda/translation/validation/OCLFeatures.java
@@ -8,24 +8,35 @@ package org.andromda.translation.validation;
 public class OCLFeatures
 {
     /**
-     * Matches on the <code>allInstances()</code> feature.
+     * Matches on the <code>allInstances</code> feature.
      */
     public static final String ALL_INSTANCES = "(\\s*\\w*\\s*(\\w+|::)*)?\\s*allInstances\\s*\\(\\s*\\)";
 
     /**
-     * Matches on the pattern of a path (i.e. java :: lang :: Integer).
+     * Matches on the pattern of a scope path (i.e. java :: lang :: Integer).
      */
-    private static final String PATH_PATTERN = "(\\w+|\\s*::\\s*)*";
+    private static final String SCOPE_PATH = "(\\w+|\\s*::\\s*)*";
 
     /**
      * Matches on the <code>oclIsKindOf</code> feature.
      */
     public static final String OCL_IS_KIND_OF = "oclIsKindOf\\s*\\(\\s*"
-        + PATH_PATTERN + "\\s*\\)\\s*";
+        + SCOPE_PATH + "\\s*\\)\\s*";
 
     /**
-     * Matches on the <code>.oclIsTypeOf</code> feature.
+     * Matches on the <code>oclIsTypeOf</code> feature.
      */
     public static final String OCL_IS_TYPE_OF = "oclIsTypeOf\\s*\\(\\s*"
-        + PATH_PATTERN + "\\s*\\)\\s*";
+        + SCOPE_PATH + "\\s*\\)\\s*";
+
+    /**
+     * Matches on the pattern of a navigational path (i.e. person.name.first)
+     */
+    private static final String NAVIGATIONAL_PATH = "(\\w+|\\s*\\.\\s*)*";
+
+    /**
+     * Matches on the <code>concat</code> feature.
+     */
+    public static final String CONCAT = "concat\\s*\\(\\s*" + NAVIGATIONAL_PATH
+        + "\\s*\\)\\s*";
 }
