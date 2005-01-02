394d4273f8263d9a7a374a940da38554703d93c1
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-01-02 19:33:13 +0000

    add a test for org.andromda.core.common.Profile

---
+++ trunk/andromda/src/test/java/org/andromda/core/common/ProfileTest.java
@@ -0,0 +1,33 @@
+package org.andromda.core.common;
+
+import java.net.URL;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests the {@link org.andromda.core.common.Profile}
+ * class.
+ * 
+ * @author Chad Brandon
+ */
+public class ProfileTest
+    extends TestCase
+{
+    public void testGetProfileValues()
+    {
+        Namespace namespace = new Namespace();
+        namespace.setName(Namespaces.DEFAULT);
+        URL profileOverrideResource = ProfileTest.class.getResource("/META-INF/profile/andromda-profile-override.xml");
+        assertNotNull(profileOverrideResource);
+        Property property = new Property();
+        property.setName(NamespaceProperties.PROFILE_MAPPINGS_URI);
+        property.setValue(profileOverrideResource.toString());
+        namespace.addProperty(property);
+        Namespaces.instance().addNamespace(namespace);
+        Profile profile = Profile.instance();
+        assertEquals("TestFrom1", profile.get("TEST_FROM_1"));
+        assertEquals("TestFrom2", profile.get("TEST_FROM_2"));
+        assertEquals("TestFrom3", profile.get("TEST_FROM_3"));
+        assertEquals("TestFromOverride", profile.get("TEST_FROM_OVERRIDE"));
+    }
+}
