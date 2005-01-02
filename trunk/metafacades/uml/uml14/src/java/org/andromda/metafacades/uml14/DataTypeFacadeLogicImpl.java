72888949e91bda1b934a6817282f6ac3424f0b6c
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-01-02 01:52:41 +0000

    no message

---
+++ trunk/metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/DataTypeFacadeLogicImpl.java
@@ -0,0 +1,19 @@
+package org.andromda.metafacades.uml14;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.metafacades.uml.DataTypeFacade.
+ *
+ * @see org.andromda.metafacades.uml.DataTypeFacade
+ */
+public class DataTypeFacadeLogicImpl
+       extends DataTypeFacadeLogic
+       implements org.andromda.metafacades.uml.DataTypeFacade
+{
+    // ---------------- constructor -------------------------------
+
+    public DataTypeFacadeLogicImpl (org.omg.uml.foundation.core.DataType metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+}
---
+++ trunk/metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/EnumerationFacadeLogicImpl.java
@@ -0,0 +1,27 @@
+package org.andromda.metafacades.uml14;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.metafacades.uml.EnumerationFacade.
+ *
+ * @see org.andromda.metafacades.uml.EnumerationFacade
+ */
+public class EnumerationFacadeLogicImpl
+       extends EnumerationFacadeLogic
+       implements org.andromda.metafacades.uml.EnumerationFacade
+{
+    // ---------------- constructor -------------------------------
+
+    public EnumerationFacadeLogicImpl (org.omg.uml.foundation.core.Enumeration metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+    /**
+     * @see org.andromda.metafacades.uml.EnumerationFacade#getLiterals()
+     */
+    protected java.util.Collection handleGetLiterals()
+    {
+        return metaObject.getLiteral();
+    }
+
+}
