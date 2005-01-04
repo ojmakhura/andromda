8dfe997b5559376c666dbe3486e91ffd39414691
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-01-04 23:39:21 +0000

    updated to make use of profile mapping file

--- contrib/cartridges/andromda-database/src/java/org/andromda/cartridges/database/DatabaseProfile.java
+++ contrib/cartridges/andromda-database/src/java/org/andromda/cartridges/database/DatabaseProfile.java
@@ -1,10 +1,18 @@
 package org.andromda.cartridges.database;
 
-public final class DatabaseProfile
-{
-    public static final String INITIAL_LOAD_SIZE = "@andromda.persistence.initialload.size";
-    public static final int INITIAL_LOAD_SIZE_DEFAULT = 20;
+import org.andromda.metafacades.uml.UMLProfile;
+import org.andromda.core.common.Profile;
 
-    public static final String INITIAL_LOAD_REFERENCES = "@andromda.persistence.initialload.references";
-    public static final int INITIAL_LOAD_REFERENCES_DEFAULT = 3;
+public final class DatabaseProfile extends UMLProfile
+{
+    /**
+     * The Profile instance from which we retrieve the mapped profile names.
+     */
+    private static final Profile profile = Profile.instance();
+
+    /**
+     * Sets the dummy data load for testing using a database with meaningless data.
+     */
+    public static final String TAGGEDVALUE_DUMMYLOAD_SIZE = profile.get("DATABASE_DUMMYLOAD_SIZE");
+    public static final int DUMMY_LOAD_SIZE_DEFAULT = 20;
 }
