b05d0459b2b8e5d031a5c4e1d36af98bcd3f63b5
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-01-06 16:04:58 +0000

    dropped 1 method

--- trunk/productbuild/etc/admin-console/core/src/java/org/andromda/adminconsole/db/PrimaryKeyColumn.java
+++ trunk/productbuild/etc/admin-console/core/src/java/org/andromda/adminconsole/db/PrimaryKeyColumn.java
@@ -2,7 +2,7 @@ package org.andromda.adminconsole.db;
 
 public interface PrimaryKeyColumn extends Column
 {
+/*
     public String getPrimaryKeyName();
-
-    public ForeignKeyColumn[] getExportedKeyColumns();
+*/
 }
