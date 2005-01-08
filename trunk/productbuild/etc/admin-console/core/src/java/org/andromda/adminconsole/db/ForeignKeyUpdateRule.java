96e37bb65d4f996c4f2cc9419dddfafa1d3cabb2
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-01-08 08:46:57 +0000

    added JavaDoc improved implementation: exception when value does not exist, using actual metadata values internally

--- trunk/productbuild/etc/admin-console/core/src/java/org/andromda/adminconsole/db/ForeignKeyUpdateRule.java
+++ trunk/productbuild/etc/admin-console/core/src/java/org/andromda/adminconsole/db/ForeignKeyUpdateRule.java
@@ -3,20 +3,54 @@ package org.andromda.adminconsole.db;
 import java.sql.DatabaseMetaData;
 import java.io.Serializable;
 
+/**
+ * A foreign key delete rule.
+ */
 public final class ForeignKeyUpdateRule implements Serializable
 {
-    public final static ForeignKeyUpdateRule NOACTION = new ForeignKeyUpdateRule(0);
-    public final static ForeignKeyUpdateRule CASCADE = new ForeignKeyUpdateRule(1);
-    public final static ForeignKeyUpdateRule SETNULL = new ForeignKeyUpdateRule(2);
-    public final static ForeignKeyUpdateRule SETDEFAULT = new ForeignKeyUpdateRule(3);
+    /**
+     * @see java.sql.DatabaseMetaData.importedKeyNoAction
+     */
+    public final static ForeignKeyUpdateRule NOACTION =
+            new ForeignKeyUpdateRule(DatabaseMetaData.importedKeyNoAction);
 
+    /**
+     * @see java.sql.DatabaseMetaData.importedKeyCascade
+     */
+    public final static ForeignKeyUpdateRule CASCADE =
+            new ForeignKeyUpdateRule(DatabaseMetaData.importedKeyCascade);
+
+    /**
+     * @see java.sql.DatabaseMetaData.importedKeySetNull
+     */
+    public final static ForeignKeyUpdateRule
+            SETNULL = new ForeignKeyUpdateRule(DatabaseMetaData.importedKeySetNull);
+
+    /**
+     * @see java.sql.DatabaseMetaData.importedKeySetDefault
+     */
+    public final static ForeignKeyUpdateRule
+            SETDEFAULT = new ForeignKeyUpdateRule(DatabaseMetaData.importedKeySetDefault);
+
+    /**
+     * This enumeration literal's internal value
+     */
     private int value = 0;
 
+    /**
+     * Privately construct a new enumeration literal.
+     *
+     * @param value the internal value for this enumeration literal
+     */
     private ForeignKeyUpdateRule(int value)
     {
         this.value = value;
     }
 
+    /**
+     * @return true if the argument object is also a ForeignKeyUpdateRule instance, and its
+     *  value equals the value of this instance
+     */
     public boolean equals(Object obj)
     {
         if (obj instanceof ForeignKeyUpdateRule)
@@ -26,6 +60,9 @@ public final class ForeignKeyUpdateRule implements Serializable
         return false;
     }
 
+    /**
+     * Given its value, returns the enumeration literal.
+     */
     public static ForeignKeyUpdateRule get(int value)
     {
         switch (value)
@@ -39,10 +76,13 @@ public final class ForeignKeyUpdateRule implements Serializable
             case DatabaseMetaData.importedKeySetDefault:
                 return SETDEFAULT;
             default:
-                return new ForeignKeyUpdateRule(value);
+                throw new RuntimeException("ForeignKeyUpdateRule.get: Argument enumeration literal value unknown");
         }
     }
 
+    /**
+     * @return this enumeration literal's internal value
+     */
     public int getValue()
     {
         return value;
