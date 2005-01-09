3f57ed437cacfe29bfd3750420207b6c8046d220
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-01-09 18:12:33 +0000

    add sortedBy support to OCL translation

---
+++ trunk/translation-libraries/andromda-query/src/java/org/andromda/translation/query/OCLFeatures.java
@@ -0,0 +1,15 @@
+package org.andromda.translation.query;
+
+/**
+ * Contains the patterns matching reserved features of the OCL language.
+ * 
+ * @author Chad Brandon
+ */
+public class OCLFeatures
+{
+    /**
+     * Matches on the <code>allInstances</code> feature.
+     */
+    public static final String ALL_INSTANCES = "allInstances\\s*\\(\\s*\\)";
+
+}
