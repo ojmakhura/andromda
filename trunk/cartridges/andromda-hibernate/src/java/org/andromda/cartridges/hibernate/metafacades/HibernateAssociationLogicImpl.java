4d82ba90b7ba92131848bfa8bdae94edd25ad75a
carloslcuenca <carloslcuenca@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-01-12 02:49:18 +0000

    extend the Association Facade with a HibernateAssociation and add the cache strategy for the association

---
+++ trunk/cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/metafacades/HibernateAssociationLogicImpl.java
@@ -0,0 +1,30 @@
+package org.andromda.cartridges.hibernate.metafacades;
+
+import org.andromda.cartridges.hibernate.HibernateProfile;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.hibernate.metafacades.HibernateAssociation.
+ *
+ * @see org.andromda.cartridges.hibernate.metafacades.HibernateAssociation
+ */
+public class HibernateAssociationLogicImpl
+       extends HibernateAssociationLogic
+       implements org.andromda.cartridges.hibernate.metafacades.HibernateAssociation
+{
+    // ---------------- constructor -------------------------------
+
+    public HibernateAssociationLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+
+    /**
+     * @see org.andromda.cartridges.hibernate.metafacades.HibernateAssociation#getHibernateCacheType()
+     */
+    protected java.lang.String handleGetHibernateCacheType() 
+    {
+        return (String)findTaggedValue(HibernateProfile.TAGGEDVALUE_HIBERNATE_ASSOCIATION_CACHE);
+    }
+    
+}
