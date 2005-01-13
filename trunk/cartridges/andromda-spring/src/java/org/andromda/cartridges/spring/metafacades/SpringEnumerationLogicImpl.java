d1cc9b6217141986f2b2a53aa875810550cd04f9
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-01-13 17:21:27 +0000

    no message

---
+++ trunk/cartridges/andromda-spring/src/java/org/andromda/cartridges/spring/metafacades/SpringEnumerationLogicImpl.java
@@ -0,0 +1,27 @@
+package org.andromda.cartridges.spring.metafacades;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.spring.metafacades.SpringEnumeration.
+ *
+ * @see org.andromda.cartridges.spring.metafacades.SpringEnumeration
+ */
+public class SpringEnumerationLogicImpl
+       extends SpringEnumerationLogic
+       implements org.andromda.cartridges.spring.metafacades.SpringEnumeration
+{
+    // ---------------- constructor -------------------------------
+
+    public SpringEnumerationLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+
+    /**
+     * @see org.andromda.cartridges.spring.metafacades.SpringEnumeration#getFullyQualifiedHibernateType()
+     */
+    protected java.lang.String handleGetFullyQualifiedHibernateType() 
+    {
+        return getFullyQualifiedName();
+    }
+}
