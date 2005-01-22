07f0a9bba089b267f0a2b75c34b213e90a493b99
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-01-22 23:11:33 +0000

    remove unnecessary code

--- cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/metafacades/HibernateGlobals.java
+++ cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/metafacades/HibernateGlobals.java
@@ -18,46 +18,6 @@ class HibernateGlobals
     final static String EJB_IMPLEMENTATION_SUFFIX = "Bean";
 
     /**
-     * DAO implementation class suffix.
-     */
-    final static String DAO_IMPLEMENTATION_SUFFIX = "DaoImpl";
-
-    /**
-     * The DAO class suffix.
-     */
-    final static String DAO_SUFFIX = "Dao";
-
-    /**
-     * The DAO base class suffix.
-     */
-    final static String DAO_BASE_SUFFIX = "DaoBase";
-
-    /**
-     * The service base class suffix.
-     */
-    final static String SERVICE_BASE_SUFFIX = "Base";
-
-    /**
-     * The bean name target suffix
-     */
-    final static String BEAN_NAME_TARGET_SUFFIX = "Target";
-
-    /**
-     * The service web service delegator.
-     */
-    final static String WEB_SERVICE_DELEGATOR_SUFFIX = "WSDelegator";
-
-    /**
-     * The prefix given to transformation method names.
-     */
-    final static String TRANSFORMATION_METHOD_PREFIX = "to";
-
-    /**
-     * Defines the prefix given to the transformation constants.
-     */
-    final static String TRANSFORMATION_CONSTANT_PREFIX = "TRANSFORM_";
-
-    /**
      * Represents the hibernate <code>delete</code> cascade option.
      */
     static final String HIBERNATE_CASCADE_DELETE = "delete";
