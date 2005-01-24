bbb4d28815a9a5c622971e964d97ac4600885b78
amartinwest <amartinwest@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-01-24 22:13:43 +0000

    add/improbe javadocs

--- andromda/src/test/java/org/andromda/core/common/ExceptionRecorderTest.java
+++ andromda/src/test/java/org/andromda/core/common/ExceptionRecorderTest.java
@@ -1,8 +1,6 @@
 /*
  * Created on 24-Jan-2005
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package org.andromda.core.common;
 
@@ -15,41 +13,45 @@ import java.io.IOException;
 import junit.framework.TestCase;
 
 /**
+ * A simple test to check the operation of the ExceptionRecorder.
+ * 
  * @author martin
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
  */
 public class ExceptionRecorderTest extends TestCase
 {
 
     /*
-     * Class under test for String record(String, Throwable, String)
+     * Test that a .exc file is created and it has at least the file header
+     * string.
      */
     public void testRecordStringThrowableString()
     {
-        Exception ex1 = new Exception( "ExceptionRecorder Test");
-        Exception ex2 = new Exception( ex1 );
-        String filename = ExceptionRecorder.record( "Test message", ex2, "test" );
-        File excFile = new File( filename );
-        assertTrue( "exception file not created:" + excFile, excFile.exists());
+        Exception ex1 = new Exception("ExceptionRecorder Test");
+        Exception ex2 = new Exception(ex1);
+        String filename = ExceptionRecorder.record("Test message", ex2, "test");
+        File excFile = new File(filename);
+        assertTrue("exception file not created:" + excFile, excFile.exists());
         FileReader fr = null;
         try
         {
-            fr = new FileReader( excFile );
-            BufferedReader br = new BufferedReader( fr );
+            fr = new FileReader(excFile);
+            BufferedReader br = new BufferedReader(fr);
             String inline;
             inline = br.readLine();
-            assertTrue( "First line not header line", ExceptionRecorder.FILE_HEADER.equals(inline));
+            assertTrue("First line not header line",
+                    ExceptionRecorder.FILE_HEADER.equals(inline));
         } catch (FileNotFoundException e)
         {
             fail(e.getMessage());
         } catch (IOException e)
         {
             fail(e.getMessage());
-        } finally {
+        } finally
+        {
             try
             {
+                // Close the file.
                 fr.close();
             } catch (Exception e)
             {
@@ -57,13 +59,15 @@ public class ExceptionRecorderTest extends TestCase
             }
             try
             {
+                // Clean up since the .exc gets created
+                // in the andromda directory and not a
+                // target directory.
                 excFile.delete();
             } catch (Exception e)
             {
                 // ignore
             }
         }
-}
-
+    }
 
 }
