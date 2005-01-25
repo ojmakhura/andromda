a5ea73109d95744cf6ece9425833e132f11e6910
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-01-25 00:25:42 +0000

    sort the metafacade-impls.properties list so that it doesn't change when the UMLMetafacadeModel is modified.

---
+++ trunk/cartridges/andromda-meta/src/java/org/andromda/cartridges/meta/MetaCartridgeUtils.java
@@ -0,0 +1,61 @@
+package org.andromda.cartridges.meta;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.andromda.metafacades.uml.ModelElementFacade;
+
+/**
+ * Contains utilities for the AndroMDA meta cartridge.
+ * 
+ * @author Chad Brandon
+ */
+public class MetaCartridgeUtils
+{
+
+    /**
+     * Sorts model elements by their fully qualified name.
+     * 
+     * @param modelElements the collection of model elements to sort.
+     * @return the sorted collection.
+     */
+    public Collection sortByFullyQualifiedName(Collection modelElements)
+    {
+        List sortedElements = null;
+        if (modelElements != null)
+        {
+            sortedElements = new ArrayList(modelElements);
+            Collections.sort(
+                sortedElements,
+                new FullyQualifiedNameComparator());
+        }
+        return sortedElements;
+    }
+
+    /**
+     * Used to sort operations by <code>fullyQualifiedName</code>.
+     */
+    private final static class FullyQualifiedNameComparator
+        implements Comparator
+    {
+        private final Collator collator = Collator.getInstance();
+
+        private FullyQualifiedNameComparator()
+        {
+            collator.setStrength(Collator.PRIMARY);
+        }
+
+        public int compare(Object objectA, Object objectB)
+        {
+            ModelElementFacade a = (ModelElementFacade)objectA;
+            ModelElementFacade b = (ModelElementFacade)objectB;
+
+            return collator.compare(a.getFullyQualifiedName(), b
+                .getFullyQualifiedName());
+        }
+    }
+}
