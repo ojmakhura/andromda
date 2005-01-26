9ed6604f666f51f885dd2c56738828b7e15b5211
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-01-26 05:32:50 +0000

    add mapping structure in support of multiple stereotypes and properties (still need to implement)

--- trunk/andromda/src/java/org/andromda/core/metafacade/MetafacadeMappingsUtils.java
+++ trunk/andromda/src/java/org/andromda/core/metafacade/MetafacadeMappingsUtils.java
@@ -1,5 +1,9 @@
 package org.andromda.core.metafacade;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 import org.andromda.core.common.ExceptionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
@@ -16,55 +20,49 @@ class MetafacadeMappingsUtils
 
     /**
      * Constructs the unique key format expected for this mapping. Note that the
-     * only argument required is the <code>object</code>,<code>suffix</code>
+     * only argument required is the <code>object</code>,<code>suffixes</code>
      * is optional.
      * 
      * @param object the begining of the key
-     * @param suffix the suffix to append
+     * @param suffixes a collection of suffixes to append
      * @return the constructed key
      */
-    protected static String constructKey(Object object, String suffix)
+    protected static String constructKey(Object object, Collection suffixes)
     {
         final String methodName = "MetafacadeMapping.constructKey";
         ExceptionUtils.checkNull(methodName, "object", object);
-        Object key = object;
-        suffix = StringUtils.trimToEmpty(suffix);
-        if (StringUtils.isNotEmpty(suffix))
+        StringBuffer key = new StringBuffer(String.valueOf(object));
+        if (suffixes != null)
         {
-            key = appendContext(key, suffix);
+            for (Iterator suffixIterator = suffixes.iterator(); suffixIterator.hasNext();)
+            {
+                key.append(':');
+                key.append(suffixIterator.next());
+            }
         }
-
         if (logger.isDebugEnabled())
             logger.debug("completed '" + methodName + "' with key --> '" + key
                 + "'");
         return key.toString();
     }
-
+    
     /**
-     * Appends a context suffix to the <code>object</code>. The context
-     * suffix is ':' +<code>suffix</code> argument. If suffix is null or an
-     * empty object, then <code>object</code> will just be returned as a
-     * String unchanged.
+     * Constructs the unique key format expected for this mapping. Note that the
+     * only argument required is the <code>object</code>,<code>suffixes</code>
+     * is optional.
      * 
-     * @param context the context which we'll be appending too.
-     * @param suffix the suffix to append.
-     * @return the new context name.
+     * @param object the begining of the key
+     * @param suffix a single suffix to append
+     * @return the constructed key
      */
-    protected static String appendContext(Object object, String context)
+    protected static String constructKey(Object object, String suffix)
     {
-        ExceptionUtils.checkNull(
-            "MetafacadeMappingsUtils.appendContextSuffix",
-            "object",
-            object);
-        String objectAsString = String.valueOf(object);
-        if (StringUtils.isNotEmpty(context))
+        Collection suffixes = null;
+        if (StringUtils.isNotEmpty(suffix))
         {
-            StringBuffer completeContext = new StringBuffer(StringUtils
-                .trimToEmpty(objectAsString));
-            completeContext.append(':' + context);
-            object = completeContext.toString();
+            suffixes = new ArrayList();
+            suffixes.add(suffix);
         }
-        return String.valueOf(object);
+        return constructKey(object, suffixes);
     }
-
 }
