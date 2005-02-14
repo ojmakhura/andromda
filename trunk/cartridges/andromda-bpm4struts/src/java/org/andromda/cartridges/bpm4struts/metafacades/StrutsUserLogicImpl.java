2fdeb3dc78cbc993c3731eaa418af79ff6e9e9ea
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-02-14 12:12:56 +0000

    implemented associatedWithStrutsUseCase property

--- trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsUserLogicImpl.java
+++ trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsUserLogicImpl.java
@@ -1,5 +1,8 @@
 package org.andromda.cartridges.bpm4struts.metafacades;
 
+import org.andromda.metafacades.uml.AssociationEndFacade;
+import org.andromda.metafacades.uml.ClassifierFacade;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -51,5 +54,26 @@ public class StrutsUserLogicImpl
             }
         }
         return parentActors;
+    }
+
+    protected boolean handleIsAssociatedWithStrutsUseCase()
+    {
+        boolean associated = false;
+
+        final Collection associationEnds = getAssociationEnds();
+        for (Iterator iterator = associationEnds.iterator(); iterator.hasNext() && !associated;)
+        {
+            AssociationEndFacade associationEnd = (AssociationEndFacade) iterator.next();
+            ClassifierFacade classifier = associationEnd.getOtherEnd().getType();
+            associated = classifier instanceof StrutsUseCase;
+        }
+
+        // a generalized user is a StrutsUser, and therefore is associated with the StrutsUseCase
+        if (associated == false)
+        {
+            associated = !getGeneralizedUsers().isEmpty();
+        }
+
+        return associated;
     }
 }
