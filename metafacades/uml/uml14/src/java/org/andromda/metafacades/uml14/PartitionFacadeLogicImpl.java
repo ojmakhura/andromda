c20a6619898dacd5067a57b336b7f5c064196327
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-03-19 12:17:21 +0000

    no message

---
+++ metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/PartitionFacadeLogicImpl.java
@@ -0,0 +1,27 @@
+package org.andromda.metafacades.uml14;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.metafacades.uml.PartitionFacade.
+ *
+ * @see org.andromda.metafacades.uml.PartitionFacade
+ */
+public class PartitionFacadeLogicImpl
+    extends PartitionFacadeLogic
+{
+    // ---------------- constructor -------------------------------
+
+    public PartitionFacadeLogicImpl (org.omg.uml.behavioralelements.activitygraphs.Partition metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+
+    /**
+     * @see org.andromda.metafacades.uml.PartitionFacade#getActivityGraph()
+     */
+    protected java.lang.Object handleGetActivityGraph()
+    {
+        return metaObject.getActivityGraph();
+    }
+
+}
---
+++ metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/SubactivityStateFacadeLogicImpl.java
@@ -0,0 +1,34 @@
+package org.andromda.metafacades.uml14;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.metafacades.uml.SubactivityStateFacade.
+ *
+ * @see org.andromda.metafacades.uml.SubactivityStateFacade
+ */
+public class SubactivityStateFacadeLogicImpl
+    extends SubactivityStateFacadeLogic
+{
+    // ---------------- constructor -------------------------------
+
+    public SubactivityStateFacadeLogicImpl (org.omg.uml.behavioralelements.activitygraphs.SubactivityState metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+
+    /**
+     * @see org.andromda.metafacades.uml.SubactivityStateFacade#getSubmachine()
+     */
+    protected java.lang.Object handleGetSubmachine()
+    {
+        return metaObject.getStateMachine();
+    }
+
+    /**
+     * @see org.andromda.metafacades.uml.SubactivityStateFacade#isDynamic()
+     */
+    protected boolean handleIsDynamic()
+    {
+        return metaObject.isDynamic();
+    }
+}
