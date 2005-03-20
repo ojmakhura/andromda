395ffe2be56f7ff744c9ef39a9ada301efe3c1fd
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-03-20 05:58:46 +0000

    Fix HIB-71 (hibernate enumerations now extend the regular java enumerations).

--- trunk/cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/metafacades/HibernateEnumerationLogicImpl.java
+++ trunk/cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/metafacades/HibernateEnumerationLogicImpl.java
@@ -1,5 +1,8 @@
 package org.andromda.cartridges.hibernate.metafacades;
 
+import org.andromda.cartridges.hibernate.HibernateUtils;
+import org.apache.commons.lang.StringUtils;
+
 /**
  * MetafacadeLogic implementation for
  * org.andromda.cartridges.hibernate.metafacades.HibernateEnumeration.
@@ -19,11 +22,44 @@ public class HibernateEnumerationLogicImpl
     }
 
     /**
+     * The pattern to use when constructing the enumeration name.
+     */
+    private static final String ENUMERATION_NAME_PATTERN = "enumerationNamePattern";
+
+    /**
+     * Returns the value of the enumeration name pattern.
+     * 
+     * @return the enumeration name pattern.
+     */
+    private String getEnumerationNamePattern()
+    {
+        return String.valueOf(this
+            .getConfiguredProperty(ENUMERATION_NAME_PATTERN));
+    }
+
+    /**
      * @see org.andromda.cartridges.hibernate.metafacades.HibernateEnumeration#getFullyQualifiedHibernateType()
      */
     protected java.lang.String handleGetFullyQualifiedHibernateType()
     {
-        return getFullyQualifiedName();
+        return super.getFullyQualifiedName();
     }
 
+    /**
+     * @see org.andromda.cartridges.hibernate.metafacades.HibernateEnumeration#getEnumerationName()
+     */
+    protected String handleGetEnumerationName()
+    {
+        return StringUtils.trimToEmpty(this.getEnumerationNamePattern())
+            .replaceAll("\\{0\\}", super.getName());
+    }
+
+    /**
+     * @see org.andromda.cartridges.hibernate.metafacades.HibernateEnumeration#getFullyQualifiedHibernateEnumerationType()
+     */
+    protected String handleGetFullyQualifiedHibernateEnumerationType()
+    {
+        return HibernateMetafacadeUtils.getFullyQualifiedName(this
+            .getPackageName(), this.getEnumerationName(), null);
+    }
 }
