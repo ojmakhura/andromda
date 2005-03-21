d3533f1a737e8732304acbec4ecb04215637747b
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-03-21 23:11:23 +0000

    removed underscore: USE_CASE

--- cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsProfile.java
+++ cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/Bpm4StrutsProfile.java
@@ -51,7 +51,7 @@ public class Bpm4StrutsProfile
     public static final String TAGGEDVALUE_TABLE_DECORATOR = profile.get("TABLE_DECORATOR");
 
     public static final String TAGGEDVALUE_EXCEPTION_TYPE = profile.get("EXCEPTION_TYPE");
-    public static final String TAGGEDVALUE_CONTROLLER_USE_CASE = profile.get("CONTROLLER_USE_CASE");
+    public static final String TAGGEDVALUE_CONTROLLER_USE_CASE = profile.get("CONTROLLER_USECASE");
     public static final String TAGGEDVALUE_USECASE_ACTIVITY = profile.get("USECASE_ACTIVITY");
     public static final String TAGGEDVALUE_ACTION_REDIRECT = profile.get("ACTION_REDIRECT");
 
