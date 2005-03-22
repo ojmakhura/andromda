cc9769bd3a85387e8871baada1a93c69e8d9f67f
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-03-22 22:44:52 +0000

    added handleGetContainerActions

--- trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsPseudostateLogicImpl.java
+++ trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsPseudostateLogicImpl.java
@@ -1,6 +1,13 @@
 package org.andromda.cartridges.bpm4struts.metafacades;
 
 import org.andromda.core.common.StringUtilsHelper;
+import org.andromda.metafacades.uml.ActivityGraphFacade;
+import org.andromda.metafacades.uml.UseCaseFacade;
+import org.andromda.metafacades.uml.TransitionFacade;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 
 
 /**
@@ -32,6 +39,34 @@ public class StrutsPseudostateLogicImpl
         return (methodName == null) ? "a" + System.currentTimeMillis() : StringUtilsHelper.lowerCamelCaseName(methodName);
     }
 
-    // ------------- relations ------------------
+    protected Collection handleGetContainerActions()
+    {
+        Collection actionSet = new HashSet();
+        ActivityGraphFacade activityGraphFacade = this.getActivityGraph();
 
+        if (activityGraphFacade instanceof StrutsActivityGraph)
+        {
+            StrutsActivityGraph activityGraph = (StrutsActivityGraph) activityGraphFacade;
+            UseCaseFacade useCase = activityGraph.getUseCase();
+
+            if (useCase instanceof StrutsUseCase)
+            {
+                Collection actions = ((StrutsUseCase)useCase).getActions();
+                for (Iterator actionIterator = actions.iterator(); actionIterator.hasNext();)
+                {
+                    StrutsAction action = (StrutsAction) actionIterator.next();
+                    Collection transitions = action.getTransitions();
+                    for (Iterator transitionIterator = transitions.iterator(); transitionIterator.hasNext();)
+                    {
+                        TransitionFacade transition = (TransitionFacade) transitionIterator.next();
+                        if (this.equals(transition.getTarget()))
+                        {
+                            actionSet.add(action);
+                        }
+                    }
+                }
+            }
+        }
+        return actionSet;
+    }
 }
