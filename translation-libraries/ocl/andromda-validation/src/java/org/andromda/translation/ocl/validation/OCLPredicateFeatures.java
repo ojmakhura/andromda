3892a2f659ba4f63194fedcff1bd2a4e1cfdecb9
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-03-24 17:47:14 +0000

    Improve translation, move some of the common pattern support into the translation core.

--- translation-libraries/ocl/andromda-validation/src/java/org/andromda/translation/ocl/validation/OCLPredicateFeatures.java
+++ translation-libraries/ocl/andromda-validation/src/java/org/andromda/translation/ocl/validation/OCLPredicateFeatures.java
@@ -6,18 +6,17 @@ import java.util.Collection;
 import org.apache.commons.lang.StringUtils;
 
 /**
- * Contains a single operation {@link #isPredicateFeature(String)
- * that determines if a passed in <code>feature</code> matches
- * the name of a feature that should use a predicate when
- * being translated.
+ * Contains a single operation {@link #isPredicateFeature(String) that
+ * determines if a passed in <code>feature</code> matches the name of a
+ * feature that should use a predicate when being translated.
  * 
  * @author Chad Brandon
  */
 class OCLPredicateFeatures
 {
     /**
-     * Contains the names of feature calls that are
-     * expected to use predicates while being translated.
+     * Contains the names of feature calls that are expected to use predicates
+     * while being translated.
      */
     private static final String[] PREDICATE_FEATURES = new String[]
     {
@@ -28,12 +27,13 @@ class OCLPredicateFeatures
         "any",
         "exists",
     };
-    
-    private static final Collection features = Arrays.asList(PREDICATE_FEATURES);
-    
+
+    private static final Collection features = Arrays
+        .asList(PREDICATE_FEATURES);
+
     /**
-     * Indicates whether or not the passed in <code>feature</code>
-     * is the name of a boolean evaluating feature.
+     * Indicates whether or not the passed in <code>feature</code> is the name
+     * of a boolean evaluating feature.
      * 
      * @param feature
      * @return true/false
