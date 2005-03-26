5dd0c1d642a1bd77eb49a142e8be5adb658c8050
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-03-26 02:58:06 +0000

    fix typo

--- cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/metafacades/HibernateFinderMethodArgumentLogicImpl.java
+++ cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/metafacades/HibernateFinderMethodArgumentLogicImpl.java
@@ -2,7 +2,6 @@ package org.andromda.cartridges.hibernate.metafacades;
 
 import org.andromda.metafacades.uml.ClassifierFacade;
 
-
 /**
  * MetafacadeLogic implementation for
  * org.andromda.cartridges.hibernate.metafacades.HibernateFinderMethodArgument.
@@ -20,47 +19,54 @@ public class HibernateFinderMethodArgumentLogicImpl
     {
         super(metaObject, context);
     }
-    
+
     /**
-     * Defines if specific setters methods will be created for primite types and dates
+     * Defines if specific setters methods will be created for primitive types and
+     * dates
      */
-    private static final String USE_SPECIALIZED_SETTERS="hibernateQueryUseSpecializedSetters";
-    
+    private static final String USE_SPECIALIZED_SETTERS = "hibernateQueryUseSpecializedSetters";
+
     /**
      * @see org.andromda.cartridges.hibernate.metafacades.HibernateFinderMethodArgument#getQueryArgumentNameSetter()
      */
     protected java.lang.String handleGetQueryArgumentNameSetter()
     {
         StringBuffer setterName = new StringBuffer();
-        boolean specializedSetters=Boolean.valueOf(
-                String.valueOf(this.getConfiguredProperty(USE_SPECIALIZED_SETTERS)))
-                .booleanValue();
-        ClassifierFacade classifier=this.getType();
-        if (classifier!=null){
-	        if (specializedSetters)
-	        {
-	            if (classifier.isPrimitive())
-	            {
-	                setterName.append("set" + classifier.getWrapperName().replaceAll("(.)*\\.", ""));
-	            }
-	            else if (classifier.isDateType()
-	                    || classifier.isStringType())
-	            {
-	                setterName.append("set" + classifier.getName());
-	            }
-	            else
-	            {
-	                setterName.append("setParameter");
-	            }
-	        }
-	        else
-	        {
-	                setterName.append("setParameter");
-	        }
-	        if (classifier.isCollectionType())
-	        {
-	            setterName.append("List");
-	        }
+        boolean specializedSetters = Boolean
+            .valueOf(
+                String.valueOf(this
+                    .getConfiguredProperty(USE_SPECIALIZED_SETTERS)))
+            .booleanValue();
+        ClassifierFacade classifier = this.getType();
+        if (classifier != null)
+        {
+            if (specializedSetters)
+            {
+                if (classifier.isPrimitive())
+                {
+                    setterName
+                        .append("set"
+                            + classifier.getWrapperName().replaceAll(
+                                "(.)*\\.",
+                                ""));
+                }
+                else if (classifier.isDateType() || classifier.isStringType())
+                {
+                    setterName.append("set" + classifier.getName());
+                }
+                else
+                {
+                    setterName.append("setParameter");
+                }
+            }
+            else
+            {
+                setterName.append("setParameter");
+            }
+            if (classifier.isCollectionType())
+            {
+                setterName.append("List");
+            }
         }
         return setterName.toString();
     }
