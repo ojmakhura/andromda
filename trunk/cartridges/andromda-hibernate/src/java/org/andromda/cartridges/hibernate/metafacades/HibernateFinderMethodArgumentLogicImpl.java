3a9cb41d3e67cbf8601dc88a4105e4f78a302651
carloslcuenca <carloslcuenca@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-03-26 01:36:05 +0000

    specialized setters for parameters at hibernate finders

--- trunk/cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/metafacades/HibernateFinderMethodArgumentLogicImpl.java
+++ trunk/cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/metafacades/HibernateFinderMethodArgumentLogicImpl.java
@@ -1,5 +1,7 @@
 package org.andromda.cartridges.hibernate.metafacades;
 
+import org.andromda.core.common.StringUtilsHelper;
+
 /**
  * MetafacadeLogic implementation for
  * org.andromda.cartridges.hibernate.metafacades.HibernateFinderMethodArgument.
@@ -17,13 +19,41 @@ public class HibernateFinderMethodArgumentLogicImpl
     {
         super(metaObject, context);
     }
-
+    
+    /**
+     * Defines w
+     */
+    private static final String USE_SPECIALIZED_SETTERS="hibernateQueryUseSpecializedSetters";
+    
     /**
      * @see org.andromda.cartridges.hibernate.metafacades.HibernateFinderMethodArgument#getQueryArgumentNameSetter()
      */
     protected java.lang.String handleGetQueryArgumentNameSetter()
     {
-        StringBuffer setterName = new StringBuffer("setParameter");
+        StringBuffer setterName = new StringBuffer();
+        boolean specializedSetters=Boolean.valueOf(
+                String.valueOf(this.getConfiguredProperty(USE_SPECIALIZED_SETTERS)))
+                .booleanValue();
+        if (specializedSetters)
+        {
+            if (this.getType().isPrimitive())
+            {
+                setterName.append("set" + this.getType().getWrapperName().replaceAll("(.)*\\.", ""));
+            }
+            else if (this.getType().isDateType()
+                    || this.getType().isStringType())
+            {
+                setterName.append("set" + this.getType().getName());
+            }
+            else
+            {
+                setterName.append("setParameter");
+            }
+        }
+        else
+        {
+                setterName.append("setParameter");
+        }
         if (this.getType().isCollectionType())
         {
             setterName.append("List");
