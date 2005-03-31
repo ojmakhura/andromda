bc1834ae914f0ba8c150621fe75f81d2b50f6cb4
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-03-31 04:57:24 +0000

    fix old name

--- metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/EntityQueryOperationLogicImpl.java
+++ metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/EntityQueryOperationLogicImpl.java
@@ -23,7 +23,7 @@ public class EntityQueryOperationLogicImpl
      */
     public java.lang.String handleGetQuery(String translation)
     {
-        final String methodName = "EntityFinderMethodFacadeImpl.getQuery";
+        final String methodName = "EntityQueryOperationLogicImpl.getQuery";
         ExceptionUtils.checkEmpty(methodName, "translation", translation);
         String[] translatedExpressions = this.translateConstraints(
             ExpressionKinds.BODY,
