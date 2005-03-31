e6d2d732c16016d526ab5b2906a2e85520a3e09c
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-03-31 05:19:50 +0000

    minor doc fix

--- trunk/andromda/src/java/org/andromda/core/mapping/Mappings.java
+++ trunk/andromda/src/java/org/andromda/core/mapping/Mappings.java
@@ -17,10 +17,10 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
  * <p>
- * An object responsible for mapping types in the object model to other language
- * identifiers/types. (For example, Java, SQL, Jdbc, etc). The public
- * constructor should NOT be used to construct this instance. An instance of
- * this object should be retrieved through the method getInstance(java.net.URL).
+ * An object responsible for mapping multiple <code>from</code> values to
+ * single <code>to</code>. The public constructor should NOT be used to
+ * construct this instance. An instance of this object should be retrieved
+ * through the method getInstance(java.net.URL).
  * </p>
  * <p>
  * The mappings will change based upon the language, database, etc being used.
