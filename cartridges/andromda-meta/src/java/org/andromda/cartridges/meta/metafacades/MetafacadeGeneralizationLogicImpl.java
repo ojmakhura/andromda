cc38af3da3140376f70d263f3dbf0d1d28059dc8
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-04-16 22:03:13 +0000

    fix super accessor name

--- cartridges/andromda-meta/src/java/org/andromda/cartridges/meta/metafacades/MetafacadeGeneralizationLogicImpl.java
+++ cartridges/andromda-meta/src/java/org/andromda/cartridges/meta/metafacades/MetafacadeGeneralizationLogicImpl.java
@@ -59,7 +59,7 @@ public class MetafacadeGeneralizationLogicImpl
             }
         }
         name = StringUtils.capitalize(name);
-        return ObjectUtils.toString(this.getConfiguredProperty(
+        return "get" + ObjectUtils.toString(this.getConfiguredProperty(
             MetaGlobals.PROPERTY_GENERALIZATION_GETTER_NAME_PATTERN)).replaceAll("\\{0\\}", name);
     }
 
