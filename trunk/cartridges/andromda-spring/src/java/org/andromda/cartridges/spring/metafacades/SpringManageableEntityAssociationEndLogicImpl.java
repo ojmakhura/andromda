0b42774d37d55166f8f8efd31ab7ceaa7e4a5c8d
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-04-17 21:28:19 +0000

    crud updates

--- trunk/cartridges/andromda-spring/src/java/org/andromda/cartridges/spring/metafacades/SpringManageableEntityAssociationEndLogicImpl.java
+++ trunk/cartridges/andromda-spring/src/java/org/andromda/cartridges/spring/metafacades/SpringManageableEntityAssociationEndLogicImpl.java
@@ -1,5 +1,7 @@
 package org.andromda.cartridges.spring.metafacades;
 
+import org.andromda.metafacades.uml.ClassifierFacade;
+
 
 /**
  * MetafacadeLogic implementation for org.andromda.cartridges.spring.metafacades.SpringManageableEntityAssociationEnd.
@@ -16,17 +18,34 @@ public class SpringManageableEntityAssociationEndLogicImpl
         super (metaObject, context);
     }
 
-    /**
-     * @see org.andromda.cartridges.spring.metafacades.SpringManageableEntityAssociationEnd#getDaoGetterName()
-     */
+    protected java.lang.String handleGetDaoName()
+    {
+        return getName() + "Dao";
+    }
+
+    protected java.lang.String handleGetDaoReferenceName()
+    {
+        String referenceName = null;
+
+        final ClassifierFacade type = getType();
+        if (type != null)
+        {
+            final char[] name = getName().toCharArray();
+            if (name.length > 0)
+            {
+                name[0] = Character.toLowerCase(name[0]);
+            }
+            referenceName = new String(name) + "Dao";
+        }
+
+        return referenceName;
+    }
+
     protected java.lang.String handleGetDaoGetterName()
     {
         return getGetterName() + "Dao";
     }
 
-    /**
-     * @see org.andromda.cartridges.spring.metafacades.SpringManageableEntityAssociationEnd#getDaoSetterName()
-     */
     protected java.lang.String handleGetDaoSetterName()
     {
         return getSetterName() + "Dao";
