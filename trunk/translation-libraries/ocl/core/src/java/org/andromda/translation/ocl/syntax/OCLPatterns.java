281164734512e3cc1c7eab853d595bc9f8e3b3a9
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-04-23 18:44:26 +0000

    improve handling of a not expression

--- trunk/translation-libraries/ocl/core/src/java/org/andromda/translation/ocl/syntax/OCLPatterns.java
+++ trunk/translation-libraries/ocl/core/src/java/org/andromda/translation/ocl/syntax/OCLPatterns.java
@@ -12,12 +12,12 @@ public class OCLPatterns
     /**
      * Matches on the pattern of a scope path (i.e. java :: lang :: Integer).
      */
-    public static final String SCOPE_PATH = "(\\w+|\\s*::\\s*)*";
+    static final String SCOPE_PATH = "[\\w+|::]+";
 
     /**
      * Matches on the pattern of a navigational path (i.e. person.name.first)
      */
-    public static final String NAVIGATIONAL_PATH = "(\\w+|\\s*\\.\\s*)+";
+    static final String NAVIGATIONAL_PATH = "[\\w*[\\.]*]+";
 
     /**
      * Indicates if this <code>expression</code> is an operation.
@@ -56,6 +56,35 @@ public class OCLPatterns
     {
         return TranslationUtils.deleteWhitespace(expression).matches(COLLECTION_CALL_RESULT_NAVIGATIONAL_PATH);
     }
+    
+    /**
+     * Indicates if this <code>expression</code> is a navigational path
+     * (some.path)
+     *
+     * @param expression the expression to match.
+     * @return true/false
+     */
+    public static boolean isNavigationalPath(Object expression)
+    {
+        return TranslationUtils.deleteWhitespace(expression).matches(NAVIGATIONAL_PATH);
+    }
 
     private static final String COLLECTION_CALL_RESULT_NAVIGATIONAL_PATH = ".+->" + OPERATION + NAVIGATIONAL_PATH;
+    
+    /**
+     * Pattern for <em>and</em> and <em>or</em> expression matching.
+     */
+    private static final String AND_OR_OR_OPERATOR = "or\\s+.*|and\\s+.*";
+    
+    /**
+     * Indicates if this <code>expression</code> is an <em>and</em>
+     * or <em>or</em> expression (or some.path, and some.path, etc)
+     *
+     * @param expression the expression to match.
+     * @return true/false
+     */
+    public static boolean isAndOrOrExpression(Object expression)
+    {
+        return TranslationUtils.trimToEmpty(expression).matches(AND_OR_OR_OPERATOR);
+    }
 }
