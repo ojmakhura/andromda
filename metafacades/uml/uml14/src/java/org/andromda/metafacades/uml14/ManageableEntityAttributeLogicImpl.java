0f7432c49ce7c3fa1269b57b129495661b6ee375
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-04-29 16:10:58 +0000

    added manageableIdDisplayStrategy, moved .display from bpm

--- metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/ManageableEntityAttributeLogicImpl.java
+++ metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/ManageableEntityAttributeLogicImpl.java
@@ -1,5 +1,9 @@
 package org.andromda.metafacades.uml14;
 
+import org.andromda.metafacades.uml.Entity;
+import org.andromda.metafacades.uml.UMLMetafacadeProperties;
+import org.apache.commons.lang.StringUtils;
+
 
 /**
  * MetafacadeLogic implementation for org.andromda.metafacades.uml.ManageableEntityAttribute.
@@ -7,13 +11,13 @@ package org.andromda.metafacades.uml14;
  * @see org.andromda.metafacades.uml.ManageableEntityAttribute
  */
 public class ManageableEntityAttributeLogicImpl
-    extends ManageableEntityAttributeLogic
+        extends ManageableEntityAttributeLogic
 {
     // ---------------- constructor -------------------------------
 
-    public ManageableEntityAttributeLogicImpl (Object metaObject, String context)
+    public ManageableEntityAttributeLogicImpl(Object metaObject, String context)
     {
-        super (metaObject, context);
+        super(metaObject, context);
     }
 
     /**
@@ -38,6 +42,39 @@ public class ManageableEntityAttributeLogicImpl
     protected java.lang.String handleGetManageableSetterName()
     {
         return getSetterName();
+    }
+
+    /**
+     * @see org.andromda.metafacades.uml.ManageableEntityAttribute#isDisplay()
+     */
+    protected boolean handleIsDisplay()
+    {
+        boolean display = true;
+
+        // only identifiers might be hidden
+        if (isIdentifier())
+        {
+            final String displayStrategy = StringUtils.trimToNull((String)getConfiguredProperty(
+                    UMLMetafacadeProperties.MANAGEABLE_ID_DISPLAY_STRATEGY));
+            // never display identifiers
+            if ("never".equalsIgnoreCase(displayStrategy))
+            {
+                display = false;
+            }
+            // always display identifiers
+            else if ("always".equalsIgnoreCase(displayStrategy))
+            {
+                display = true;
+            }
+            // only display identifiers when explicitely modeled
+            else // if ("auto".equalsIgnoreCase(displayStrategy))
+            {
+                // if the owning entity has its identifiers generated then we assume this must be (one of them)/(it)
+                display = !((Entity)getOwner()).isDynamicIdentifiersPresent();
+            }
+        }
+
+        return display;
     }
 
 }
