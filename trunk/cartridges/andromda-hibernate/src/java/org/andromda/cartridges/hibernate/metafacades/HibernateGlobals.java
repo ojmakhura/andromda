5868bdfd7bbb99ba9d73228ed4ec1601ba090f4c
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-05-05 16:11:06 +0000

    add ability to define whether specific collection interfaces shall be used or default when defining the association end types.

--- trunk/cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/metafacades/HibernateGlobals.java
+++ trunk/cartridges/andromda-hibernate/src/java/org/andromda/cartridges/hibernate/metafacades/HibernateGlobals.java
@@ -126,4 +126,17 @@ class HibernateGlobals
      * The 'bag' type implementation to use.
      */
     static final String BAG_TYPE_IMPLEMENTATION = "bagTypeImplementation";
+    
+    /**
+     * A flag indicating whether or not specific (java.util.Set, java.util.List, 
+     * etc) collection interfaces should be used in assocation mutators and accessors
+     * or whether the generic java.util.Collection interface should be used.
+     */
+    static final String SPECIFIC_COLLECTION_INTERFACES = "specificCollectionInterfaces";
+    
+    /**
+     * The property that defines the default collection interface, this is the interface
+     * used if the property defined by {@link #SPECIFIC_COLLECTION_INTERFACES} is true.
+     */
+    static final String DEFAULT_COLLECTION_INTERFACE = "defaultCollectionInterface";
 }
