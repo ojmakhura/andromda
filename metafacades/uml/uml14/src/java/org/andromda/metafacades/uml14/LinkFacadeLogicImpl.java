2133bb03d553bccaee44ea4ca8f8e02e2d3cbe02
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-05-13 18:10:20 +0000

    UMLMETA-34: added LinkFacade, LinkEndFacade and InstanceFacade

---
+++ metafacades/uml/uml14/src/java/org/andromda/metafacades/uml14/LinkFacadeLogicImpl.java
@@ -0,0 +1,16 @@
+package org.andromda.metafacades.uml14;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.metafacades.uml.LinkFacade.
+ *
+ * @see org.andromda.metafacades.uml.LinkFacade
+ */
+public class LinkFacadeLogicImpl
+    extends LinkFacadeLogic
+{
+    public LinkFacadeLogicImpl (org.omg.uml.behavioralelements.commonbehavior.Link metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+}
