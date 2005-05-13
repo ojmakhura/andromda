2958715e68641ef49cc6994921a3df4bfed34c94
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-05-13 18:21:05 +0000

    fix CORE-46 (history files written on a user basis)

--- andromda/src/java/org/andromda/core/templateengine/TemplateEngine.java
+++ andromda/src/java/org/andromda/core/templateengine/TemplateEngine.java
@@ -1,38 +1,43 @@
 package org.andromda.core.templateengine;
 
 import java.io.Writer;
+
 import java.util.List;
 import java.util.Map;
+
 
 /**
  * The interface that all templates engines used within AndroMDA must implement.
  * It allows us to plug-in the template engine to use for processing of
  * templates used by the system.
- * 
+ *
  * @author Chad Brandon
  */
 public interface TemplateEngine
 {
-
     /**
      * Initializes the TempateEngine.
-     * 
+     *
      * @param namespace The name of the plugin this can be used for whatever the
      *        template engine implementation likes. For example, it can help
      *        determine the name of the log file to which output is logged.
      */
-    public void init(String namespace) throws Exception;
+    public void init(String namespace)
+        throws Exception;
 
     /**
      * Processes a template.
-     * 
+     *
      * @param templateFile the path to the template file that will be processed.
      * @param templateObjects any additional objects we wish to make available
      *        to the translation template that is processed
      * @param output the Writer to which to write the output of the processing.
      * @throws Exception any exception that may occur
      */
-    public void processTemplate(String templateFile, Map templateObjects, Writer output)
+    public void processTemplate(
+        String templateFile,
+        Map templateObjects,
+        Writer output)
         throws Exception;
 
     /**
@@ -43,14 +48,14 @@ public interface TemplateEngine
 
     /**
      * Returns the list of macro libraries used within this template engine.
-     * 
+     *
      * @return List the list of macros
      */
     public List getMacroLibraries();
 
     /**
      * Adds a a macro library for use within this template engine.
-     * 
+     *
      * @param macroLibrary
      */
     public void addMacroLibrary(String macroLibrary);
@@ -61,7 +66,7 @@ public interface TemplateEngine
      * location. This allows the ability to define templates external to plugins
      * so that these templates can override plugin templates in order to provide
      * customization.
-     * 
+     *
      * @param the location of the merge files.
      */
     public void setMergeLocation(String mergeLocation);
@@ -69,7 +74,7 @@ public interface TemplateEngine
     /**
      * Evaluates the <code>expression</code> contained within the template
      * being processed and returns the result.
-     * 
+     *
      * @return the evaluated expression String.
      */
     public String getEvaluatedExpression(String expression);
