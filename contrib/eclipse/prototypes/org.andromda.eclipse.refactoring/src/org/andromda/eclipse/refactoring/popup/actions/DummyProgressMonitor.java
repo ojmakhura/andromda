e24907f039d1e75000a4533bba1585de70590b63
mattes3 <mattes3@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-05-19 16:06:04 +0000

    Prototype for automatic refactoring of Java elements in Eclipse.

---
+++ contrib/eclipse/prototypes/org.andromda.eclipse.refactoring/src/org/andromda/eclipse/refactoring/popup/actions/DummyProgressMonitor.java
@@ -0,0 +1,57 @@
+package org.andromda.eclipse.refactoring.popup.actions;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class DummyProgressMonitor
+        implements IProgressMonitor
+{
+
+    public void beginTask(String name, int totalWork)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void done()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void internalWorked(double work)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public boolean isCanceled()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public void setCanceled(boolean value)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setTaskName(String name)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void subTask(String name)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void worked(int work)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
