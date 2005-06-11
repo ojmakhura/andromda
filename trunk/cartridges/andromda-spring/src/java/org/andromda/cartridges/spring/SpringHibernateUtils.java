f421bda1cffca174aec990314cc93a2c719ddfd5
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-06-11 22:42:48 +0000

    parameterize the hibernate package names so that we can move between hibernate 2 and hibernate 3

---
+++ trunk/cartridges/andromda-spring/src/java/org/andromda/cartridges/spring/SpringHibernateUtils.java
@@ -0,0 +1,75 @@
+package org.andromda.cartridges.spring;
+
+
+/**
+ * Contains utilities used within the Spring cartridge
+ * when dealing with Hibernate.
+ *
+ * @author Chad Brandon
+ */
+public class SpringHibernateUtils
+{
+    /**
+     * The version of Hibernate we're generating for.
+     */
+    private String hibernateVersion = "3";
+
+    /**
+     * Sets the version of Hibernate we're generating for.
+     *
+     * @param hibernateVersion the Hibernate version.
+     */
+    public void setHibernateVersion(final String hibernateVersion)
+    {
+        this.hibernateVersion = hibernateVersion;
+    }
+
+    /**
+     * The Hibernate 2 version number (for determining the
+     * correct package).
+     */
+    private static final String VERSION_2 = "2";
+
+    /**
+     * Gets the appropriate hibernate package name for the given
+     * <code>version</code>.
+     * @param hibernateVersion the version of hibernate for which to retrieve
+     *        the package (version 2 or 3 are valid), default is the
+     *        package for hibernate 3.
+     * @return the base package name.
+     */
+    public String getBasePackage()
+    {
+        String packageName = "org.hibernate";
+        if (VERSION_2.equals(hibernateVersion))
+        {
+            packageName = "net.sf.hibernate";
+        }
+        return packageName;
+    }
+
+    /**
+     * Gets the appropriate hibernate criterion package name for the given <code>version</code>.
+     *
+     * @param hibernateVersion the version of hibernate for which to retrieve
+     *        the package (version 2 or 3 are valid), default is the
+     *        package for hibernate 3.
+     * @return the Hibernate criterion package name.
+     */
+    public String getCriterionPackage()
+    {
+        StringBuffer packageName = new StringBuffer();
+        if (VERSION_2.equals(hibernateVersion))
+        {
+            packageName.append(".expression");
+        }
+        else
+        {
+            packageName.append(".criterion");
+        }
+        packageName.insert(
+            0,
+            this.getBasePackage());
+        return packageName.toString();
+    }
+}
