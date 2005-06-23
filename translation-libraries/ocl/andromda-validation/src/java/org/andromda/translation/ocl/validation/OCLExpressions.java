ab91ca6fe1c0f48eea25e0b508d8baabec9cf637
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-06-23 16:20:49 +0000

    minor improvements

--- translation-libraries/ocl/andromda-validation/src/java/org/andromda/translation/ocl/validation/OCLExpressions.java
+++ translation-libraries/ocl/andromda-validation/src/java/org/andromda/translation/ocl/validation/OCLExpressions.java
@@ -2,303 +2,414 @@ package org.andromda.translation.ocl.validation;
 
 import org.apache.commons.lang.BooleanUtils;
 
+
 /**
  * Used to translate OCL expressions to their corresponding Java expressions.
  */
 public final class OCLExpressions
 {
-    public static boolean equal(Object first, Object second)
+    public static boolean equal(
+        final Object first,
+        final Object second)
     {
         return (first == null) ? (second == null) : first.equals(second);
     }
 
-    public static boolean equal(boolean first, boolean second)
+    public static boolean equal(
+        final boolean first,
+        final boolean second)
     {
         return first == second;
     }
 
-    public static boolean equal(int first, int second)
+    public static boolean equal(
+        final int first,
+        final int second)
     {
         return first == second;
     }
 
-    public static boolean equal(short first, short second)
+    public static boolean equal(
+        final short first,
+        final short second)
     {
         return first == second;
     }
 
-    public static boolean equal(byte first, byte second)
+    public static boolean equal(
+        final byte first,
+        final byte second)
     {
         return first == second;
     }
 
-    public static boolean equal(char first, char second)
+    public static boolean equal(
+        final char first,
+        final char second)
     {
         return first == second;
     }
 
-    public static boolean equal(float first, float second)
+    public static boolean equal(
+        final float first,
+        final float second)
     {
         return first == second;
     }
 
-    public static boolean equal(double first, double second)
+    public static boolean equal(
+        final double first,
+        final double second)
     {
         return first == second;
     }
 
-    public static boolean equal(long first, long second)
+    public static boolean equal(
+        final long first,
+        final long second)
     {
         return first == second;
     }
 
-    public static boolean equal(Object first, boolean second)
+    public static boolean equal(
+        final Object first,
+        final boolean second)
     {
-        return (second) ?
-                Boolean.TRUE.equals(first) || BooleanUtils.toBoolean(String.valueOf(first)) :
-                Boolean.FALSE.equals(first) || !BooleanUtils.toBoolean(String.valueOf(first));
+        return (second) ? Boolean.TRUE.equals(first) || BooleanUtils.toBoolean(String.valueOf(first))
+                        : Boolean.FALSE.equals(first) || !BooleanUtils.toBoolean(String.valueOf(first));
     }
 
-    public static boolean equal(boolean first, Object second)
+    public static boolean equal(
+        final boolean first,
+        final Object second)
     {
         return equal(second, first);
     }
 
-    public static boolean notEqual(Object first, Object second)
+    public static boolean notEqual(
+        final Object first,
+        final Object second)
     {
-        return (first == null) ? (second != null) : !first.equals(second);
+        return first == null ? second != null : !first.equals(second);
     }
 
-    public static boolean notEqual(boolean first, boolean second)
+    public static boolean notEqual(
+        final boolean first,
+        final boolean second)
     {
         return first != second;
     }
 
-    public static boolean equal(Object first, long second)
+    public static boolean equal(
+        final Object first,
+        final long second)
     {
-        boolean equal = first != null && Comparable.class.isAssignableFrom(first.getClass());
+        boolean equal = first instanceof Comparable;
         if (equal)
         {
-            Comparable comparable = (Comparable)first;
+            final Comparable comparable = (Comparable)first;
             equal = comparable.equals(new Long(second));
         }
         return equal;
     }
 
-    public static boolean notEqual(int first, int second)
+    public static boolean notEqual(
+        final int first,
+        final int second)
     {
         return first != second;
     }
 
-    public static boolean notEqual(short first, short second)
+    public static boolean notEqual(
+        final short first,
+        final short second)
     {
         return first != second;
     }
 
-    public static boolean notEqual(byte first, byte second)
+    public static boolean notEqual(
+        final byte first,
+        final byte second)
     {
         return first != second;
     }
 
-    public static boolean notEqual(char first, char second)
+    public static boolean notEqual(
+        final char first,
+        final char second)
     {
         return first != second;
     }
 
-    public static boolean notEqual(float first, float second)
+    public static boolean notEqual(
+        final float first,
+        final float second)
     {
         return first != second;
     }
 
-    public static boolean notEqual(double first, double second)
+    public static boolean notEqual(
+        final double first,
+        final double second)
     {
         return first != second;
     }
 
-    public static boolean notEqual(long first, long second)
+    public static boolean notEqual(
+        final long first,
+        final long second)
     {
         return first != second;
     }
 
-    public static boolean notEqual(Object first, boolean second)
+    public static boolean notEqual(
+        final Object first,
+        final boolean second)
     {
         return !equal(first, second);
     }
 
-    public static boolean notEqual(boolean first, Object second)
+    public static boolean notEqual(
+        final boolean first,
+        final Object second)
     {
         return notEqual(second, first);
     }
 
-    public static boolean less(Comparable first, Comparable second)
+    public static boolean less(
+        final Comparable first,
+        final Comparable second)
     {
-        return (first == null) ? (second == null) : (first.compareTo(second) < 0);
+        return first == null ? second == null : first.compareTo(second) < 0;
     }
 
-    public static boolean less(int first, int second)
-    {
-        return first < second;
-    }
-
-    public static boolean less(short first, short second)
-    {
-        return first < second;
-    }
-
-    public static boolean less(byte first, byte second)
+    public static boolean less(
+        final int first,
+        final int second)
     {
         return first < second;
     }
 
-    public static boolean less(char first, char second)
+    public static boolean less(
+        final short first,
+        final short second)
     {
         return first < second;
     }
 
-    public static boolean less(float first, float second)
+    public static boolean less(
+        final byte first,
+        final byte second)
     {
         return first < second;
     }
 
-    public static boolean less(double first, double second)
+    public static boolean less(
+        final char first,
+        final char second)
     {
         return first < second;
     }
 
-    public static boolean less(long first, long second)
+    public static boolean less(
+        final float first,
+        final float second)
     {
         return first < second;
     }
 
-    public static boolean lessOrEqual(Comparable first, Comparable second)
+    public static boolean less(
+        final double first,
+        final double second)
     {
-        return (first == null) ? (second == null) : (first.compareTo(second) <= 0);
+        return first < second;
     }
 
-    public static boolean lessOrEqual(int first, int second)
+    public static boolean less(
+        final long first,
+        final long second)
     {
-        return first <= second;
+        return first < second;
     }
 
-    public static boolean lessOrEqual(short first, short second)
+    public static boolean lessOrEqual(
+        final Comparable first,
+        final Comparable second)
     {
-        return first <= second;
+        return first == null ? second == null : (first.compareTo(second) <= 0);
     }
 
-    public static boolean lessOrEqual(byte first, byte second)
-    {
-        return first <= second;
-    }
-
-    public static boolean lessOrEqual(char first, char second)
-    {
-        return first <= second;
-    }
-
-    public static boolean lessOrEqual(float first, float second)
+    public static boolean lessOrEqual(
+        final int first,
+        final int second)
     {
         return first <= second;
     }
 
-    public static boolean lessOrEqual(double first, double second)
+    public static boolean lessOrEqual(
+        final short first,
+        final short second)
     {
         return first <= second;
     }
 
-    public static boolean lessOrEqual(long first, long second)
+    public static boolean lessOrEqual(
+        final byte first,
+        final byte second)
     {
         return first <= second;
     }
 
-    public static boolean greater(Comparable first, Comparable second)
+    public static boolean lessOrEqual(
+        final char first,
+        final char second)
     {
-        return (first == null) ? (second == null) : (first.compareTo(second) > 0);
+        return first <= second;
     }
 
-    public static boolean greater(Object first, long second)
+    public static boolean lessOrEqual(
+        final float first,
+        final float second)
+    {
+        return first <= second;
+    }
+
+    public static boolean lessOrEqual(
+        final double first,
+        final double second)
+    {
+        return first <= second;
+    }
+
+    public static boolean lessOrEqual(
+        final long first,
+        final long second)
+    {
+        return first <= second;
+    }
+
+    public static boolean greater(
+        final Comparable first,
+        final Comparable second)
+    {
+        return first == null ? second == null : first.compareTo(second) > 0;
+    }
+
+    public static boolean greater(
+        final Object first,
+        final long second)
     {
         boolean greater = first != null && Comparable.class.isAssignableFrom(first.getClass());
         if (greater)
         {
-            Comparable comparable = (Comparable)first;
+            final Comparable comparable = (Comparable)first;
             greater = comparable.compareTo(new Long(second)) > 0;
         }
         return greater;
     }
 
-    public static boolean greater(int first, int second)
+    public static boolean greater(
+        final int first,
+        final int second)
     {
         return first > second;
     }
 
-    public static boolean greater(short first, short second)
+    public static boolean greater(
+        final short first,
+        final short second)
     {
         return first > second;
     }
 
-    public static boolean greater(byte first, byte second)
+    public static boolean greater(
+        final byte first,
+        final byte second)
     {
         return first > second;
     }
 
-    public static boolean greater(char first, char second)
+    public static boolean greater(
+        final char first,
+        final char second)
     {
         return first > second;
     }
 
-    public static boolean greater(float first, float second)
+    public static boolean greater(
+        final float first,
+        final float second)
     {
         return first > second;
     }
 
-    public static boolean greater(double first, double second)
+    public static boolean greater(
+        final double first,
+        final double second)
     {
         return first > second;
     }
 
-    public static boolean greater(long first, long second)
+    public static boolean greater(
+        final long first,
+        final long second)
     {
         return first > second;
     }
 
-    public static boolean greaterOrEqual(Comparable first, Comparable second)
+    public static boolean greaterOrEqual(
+        final Comparable first,
+        final Comparable second)
     {
-        return (first == null) ? (second == null) : (first.compareTo(second) >= 0);
+        return first == null ? second == null : first.compareTo(second) >= 0;
     }
 
-    public static boolean greaterOrEqual(int first, int second)
-    {
-        return first >= second;
-    }
-
-    public static boolean greaterOrEqual(short first, short second)
-    {
-        return first >= second;
-    }
-
-    public static boolean greaterOrEqual(byte first, byte second)
+    public static boolean greaterOrEqual(
+        final int first,
+        final int second)
     {
         return first >= second;
     }
 
-    public static boolean greaterOrEqual(char first, char second)
+    public static boolean greaterOrEqual(
+        final short first,
+        final short second)
     {
         return first >= second;
     }
 
-    public static boolean greaterOrEqual(float first, float second)
+    public static boolean greaterOrEqual(
+        final byte first,
+        final byte second)
     {
         return first >= second;
     }
 
-    public static boolean greaterOrEqual(double first, double second)
+    public static boolean greaterOrEqual(
+        final char first,
+        final char second)
     {
         return first >= second;
     }
 
-    public static boolean greaterOrEqual(long first, long second)
+    public static boolean greaterOrEqual(
+        final float first,
+        final float second)
     {
         return first >= second;
     }
 
+    public static boolean greaterOrEqual(
+        final double first,
+        final double second)
+    {
+        return first >= second;
+    }
+
+    public static boolean greaterOrEqual(
+        final long first,
+        final long second)
+    {
+        return first >= second;
+    }
 }
