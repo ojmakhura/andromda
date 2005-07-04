676f8c3b9276310fed5b00a6745e33f952d01e69
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-07-04 22:58:22 +0000

    improved support for intelligently detecting target nodes from a transition (added JBpmTransition.possibleTargetNodes property)

--- trunk/cartridges/andromda-jbpm/src/java/org/andromda/cartridges/jbpm/metafacades/JBpmTransitionLogicImpl.java
+++ trunk/cartridges/andromda-jbpm/src/java/org/andromda/cartridges/jbpm/metafacades/JBpmTransitionLogicImpl.java
@@ -3,6 +3,14 @@ package org.andromda.cartridges.jbpm.metafacades;
 import org.andromda.cartridges.jbpm.JBpmProfile;
 import org.andromda.metafacades.uml.ActivityGraphFacade;
 import org.andromda.metafacades.uml.GuardFacade;
+import org.andromda.metafacades.uml.PseudostateFacade;
+import org.andromda.metafacades.uml.StateVertexFacade;
+import org.andromda.metafacades.uml.TransitionFacade;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 
 /**
@@ -40,5 +48,36 @@ public class JBpmTransitionLogicImpl
     protected boolean handleIsTaskNode()
     {
         return hasStereotype(JBpmProfile.STEREOTYPE_TASK);
+    }
+
+    protected java.util.Collection handleGetPossibleTargetNodes()
+    {
+        final Set targetNodes = new HashSet();
+        this.collectTargetNodes(targetNodes, this);
+        return targetNodes;
+    }
+
+    /**
+     * Recursively collects all nodes that might be a direct or indirect runtime target for this transition.
+     * This method steps over pseudostates and analyzes the outgoig transitions.
+     */
+    private void collectTargetNodes(Collection nodes, TransitionFacade transition)
+    {
+        final StateVertexFacade target = transition.getTarget();
+
+        if (target instanceof PseudostateFacade)
+        {
+            final Collection outgoingTransitions = target.getOutgoing();
+            for (final Iterator outgoingTransitionIterator = outgoingTransitions.iterator();
+                    outgoingTransitionIterator.hasNext();)
+            {
+                final TransitionFacade outgoingTransition = (TransitionFacade)outgoingTransitionIterator.next();
+                this.collectTargetNodes(nodes, outgoingTransition);
+            }
+        }
+        else
+        {
+            nodes.add(target);
+        }
     }
 }
