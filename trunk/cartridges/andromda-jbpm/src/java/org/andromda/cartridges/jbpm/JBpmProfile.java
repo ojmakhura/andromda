812b67df171fa959d41751975fe49dc6b0f1477c
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-07-05 17:22:28 +0000

    completed support for timers, fixed an issue where final state nodes weren't properly generated, added a timer transition tagged value, added more constraints to the meta-model

--- trunk/cartridges/andromda-jbpm/src/java/org/andromda/cartridges/jbpm/JBpmProfile.java
+++ trunk/cartridges/andromda-jbpm/src/java/org/andromda/cartridges/jbpm/JBpmProfile.java
@@ -23,4 +23,5 @@ public class JBpmProfile
     public static final String TAGGEDVALUE_ASSIGNMENT_EXPRESSION = profile.get("ASSIGNMENT_EXPRESSION");
     public static final String TAGGEDVALUE_TIMER_REPEAT = profile.get("TIMER_REPEAT");
     public static final String TAGGEDVALUE_TIMER_DUEDATE = profile.get("TIMER_DUEDATE");
+    public static final String TAGGEDVALUE_TIMER_TRANSITION = profile.get("TIMER_TRANSITION");
 }
