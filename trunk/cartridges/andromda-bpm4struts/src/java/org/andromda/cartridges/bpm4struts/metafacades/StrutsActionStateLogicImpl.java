199abf7e8b9133203a8ba08a6b84f80d5531f5a5
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-07-13 23:06:54 +0000

    remove more unused code

--- trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsActionStateLogicImpl.java
+++ trunk/cartridges/andromda-bpm4struts/src/java/org/andromda/cartridges/bpm4struts/metafacades/StrutsActionStateLogicImpl.java
@@ -1,13 +1,5 @@
 package org.andromda.cartridges.bpm4struts.metafacades;
 
-import org.andromda.core.common.StringUtilsHelper;
-import org.andromda.metafacades.uml.ActivityGraphFacade;
-import org.andromda.metafacades.uml.CallEventFacade;
-import org.andromda.metafacades.uml.EventFacade;
-import org.andromda.metafacades.uml.StateMachineFacade;
-import org.andromda.metafacades.uml.TransitionFacade;
-import org.andromda.metafacades.uml.UseCaseFacade;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -15,6 +7,11 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import org.andromda.metafacades.uml.ActivityGraphFacade;
+import org.andromda.metafacades.uml.StateMachineFacade;
+import org.andromda.metafacades.uml.TransitionFacade;
+import org.andromda.metafacades.uml.UseCaseFacade;
 
 
 /**
@@ -29,17 +26,7 @@ public class StrutsActionStateLogicImpl
     {
         super(metaObject, context);
     }
-
-    protected boolean handleIsContainedInFrontEndUseCase()
-    {
-        return this.getStateMachine() instanceof StrutsActivityGraph;
-    }
-
-    protected java.lang.String handleGetActionMethodName()
-    {
-        return '_' + StringUtilsHelper.lowerCamelCaseName(getName());
-    }
-
+    
     /**
      * Overridden because StrutsAction does not extend FrontEndAction.
      * 
@@ -71,49 +58,6 @@ public class StrutsActionStateLogicImpl
         return new ArrayList(actionSet);
     }
 
-
-    protected List handleGetControllerCalls()
-    {
-        final List controllerCallsList = new ArrayList();
-        final Collection deferrableEvents = getDeferrableEvents();
-        for (final Iterator iterator = deferrableEvents.iterator(); iterator.hasNext();)
-        {
-            final EventFacade event = (EventFacade)iterator.next();
-            if (event instanceof CallEventFacade)
-            {
-                final Object operationObject = ((CallEventFacade)event).getOperation();
-                if (operationObject != null)
-                {
-                    controllerCallsList.add(operationObject);
-                }
-            }
-            else if (event instanceof StrutsTrigger)
-            {
-                final Object callObject = ((StrutsTrigger)event).getControllerCall();
-                if (callObject != null)
-                {
-                    controllerCallsList.add(callObject);
-                }
-            }
-        }
-        return controllerCallsList;
-    }
-
-    protected java.lang.Object handleGetForward()
-    {
-        Object forward = null;
-
-        for (final Iterator iterator = getOutgoing().iterator(); iterator.hasNext() && forward == null;)
-        {
-            final TransitionFacade transition = (TransitionFacade)iterator.next();
-            if (!(transition instanceof StrutsExceptionHandler))
-            {
-                forward = transition;
-            }
-        }
-        return forward;
-    }
-
     /**
      * @see org.andromda.metafacades.uml.FrontEndActionState#getExceptions()
      */
@@ -130,10 +74,5 @@ public class StrutsActionStateLogicImpl
             }
         }
         return new ArrayList(exceptionsMap.values());
-    }
-
-    protected boolean handleIsServerSide()
-    {
-        return !(this instanceof StrutsJsp);
     }
 }
