4897dd77e2428dc7db443f08ea8e02eb7cf7dc6d
draftdog <draftdog@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-07-14 23:24:15 +0000

    fixed platform compatiblity issue with the newline characters, always using \n now, many jbpm improvements, mainly related to using pseudostates such as forks

--- cartridges/andromda-jbpm/src/java/org/andromda/cartridges/jbpm/metafacades/JBpmNodeLogicImpl.java
+++ cartridges/andromda-jbpm/src/java/org/andromda/cartridges/jbpm/metafacades/JBpmNodeLogicImpl.java
@@ -1,9 +1,5 @@
 package org.andromda.cartridges.jbpm.metafacades;
 
-import org.andromda.metafacades.uml.ActivityGraphFacade;
-import org.andromda.metafacades.uml.StateMachineFacade;
-import org.andromda.core.common.StringUtilsHelper;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -24,12 +20,6 @@ public class JBpmNodeLogicImpl
         super (metaObject, context);
     }
 
-    protected boolean handleIsContainedInBusinessProcess()
-    {
-        return this.getStateMachine() instanceof ActivityGraphFacade
-                && ((ActivityGraphFacade)this.getStateMachine()).getUseCase() instanceof JBpmProcessDefinition;
-    }
-
     /**
      * @see org.andromda.cartridges.jbpm.metafacades.JBpmNode#isTaskNode()
      */
@@ -39,150 +29,10 @@ public class JBpmNodeLogicImpl
     }
 
     /**
-     * @see org.andromda.cartridges.jbpm.metafacades.JBpmNode#getBeforeSignal()
-     */
-    protected java.util.List handleGetBeforeSignal()
-    {
-        final List beforeSignals = new ArrayList();
-
-        final Collection events = this.getDeferrableEvents();
-        for (final Iterator eventIterator = events.iterator(); eventIterator.hasNext();)
-        {
-            final Object eventObject = eventIterator.next();
-            if (eventObject instanceof JBpmAction && ((JBpmAction)eventObject).isBeforeSignal())
-            {
-                beforeSignals.add(eventObject);
-            }
-        }
-
-        return beforeSignals;
-    }
-
-    /**
-     * @see org.andromda.cartridges.jbpm.metafacades.JBpmNode#getAfterSignal()
-     */
-    protected java.util.List handleGetAfterSignal()
-    {
-        final List afterSignals = new ArrayList();
-
-        final Collection events = this.getDeferrableEvents();
-        for (final Iterator eventIterator = events.iterator(); eventIterator.hasNext();)
-        {
-            final Object eventObject = eventIterator.next();
-            if (eventObject instanceof JBpmAction && ((JBpmAction)eventObject).isAfterSignal())
-            {
-                afterSignals.add(eventObject);
-            }
-        }
-
-        return afterSignals;
-    }
-
-    /**
-     * @see org.andromda.cartridges.jbpm.metafacades.JBpmNode#getNodeEnter()
-     */
-    protected java.util.List handleGetNodeEnter()
-    {
-        final List nodeEnters = new ArrayList();
-
-        final Collection events = this.getDeferrableEvents();
-        for (final Iterator eventIterator = events.iterator(); eventIterator.hasNext();)
-        {
-            final Object eventObject = eventIterator.next();
-            if (eventObject instanceof JBpmAction && ((JBpmAction)eventObject).isNodeEnter())
-            {
-                nodeEnters.add(eventObject);
-            }
-        }
-
-        return nodeEnters;
-    }
-
-    /**
-     * @see org.andromda.cartridges.jbpm.metafacades.JBpmNode#getNodeLeave()
-     */
-    protected java.util.List handleGetNodeLeave()
-    {
-        final List nodeLeaves = new ArrayList();
-
-        final Collection events = this.getDeferrableEvents();
-        for (final Iterator eventIterator = events.iterator(); eventIterator.hasNext();)
-        {
-            final Object eventObject = eventIterator.next();
-            if (eventObject instanceof JBpmAction && ((JBpmAction)eventObject).isNodeLeave())
-            {
-                nodeLeaves.add(eventObject);
-            }
-        }
-
-        return nodeLeaves;
-    }
-
-    /**
-     * @see org.andromda.cartridges.jbpm.metafacades.JBpmNode#getTasks()
-     */
-    protected java.util.List handleGetTasks()
-    {
-        final List tasks = new ArrayList();
-
-        final Collection events = this.getDeferrableEvents();
-        for (final Iterator eventIterator = events.iterator(); eventIterator.hasNext();)
-        {
-            final Object eventObject = eventIterator.next();
-            if (eventObject instanceof JBpmAction && ((JBpmAction)eventObject).isTask())
-            {
-                tasks.add(eventObject);
-            }
-        }
-
-        return tasks;
-    }
-
-    protected List handleGetTimers()
-    {
-        final List timers = new ArrayList();
-
-        final Collection events = this.getDeferrableEvents();
-        for (final Iterator eventIterator = events.iterator(); eventIterator.hasNext();)
-        {
-            final Object eventObject = eventIterator.next();
-            if (eventObject instanceof JBpmAction && ((JBpmAction)eventObject).isTimer())
-            {
-                timers.add(eventObject);
-            }
-        }
-
-        return timers;
-    }
-
-    /**
      * @see org.andromda.cartridges.jbpm.metafacades.JBpmNode#getSwimlane()
      */
     protected java.lang.Object handleGetSwimlane()
     {
         return this.getPartition();
-    }
-
-    protected String handleGetNodeClassName()
-    {
-        return StringUtilsHelper.upperCamelCaseName(this.getName()) + "Node";
-    }
-
-    protected String handleGetNodePackageName()
-    {
-        return (this.getProcessDefinition() == null) ? null : this.getProcessDefinition().getPackageName();
-    }
-
-    protected Object handleGetProcessDefinition()
-    {
-        Object processDefinition = null;
-
-        final StateMachineFacade stateMachine = this.getStateMachine();
-        if (stateMachine instanceof ActivityGraphFacade)
-        {
-            processDefinition = ((ActivityGraphFacade)stateMachine).getUseCase();
-        }
-
-        return (processDefinition instanceof JBpmProcessDefinition) ? processDefinition : null;
     }
 }
