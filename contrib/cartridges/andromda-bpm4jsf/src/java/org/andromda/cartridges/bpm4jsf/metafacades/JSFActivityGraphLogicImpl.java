cd026ee209e06ec1eb73625b055f49eef9f68070
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-08-29 21:45:43 +0000

    add a couple of constraints

---
+++ contrib/cartridges/andromda-bpm4jsf/src/java/org/andromda/cartridges/bpm4jsf/metafacades/JSFActivityGraphLogicImpl.java
@@ -0,0 +1,17 @@
+package org.andromda.cartridges.bpm4jsf.metafacades;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.bpm4jsf.metafacades.JSFActivityGraph.
+ *
+ * @see org.andromda.cartridges.bpm4jsf.metafacades.JSFActivityGraph
+ */
+public class JSFActivityGraphLogicImpl
+    extends JSFActivityGraphLogic
+{
+
+    public JSFActivityGraphLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+}
