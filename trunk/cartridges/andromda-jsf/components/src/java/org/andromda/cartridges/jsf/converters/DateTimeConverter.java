4fd9ac52cee4799d6dc5ce1cb168f9fa5427cc03
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-09-13 14:13:24 +0000

    converter wasn't correctly handling dates and calendars

--- trunk/cartridges/andromda-jsf/components/src/java/org/andromda/cartridges/jsf/converters/DateTimeConverter.java
+++ trunk/cartridges/andromda-jsf/components/src/java/org/andromda/cartridges/jsf/converters/DateTimeConverter.java
@@ -6,43 +6,84 @@ import java.util.Date;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.ConverterException;
+import javax.faces.el.ValueBinding;
+
 
 /**
  * Overrides the default DateTimeConverter to include conversion of Calendar
  * instances as well as Date instances.
- * 
+ *
  * @author Chad Brandon
  */
 public class DateTimeConverter
     extends javax.faces.convert.DateTimeConverter
 {
-   
     /**
      * @see javax.faces.convert.Converter#getAsString(javax.faces.context.FacesContext, javax.faces.component.UIComponent, java.lang.Object)
      */
-    public String getAsString(FacesContext context, UIComponent component, Object value) throws ConverterException
+    public String getAsString(
+        FacesContext context,
+        UIComponent component,
+        Object value)
+        throws ConverterException
     {
         if (value instanceof Calendar)
         {
             value = ((Calendar)value).getTime();
         }
-        return super.getAsString(context, component, value);
+        return super.getAsString(
+            context,
+            component,
+            value);
     }
 
     /**
      * @see javax.faces.convert.Converter#getAsObject(javax.faces.context.FacesContext, javax.faces.component.UIComponent, java.lang.String)
      */
-    public Object getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException
+    public Object getAsObject(
+        FacesContext context,
+        UIComponent component,
+        String value)
+        throws ConverterException
     {
-        Object asObject = super.getAsObject(context, component, value);
-        if (asObject instanceof Date)
+        Object asObject = null;
+        final Class componentType = this.getComponentType(
+                context,
+                component);
+        if (componentType != null)
         {
-            final Calendar calendar = Calendar.getInstance();
-            calendar.setTime((Date)asObject);
-            asObject = calendar;
+            asObject = super.getAsObject(
+                    context,
+                    component,
+                    value);
+            if (Calendar.class.isAssignableFrom(componentType) && asObject instanceof Date)
+            {
+                final Calendar calendar = Calendar.getInstance();
+                calendar.setTime((Date)asObject);
+                asObject = calendar;
+            }
         }
         return asObject;
     }
-    
+
+    /**
+     * Gets the component type for the given <code>component</code>.
+     * @param context the current faces context.
+     * @param component the component from which to retrieve the type.
+     * @return true/false
+     */
+    private Class getComponentType(
+        final FacesContext context,
+        final UIComponent component)
+    {
+        Class type = null;
+        final ValueBinding binding = component.getValueBinding("value");
+        if (binding != null)
+        {
+            type = binding.getType(context);
+        }
+        return type;
+    }
+
     public static final String CONVERTER_ID = "andromda.faces.DateTime";
 }
