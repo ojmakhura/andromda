9a8423e4935a6763a952f20c96d0619e785b5298
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-09-15 03:25:28 +0000

    Start on writing the test for the spring transformation (refactored directories to be consistent with other modules)

---
+++ contrib/transformations/spring/src/test/java/org/andromda/transformations/spring/SpringTransformationTest.java
@@ -0,0 +1,79 @@
+package org.andromda.transformations.spring;
+
+
+import java.net.URL;
+
+import junit.framework.TestCase;
+
+import org.andromda.transformers.atl.ATLTransformer;
+import org.andromda.transformers.atl.Library;
+import org.andromda.transformers.atl.Model;
+import org.apache.log4j.BasicConfigurator;
+
+public class SpringTransformationTest
+    extends TestCase
+{
+    public void testTransform()
+    {
+    	BasicConfigurator.configure();
+    	
+        ATLTransformer transformer = new ATLTransformer();
+        
+        // uml2java test (this transforms a UML to a JAVA metamodel)
+        
+        // - the path of the package location of the test resources
+        final URL testResourceUrl = SpringTransformationTest.class.getResource("/atl");
+        assertNotNull(testResourceUrl);
+        final String testResourcePath = testResourceUrl.toString() + '/';
+            
+        final String atlPath = testResourcePath + "UML2Spring.atl";
+        
+        final String mdrRepository = "MDR";
+        
+        // - set up the UML meta model (this is the input model's metamodel)
+        final String umlMetamodelName = "UML";
+        final Model umlMetamodel = new Model();
+        final URL umlMetamodelUrl = SpringTransformationTest.class.getResource("/M2_DiagramInterchangeModel.xml");
+        assertNotNull(umlMetamodelUrl);
+        final String umlMetamodelPath = umlMetamodelUrl.toString();
+        umlMetamodel.setName(umlMetamodelName);
+        umlMetamodel.setPath(umlMetamodelPath.toString());
+        
+        // - setup the source model.
+        final Model sourceModel = new Model();
+        sourceModel.setName("IN");
+        sourceModel.setRepository(mdrRepository);
+        final URL sourceModelUrl = SpringTransformationTest.class.getResource("/SpringTransformationTests.xml.zip");
+        assertNotNull(sourceModelUrl);
+        final String sourceModelPath = "jar:" + sourceModelUrl + "!/SpringTransformationTests.xml";
+        sourceModel.setPath(sourceModelPath.toString());
+        // - set this first input model's meta model as UML
+        sourceModel.setMetamodel(umlMetamodelName);
+        
+        // - setup the target metamodel (this is the output model's metamodel)
+        final String javaMetamodelName = "JAVA";
+        final Model javaMetamodel = new Model();
+        javaMetamodel.setName(javaMetamodelName);
+        final URL springMetamodelUrl = SpringTransformationTest.class.getResource("/SpringMetamodel.xml");
+        assertNotNull(springMetamodelUrl);
+        final String springMetamodelPath = springMetamodelUrl.toString();
+        javaMetamodel.setPath(springMetamodelPath);
+        
+        // - setup the target model
+        final Model targetModel = new Model();
+        targetModel.setName("OUT");
+        targetModel.setRepository(mdrRepository);
+        // - this is the path of the transformed output model
+        final String targetModelPath = testResourcePath + "transformed-output.xmi";
+        targetModel.setPath(targetModelPath);
+        targetModel.setMetamodel(javaMetamodelName);
+        
+        // - load up the metamodels and models
+        Model[] metamodels = new Model[] {umlMetamodel, javaMetamodel};
+        Model[] sourceModels = new Model[] {sourceModel};
+        Model[] targetModels = new Model[] {targetModel};
+        
+        // - perform the transformation
+        transformer.transform(atlPath, (Library[])null, metamodels, sourceModels, targetModels);
+    }
+}
