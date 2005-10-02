60b5d929cefc373961b2104bc62be59bed63cefe
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-10-02 22:08:23 +0000

    add support for session objects

--- cartridges/andromda-jsf/src/java/org/andromda/cartridges/jsf/metafacades/JSFControllerLogicImpl.java
+++ cartridges/andromda-jsf/src/java/org/andromda/cartridges/jsf/metafacades/JSFControllerLogicImpl.java
@@ -1,7 +1,12 @@
 package org.andromda.cartridges.jsf.metafacades;
 
-import org.andromda.cartridges.jsf.metafacades.JSFControllerLogic;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.andromda.cartridges.jsf.JSFGlobals;
+import org.andromda.metafacades.uml.DependencyFacade;
+import org.andromda.metafacades.uml.ModelElementFacade;
 import org.andromda.utils.StringUtilsHelper;
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
@@ -58,5 +63,22 @@ public class JSFControllerLogicImpl
     protected String handleGetBeanName()
     {
         return StringUtilsHelper.lowerCamelCaseName(this.getName());
+    }
+
+    /**
+     * @see org.andromda.cartridges.jsf.metafacades.JSFController#getSessionObjectReferences()
+     */
+    protected List handleGetSessionObjectReferences()
+    {
+        final List references = new ArrayList(this.getSourceDependencies());
+        for (final Iterator iterator = references.iterator(); iterator.hasNext();)
+        {
+            final ModelElementFacade targetElement = ((DependencyFacade)iterator.next()).getTargetElement();
+            if (!(targetElement instanceof JSFSessionObject))
+            {
+                iterator.remove();   
+            }
+        }
+        return references;
     }
 }
