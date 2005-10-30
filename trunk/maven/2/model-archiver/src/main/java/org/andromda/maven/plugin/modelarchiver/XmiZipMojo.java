2fe23bc39065ea91f4daf6d8e392b8789d210dec
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-10-30 17:16:31 +0000

    add versioning of the reference modules

--- trunk/maven/2/model-archiver/src/main/java/org/andromda/maven/plugin/modelarchiver/XmiZipMojo.java
+++ trunk/maven/2/model-archiver/src/main/java/org/andromda/maven/plugin/modelarchiver/XmiZipMojo.java
@@ -1,8 +1,11 @@
 package org.andromda.maven.plugin.modelarchiver;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
+import org.andromda.core.common.ResourceUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.maven.archiver.MavenArchiveConfiguration;
 import org.apache.maven.archiver.MavenArchiver;
 import org.apache.maven.plugin.AbstractMojo;
@@ -87,12 +90,20 @@ public class XmiZipMojo
     protected ArchiverManager archiverManager;
 
     /**
+     * The extensions to search for when doing replacement of embedded model HREF references
+     * within the archived model file from non-versioned to versioned references.
+     *
+     * @parameter expression=".xml,.xml.zip,.xmi"
+     * @required
+     */
+    protected String replacementExtensions;
+
+    /**
      * The maven archiver to use.
      *
      * @parameter
      */
     private MavenArchiveConfiguration archive = new MavenArchiveConfiguration();
-    private File buildDir;
 
     public void execute()
         throws MojoExecutionException
@@ -107,14 +118,16 @@ public class XmiZipMojo
         try
         {
             // - delete and remake the directory each time
-            final File buildDirectory = this.getBuildDir();
+            final File buildDirectory = this.getBuildDirectory();
             FileUtils.deleteDirectory(buildDirectory);
             buildDirectory.mkdirs();
 
             final File modelSourceDir = modelSourceDirectory;
+            final String[] replacementExtensions =
+                this.replacementExtensions != null ? this.replacementExtensions.split(",\\s*") : new String[0];
             if (modelSourceDir.exists())
             {
-                getLog().info("Copy xml.zip resources to " + getBuildDir().getAbsolutePath());
+                getLog().info("Copy xml.zip resources to " + this.getBuildDirectory().getAbsolutePath());
                 final File[] modelFiles = modelSourceDir.listFiles();
                 for (int ctr = 0; ctr < modelFiles.length; ctr++)
                 {
@@ -136,43 +149,83 @@ public class XmiZipMojo
                                 FileUtils.rename(
                                     extractedFile,
                                     newFile);
+                                String contents = ResourceUtils.getContents(newFile.toURL());
+                                for (int ctr3 = 0; ctr3 < replacementExtensions.length; ctr3++)
+                                {
+                                    final String version = escapePattern(this.project.getVersion());
+                                    final String extension = escapePattern(replacementExtensions[ctr3]);
+                                    final String extensionPattern = "((\\-" + version + ")?)" + extension;
+                                    final String newExtension = "\\-" + version + extension;
+                                    contents = contents.replaceAll(
+                                            extensionPattern,
+                                            newExtension);
+                                }
+                                final FileWriter fileWriter = new FileWriter(newFile);
+                                fileWriter.write(contents);
+                                fileWriter.flush();
                             }
                         }
                     }
                 }
             }
         }
-        catch (Throwable throwable)
+        catch (final Throwable throwable)
         {
             throw new MojoExecutionException("Error copying model resources", throwable);
         }
 
         try
         {
-            File xmlZipFile = new File(outputDirectory, finalName + ".xml.zip");
+            final File xmlZipFile = new File(outputDirectory, finalName + ".xml.zip");
             project.getArtifact().setFile(xmlZipFile);
-            MavenArchiver archiver = new MavenArchiver();
+            final MavenArchiver archiver = new MavenArchiver();
             archiver.setArchiver(jarArchiver);
             archiver.setOutputFile(xmlZipFile);
 
-            archiver.getArchiver().addDirectory(getBuildDir());
+            archiver.getArchiver().addDirectory(getBuildDirectory());
             archiver.createArchive(
                 project,
                 archive);
         }
-        catch (Exception e)
+        catch (final Throwable throwable)
         {
-            throw new MojoExecutionException("Error assembling model", e);
+            throw new MojoExecutionException("Error assembling model", throwable);
         }
     }
 
-    protected File getBuildDir()
+    /**
+     * Escapes the pattern so that the reserved regular expression
+     * characters are used literally.
+     * @param pattern the pattern to replace.
+     * @return the resulting pattern.
+     */
+    private static String escapePattern(String pattern)
     {
-        if (buildDir == null)
+        pattern = StringUtils.replace(
+                pattern,
+                ".",
+                "\\.");
+        pattern = StringUtils.replace(
+                pattern,
+                "-",
+                "\\-");
+        return pattern;
+    }
+
+    private File buildDirectory;
+
+    /**
+     * Gets the current build directory as a file instance.
+     *
+     * @return the build directory as a file.
+     */
+    protected File getBuildDirectory()
+    {
+        if (buildDirectory == null)
         {
-            buildDir = new File(workDirectory);
+            buildDirectory = new File(workDirectory);
         }
-        return buildDir;
+        return buildDirectory;
     }
 
     /**
@@ -182,8 +235,8 @@ public class XmiZipMojo
      * @param location Location where to put the unpacked files.
      */
     protected void unpack(
-        File file,
-        File location)
+        final File file,
+        final File location)
         throws MojoExecutionException, NoSuchArchiverException
     {
         String archiveExt = FileUtils.getExtension(file.getAbsolutePath()).toLowerCase();
