adc03c679b59f089431e2c170715233c3ebaa24b
nbhatia <nbhatia@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-11-03 18:11:56 +0000

    *** empty log message ***

---
+++ contrib/cartridges/andromda-aspdotnet/src/java/org/andromda/cartridges/aspdotnet/metafacades/AspControllerOperationLogicImpl.java
@@ -0,0 +1,17 @@
+package org.andromda.cartridges.aspdotnet.metafacades;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.aspdotnet.metafacades.AspControllerOperation.
+ *
+ * @see org.andromda.cartridges.aspdotnet.metafacades.AspControllerOperation
+ */
+public class AspControllerOperationLogicImpl
+    extends AspControllerOperationLogic
+{
+
+    public AspControllerOperationLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+}
---
+++ contrib/cartridges/andromda-aspdotnet/src/java/org/andromda/cartridges/aspdotnet/metafacades/AspFinalStateLogicImpl.java
@@ -0,0 +1,17 @@
+package org.andromda.cartridges.aspdotnet.metafacades;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.aspdotnet.metafacades.AspFinalState.
+ *
+ * @see org.andromda.cartridges.aspdotnet.metafacades.AspFinalState
+ */
+public class AspFinalStateLogicImpl
+    extends AspFinalStateLogic
+{
+
+    public AspFinalStateLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+}
