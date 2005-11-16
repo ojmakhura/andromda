eb5cea1b63c1ea6009bfd78e0d88a43be780bf0a
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-11-16 00:40:18 +0000

    Start on andromdapp generator for maven2

---
+++ maven/2/andromdapp/src/main/java/org/andromda/maven/plugin/andromdapp/AndroMDApp.java
@@ -0,0 +1,269 @@
+package org.andromda.maven.plugin.andromdapp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.net.URL;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.andromda.core.common.ResourceFinder;
+import org.andromda.core.common.ResourceUtils;
+import org.andromda.core.common.XmlObjectFactory;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+
+/**
+ * Represents an instance of the AndroMDA application
+ * generator.
+ *
+ * @author Chad Brandon
+ */
+public class AndroMDApp
+{
+    /**
+     * Runs the AndroMDApp generation process.
+     */
+    public void run()
+    {
+        try
+        {
+            this.initialize();
+            this.promptUser();
+        }
+        catch (final Throwable throwable)
+        {
+            throw new AndroMDAppException(throwable);
+        }
+    }
+
+    /**
+     * The name of the AndroMDApp descriptor.
+     */
+    private static final String DESCRIPTOR = "andromdapp.xml";
+
+    /**
+     * The directory in which the descriptors are kept.
+     */
+    private static final String DESCRIPTOR_DIRECTORY = "META-INF/andromdapp";
+
+    /**
+     * The velocity template context.
+     */
+    private VelocityContext templateContext = null;
+
+    /**
+     * All types of discovered AndroMDApps
+     */
+    private Map types = new LinkedHashMap();
+
+    /**
+     * Performs any required initialization.
+     * @throws Exception
+     */
+    private void initialize()
+        throws Exception
+    {
+        final URL[] descriptorDirectories = ResourceFinder.findResources(DESCRIPTOR_DIRECTORY);
+        if (descriptorDirectories != null && descriptorDirectories.length > 0)
+        {
+            final int numberOfDescriptorDirectories = descriptorDirectories.length;
+            for (int ctr = 0; ctr < numberOfDescriptorDirectories; ctr++)
+            {
+                final List directoryContents =
+                    ResourceUtils.getDirectoryContents(
+                        descriptorDirectories[ctr],
+                        true,
+                        null);
+                for (final Iterator iterator = directoryContents.iterator(); iterator.hasNext();)
+                {
+                    final String uri = (String)iterator.next();
+                    if (uri.replaceAll(
+                            ".*(\\+|/)",
+                            "").equals(DESCRIPTOR))
+                    {
+                        final XmlObjectFactory factory = XmlObjectFactory.getInstance(AndroMDApp.class);
+                        final AndroMDApp andromdapp = (AndroMDApp)factory.getObject(ResourceUtils.toURL(uri));
+                        this.types.put(
+                            andromdapp.getType(),
+                            andromdapp);
+                    }
+                }
+            }
+        }
+
+        final Properties properties = new Properties();
+        properties.put(
+            VelocityEngine.RESOURCE_LOADER,
+            "classpath");
+        properties.put(
+            "classpath." + VelocityEngine.RESOURCE_LOADER + ".class",
+            ClasspathResourceLoader.class.getName());
+
+        Velocity.init(properties);
+        this.templateContext = new VelocityContext(this.promptUser());
+    }
+
+    /**
+     * Prompts the user for the input required to generate an application with the correct information.
+     */
+    private Map promptUser()
+    {
+        final Map properties = new HashMap();
+        AndroMDApp andromdapp = null;
+        if (this.types.size() > 1)
+        {
+            final List types = new ArrayList();
+            final StringBuffer typesChoice = new StringBuffer("[");
+            for (final Iterator iterator = this.types.keySet().iterator(); iterator.hasNext();)
+            {
+                final AndroMDApp type = (AndroMDApp)iterator.next();
+                typesChoice.append(type.getType());
+                types.add(types);
+                if (iterator.hasNext())
+                {
+                    typesChoice.append("|");
+                }
+            }
+            this.printText("Please choose the type of application to generate " + typesChoice);
+            String selectedType = this.readLine();
+            while (!types.contains(selectedType))
+            {
+                selectedType = this.readLine();
+            }
+            andromdapp = (AndroMDApp)this.types.get(selectedType);
+        }
+        else if (!this.types.isEmpty())
+        {
+            andromdapp = (AndroMDApp)((Map.Entry)this.types.entrySet().iterator().next()).getValue();
+        }
+        else
+        {
+            throw new AndroMDAppException("No '" + DESCRIPTOR + "' descriptor files could be found");
+        }
+
+        for (final Iterator iterator = andromdapp.prompts.iterator(); iterator.hasNext();)
+        {
+            final Prompt prompt = (Prompt)iterator.next();
+            final String id = prompt.getId();
+            String response = this.promptForInput(prompt);
+            while (!prompt.isValidResponse(response))
+            {
+                response = this.promptForInput(prompt);
+            }
+
+            properties.put(
+                id,
+                response);
+        }
+
+        System.out.println("the end!!!!!");
+        return Collections.unmodifiableMap(properties);
+    }
+
+    /**
+     * Prompts the user for the information contained in the given <code>prompt</code>.
+     *
+     * @param prompt the prompt from which to format the prompt text.
+     * @return the response of the prompt.
+     */
+    private String promptForInput(final Prompt prompt)
+    {
+        this.printText(prompt.getText());
+        return this.readLine();
+    }
+
+    /**
+     * Prints text to the console.
+     *
+     * @param text the text to print to the console;
+     */
+    private void printText(final String text)
+    {
+        System.out.println();
+        System.out.println(text);
+        System.out.flush();
+    }
+
+    /**
+     * Reads a line from standard input and returns the value.
+     *
+     * @return the value read from standard input.
+     */
+    private String readLine()
+    {
+        final BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String inputString = null;
+        try
+        {
+            inputString = input.readLine();
+        }
+        catch (final IOException exception)
+        {
+            inputString = null;
+        }
+        return inputString == null || inputString.trim().length() == 0 ? null : inputString;
+    }
+
+    /**
+     * The type of this AndroMDApp (i.e. 'j2ee', '.net', etc).
+     */
+    private String type;
+
+    /**
+     * Gets the type of this AndroMDApp.
+     *
+     * @return Returns the type.
+     */
+    public String getType()
+    {
+        return type;
+    }
+
+    /**
+     * Sets the type of this AndroMDApp.
+     *
+     * @param type The type to set.
+     */
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    /**
+     * Stores the available prompts for this andromdapp.
+     */
+    private List prompts = new ArrayList();
+
+    /**
+     * Adds a prompt to the collection of prompts
+     * contained within this instance.
+     *
+     * @param prompt the prompt to add.
+     */
+    public void addPrompt(final Prompt prompt)
+    {
+        this.prompts.add(prompt);
+    }
+
+    /**
+     * Gets all available prompts.
+     *
+     * @return the list of prompts.
+     */
+    public List getPrompts()
+    {
+        return this.prompts;
+    }
+}
---
+++ maven/2/andromdapp/src/main/java/org/andromda/maven/plugin/andromdapp/AndroMDAppMojo.java
@@ -0,0 +1,32 @@
+package org.andromda.maven.plugin.andromdapp;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+
+/**
+ * AndroMDA application generator Mojo.
+ *
+ * @author Chad Brandon
+ * @goal generate
+ * @requiresProject false
+ */
+public class AndroMDAppMojo
+    extends AbstractMojo
+{
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        try
+        {
+            AndroMDApp andromdapp = new AndroMDApp();
+            andromdapp.run();
+        }
+        catch (final Throwable throwable)
+        {
+            throw new MojoExecutionException("An error occurred while attempting to generate an application", throwable);
+        }
+    }
+
+}
