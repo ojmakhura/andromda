5bd9b448aa492ea482e038bf1d98e5ac5521d833
nbhatia <nbhatia@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-11-17 04:48:06 +0000

    Added transaction support

--- contrib/samples/time-tracker-dotnet/TimeTrackerConsole/Program.cs
+++ contrib/samples/time-tracker-dotnet/TimeTrackerConsole/Program.cs
@@ -9,6 +9,12 @@ namespace TimeTracker
     {
         private static Random random;
 
+        // Data Access Objects
+        private static IPersonDao personDao = DaoFactory.GetPersonDao();
+        private static ITaskDao taskDao = DaoFactory.GetTaskDao();
+        private static ITimecardDao timecardDao = DaoFactory.GetTimecardDao();
+        private static ITimeAllocationDao timeAllocationDao = DaoFactory.GetTimeAllocationDao();
+
         public static void Main(string[] args)
         {
             StartApplication();
@@ -36,11 +42,24 @@ namespace TimeTracker
             Console.Write("Person Name: ");
             String name = Console.ReadLine();
 
+            // Instantiate the person
             Person person = Person.Factory.newInstance();
             person.Name = name;
             person.Rate = Quantity.newInstance(10, "USD/hour");
-            GetPersonDao().Create(person);
-            Console.WriteLine(name + " added.");
+
+            // Persist in database
+            personDao.BeginTransaction();
+            try
+            {
+                personDao.Create(person);
+                personDao.CommitTransaction();
+                Console.WriteLine(name + " added.");
+            }
+            catch (Exception e)
+            {
+                personDao.RollbackTransaction();
+                Console.WriteLine(e);
+            }
         }
 
         private static void AddTask()
@@ -48,42 +67,148 @@ namespace TimeTracker
             Console.Write("Task Name: ");
             String name = Console.ReadLine();
 
+            // Instantiate the task
             Task task = Task.Factory.newInstance();
             task.Name = name;
-            GetTaskDao().Create(task);
-            Console.WriteLine("Task " + task.Id + " added: " + task.Name);
+
+            // Persist in database
+            taskDao.BeginTransaction();
+            try
+            {
+                taskDao.Create(task);
+                taskDao.CommitTransaction();
+                Console.WriteLine("Task " + task.Id + " added: " + task.Name);
+            }
+            catch (Exception e)
+            {
+                taskDao.RollbackTransaction();
+                Console.WriteLine(e);
+            }
         }
 
         private static void AddTimecard()
         {
             Console.WriteLine("Adding timecard...");
 
-            // Pick a person who is submitting the timecard
-            IList people = new ArrayList(GetPersonDao().LoadAll());
-            Person person = (Person)people[random.Next(people.Count)];
-
-            // Create a timecard
-            Timecard timecard = Timecard.Factory.newInstance();
-            timecard.BegDate = DateTime.Now;
-            timecard.Status = TimecardStatus.Draft;
-            person.AddTimecard(timecard);
-            GetTimecardDao().Create(timecard);
-
-            // Add allocations
-            IList tasks = new ArrayList(GetTaskDao().LoadAll());
-            int count = random.Next(1, 4);
-            for (int i = 0; i < count; i++)
+            timecardDao.BeginTransaction();
+            try
             {
-                TimeAllocation timeAllocation = TimeAllocation.Factory.newInstance();
-                timeAllocation.BegTime = timecard.BegDate.Value.Add(new TimeSpan(i, 0, 0));
-                timeAllocation.EndTime = timecard.BegDate.Value.Add(new TimeSpan(i + 1, 0, 0));
-                Task task = (Task)tasks[random.Next(tasks.Count)];
-                task.AddTimeAllocation(timeAllocation);
-                timecard.AddTimeAllocation(timeAllocation);
-                GetTimeAllocationDao().Create(timeAllocation);
+                // Pick a person who is submitting the timecard
+                IList people = new ArrayList(personDao.LoadAll());
+                Person person = (Person)people[random.Next(people.Count)];
+
+                // Instantiate a timecard and persist it
+                Timecard timecard = Timecard.Factory.newInstance();
+                timecard.BegDate = DateTime.Now;
+                timecard.Status = TimecardStatus.Draft;
+                person.AddTimecard(timecard);
+                timecardDao.Create(timecard);
+
+                // Instantiate allocations and persist them
+                IList tasks = new ArrayList(taskDao.LoadAll());
+                int count = random.Next(1, 4);
+                for (int i = 0; i < count; i++)
+                {
+                    TimeAllocation timeAllocation = TimeAllocation.Factory.newInstance();
+                    timeAllocation.BegTime = timecard.BegDate.Value.Add(new TimeSpan(i, 0, 0));
+                    timeAllocation.EndTime = timecard.BegDate.Value.Add(new TimeSpan(i + 1, 0, 0));
+                    Task task = (Task)tasks[random.Next(tasks.Count)];
+                    task.AddTimeAllocation(timeAllocation);
+                    timecard.AddTimeAllocation(timeAllocation);
+                    timeAllocationDao.Create(timecard.Allocations);
+                }
+
+                // Commit the transaction
+                timecardDao.CommitTransaction();
+
+                Console.WriteLine(
+                    "Timecard " + timecard.Id + " created with " +
+                    timecard.Allocations.Count + " allocations");
+            }
+            catch (Exception e)
+            {
+                timecardDao.RollbackTransaction();
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void UpdateTimecard()
+        {
+            // Choose a timecard to update
+            int id = 0;
+            Console.Write("Timecard Id (enter 0 to skip): ");
+            try
+            {
+                id = Convert.ToInt32(Console.ReadLine());
+                if (id == 0) return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+                return;
             }
 
-            Console.WriteLine("Timecard " + timecard.Id + " created.");
+            // Update the timecard
+            timecardDao.BeginTransaction();
+            try
+            {
+                // Get timecard from database
+                Timecard timecard = timecardDao.Load(id);
+                Console.WriteLine("Current value:");
+                ShowTimecard(timecard);
+
+                // Modify timecard values
+                timecard.BegDate = timecard.BegDate.Value.Add(new TimeSpan(1, 0, 0)); ;
+
+                // Modify time allocation values
+                foreach (TimeAllocation timeAllocation in timecard.Allocations)
+                {
+                    timeAllocation.BegTime = timeAllocation.BegTime.Value.Add(new TimeSpan(1, 0, 0));
+                    timeAllocation.EndTime = timeAllocation.EndTime.Value.Add(new TimeSpan(1, 0, 0));
+                }
+
+                // Update timecard in database. Allocations updates should cascade.
+                timecardDao.Update(timecard);
+                timecardDao.CommitTransaction();
+
+                Console.WriteLine("Timecard updated");
+            }
+            catch (Exception e)
+            {
+                timecardDao.RollbackTransaction();
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void DeleteTimecard()
+        {
+            // Choose a timecard to delete
+            int id = 0;
+            Console.Write("Timecard Id (enter 0 to skip): ");
+            try
+            {
+                id = Convert.ToInt32(Console.ReadLine());
+                if (id == 0) return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+                return;
+            }
+
+            // Delete the timecard
+            timecardDao.BeginTransaction();
+            try
+            {
+                timecardDao.Remove(id);
+                timecardDao.CommitTransaction();
+                Console.WriteLine("Timecard removed");
+            }
+            catch (Exception e)
+            {
+                timecardDao.RollbackTransaction();
+                Console.WriteLine(e);
+            }
         }
 
         private static void ShowData()
@@ -98,7 +223,7 @@ namespace TimeTracker
         private static void ShowPeople()
         {
             Console.WriteLine("People:");
-            ICollection people = GetPersonDao().LoadAll();
+            ICollection people = personDao.LoadAll();
             foreach (Person person in people)
             {
                 Console.WriteLine(
@@ -112,7 +237,7 @@ namespace TimeTracker
         private static void ShowTasks()
         {
             Console.WriteLine("Tasks:");
-            ICollection tasks = GetTaskDao().LoadAll();
+            ICollection tasks = taskDao.LoadAll();
             foreach (Task task in tasks)
             {
                 Console.WriteLine(
@@ -124,22 +249,27 @@ namespace TimeTracker
         private static void ShowTimecards()
         {
             Console.WriteLine("Timecards:");
-            ICollection timecards = GetTimecardDao().LoadAll();
+            ICollection timecards = timecardDao.LoadAll();
             foreach (Timecard timecard in timecards)
             {
+                ShowTimecard(timecard);
+            }
+        }
+
+        private static void ShowTimecard(Timecard timecard)
+        {
+            Console.WriteLine(
+                timecard.Id +
+                ": " + timecard.Owner.Name +
+                ", " + timecard.Status +
+                ", " + timecard.BegDate);
+            foreach (TimeAllocation timeAllocation in timecard.Allocations)
+            {
                 Console.WriteLine(
-                    timecard.Id +
-                    ": " + timecard.Owner.Name +
-                    ", " + timecard.Status +
-                    ", " + timecard.BegDate);
-                foreach (TimeAllocation timeAllocation in timecard.Allocations)
-                {
-                    Console.WriteLine(
-                        "    " + timeAllocation.Id +
-                        ": " + timeAllocation.BegTime +
-                        ", " + timeAllocation.EndTime +
-                        ", " + timeAllocation.Task.Name);
-                }
+                    "    " + timeAllocation.Id +
+                    ": " + timeAllocation.BegTime +
+                    ", " + timeAllocation.EndTime +
+                    ", " + timeAllocation.Task.Name);
             }
         }
 
@@ -151,9 +281,12 @@ namespace TimeTracker
             AddPerson,
             AddTask,
             AddTimecard,
+            UpdateTimecard,
+            DeleteTimecard,
             ShowData,
             Quit,
-            ShowMenu
+            ShowMenu,
+            Last
         }
 
         private static void StartApplication()
@@ -179,32 +312,48 @@ namespace TimeTracker
 
                 // Process the user's choice
                 SessionManagerFactory.SessionManager.HandleSessionStart();
-                switch (menuChoice)
+                try
                 {
-                    case MenuChoice.None:
-                        break;
-                    case MenuChoice.CreateDatabase:
-                        CreateDatabase();
-                        break;
-                    case MenuChoice.AddPerson:
-                        AddPerson();
-                        break;
-                    case MenuChoice.AddTask:
-                        AddTask();
-                        break;
-                    case MenuChoice.AddTimecard:
-                        AddTimecard();
-                        break;
-                    case MenuChoice.ShowData:
-                        ShowData();
-                        break;
-                    case MenuChoice.ShowMenu:
-                        ShowMenu();
-                        break;
-                    case MenuChoice.Quit:
-                        break;
+                    switch (menuChoice)
+                    {
+                        case MenuChoice.None:
+                            break;
+                        case MenuChoice.CreateDatabase:
+                            CreateDatabase();
+                            break;
+                        case MenuChoice.AddPerson:
+                            AddPerson();
+                            break;
+                        case MenuChoice.AddTask:
+                            AddTask();
+                            break;
+                        case MenuChoice.AddTimecard:
+                            AddTimecard();
+                            break;
+                        case MenuChoice.UpdateTimecard:
+                            UpdateTimecard();
+                            break;
+                        case MenuChoice.DeleteTimecard:
+                            DeleteTimecard();
+                            break;
+                        case MenuChoice.ShowData:
+                            ShowData();
+                            break;
+                        case MenuChoice.ShowMenu:
+                            ShowMenu();
+                            break;
+                        case MenuChoice.Quit:
+                            break;
+                    }
                 }
-                SessionManagerFactory.SessionManager.HandleSessionEnd();
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                finally
+                {
+                    SessionManagerFactory.SessionManager.HandleSessionEnd();
+                }
             }
         }
 
@@ -213,33 +362,15 @@ namespace TimeTracker
             SessionManagerFactory.SessionManager.HandleApplicationEnd();
         }
 
-        private static IPersonDao GetPersonDao()
-        {
-            return DaoFactory.GetPersonDao();
-        }
-
-        private static ITaskDao GetTaskDao()
-        {
-            return DaoFactory.GetTaskDao();
-        }
-
-        private static ITimecardDao GetTimecardDao()
-        {
-            return DaoFactory.GetTimecardDao();
-        }
-
-        private static ITimeAllocationDao GetTimeAllocationDao()
-        {
-            return DaoFactory.GetTimeAllocationDao();
-        }
-
         private static void ShowMenu()
         {
             Console.WriteLine("1. Create Database");
             Console.WriteLine("2. Add Person");
             Console.WriteLine("3. Add Task");
             Console.WriteLine("4. Add Timecard");
-            Console.WriteLine("5. Show Data");
+            Console.WriteLine("5. Update Timecard");
+            Console.WriteLine("6. Delete Timecard");
+            Console.WriteLine("7. Show Data");
         }
 
         private static MenuChoice GetMenuChoice()
@@ -263,7 +394,7 @@ namespace TimeTracker
                 else
                 {
                     int intChoice = Convert.ToInt32(inputString);
-                    if (intChoice >= 1 && intChoice <= 5)
+                    if (intChoice > (int)MenuChoice.None && intChoice < (int)MenuChoice.Last)
                         menuChoice = (MenuChoice)intChoice;
                 }
             }
