91446a306f55550e6207473cdd127babd54a1682
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-11-24 06:15:51 +0000

    improve performance of normalizing the URIs

--- repositories/emf/src/main/java/org/andromda/repositories/emf/EMFURIConverter.java
+++ repositories/emf/src/main/java/org/andromda/repositories/emf/EMFURIConverter.java
@@ -1,36 +1,49 @@
 package org.andromda.repositories.emf;
 
 import java.io.InputStream;
+
 import java.net.URL;
+
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import org.andromda.core.common.AndroMDALogger;
 import org.andromda.core.common.ResourceUtils;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.impl.URIConverterImpl;
 
+
 /**
  * Extends the default URIConverterImpl to be able to find the discover physical path of URIs when
  * given the moduleSearchPaths.
- * 
+ *
  * @author Chad Brandon
  */
 public class EMFURIConverter
     extends URIConverterImpl
 {
-    
     /**
      * Creates a new instance of EMFURIConvert taking the <code>moduleSearchPaths</code>
-     * as an argument. These are the paths used to attempt to normalize a given URI during 
+     * as an argument. These are the paths used to attempt to normalize a given URI during
      * the call to {@link #normalize(URI)} provided that it couldn't be found in the normal manner.
      * @param moduleSearchPaths
-     */ 
+     */
     public EMFURIConverter(final List moduleSearchPaths)
     {
         this.moduleSearchPaths = moduleSearchPaths;
     }
-    
+
+    /**
+     * Stores the module search paths.
+     */
     private List moduleSearchPaths;
+
+    /**
+     * Stores the URIs that have been normalized.
+     */
+    private final Map normalizedUris = new HashMap();
 
     /**
      * Overwridden
@@ -39,50 +52,69 @@ public class EMFURIConverter
      */
     public URI normalize(URI uri)
     {
-        URI normalizedUri = super.normalize(uri);
-        if (normalizedUri.equals(uri))
+        try
         {
-            final String resourceName = uri.toString().replaceAll(
-                    ".*(\\\\+|/)",
-                    "");
-            if (this.moduleSearchPaths != null)
+            URI normalizedUri = super.normalize(uri);
+            if (normalizedUri.equals(uri))
             {
-                for (final Iterator iterator = this.moduleSearchPaths.iterator(); iterator.hasNext();)
+                final String resourceName = uri.toString().replaceAll(
+                        ".*(\\\\+|/)",
+                        "");
+                if (this.moduleSearchPaths != null)
                 {
-                    String searchPath = (String)iterator.next();
-                    final String completePath = (searchPath + "/" + resourceName).replaceAll(
-                            "\\\\+|/",
-                            "/");
-                    try
+                    if (!normalizedUris.containsKey(uri))
                     {
-                        InputStream stream = null;
-                        URL url = ResourceUtils.toURL(completePath);
-                        try
+                        for (final Iterator iterator = this.moduleSearchPaths.iterator(); iterator.hasNext();)
                         {
-                            stream = url.openStream();
-                            stream.close();
-                            normalizedUri = EMFRepositoryFacadeUtils.createUri(completePath);
-                        }
-                        catch (final Exception exception)
-                        {
-                            url = null;
-                        }
-                        finally
-                        {
-                            stream = null;
+                            String searchPath = (String)iterator.next();
+                            final String completePath = (searchPath + "/" + resourceName).replaceAll(
+                                    "\\\\+|/",
+                                    "/");
+                            try
+                            {
+                                InputStream stream = null;
+                                URL url = ResourceUtils.toURL(completePath);
+                                try
+                                {
+                                    stream = url.openStream();
+                                    stream.close();
+                                    normalizedUri = EMFRepositoryFacadeUtils.createUri(completePath);
+                                    AndroMDALogger.info("referenced model --> '" + normalizedUri + "'");
+                                }
+                                catch (final Exception exception)
+                                {
+                                    url = null;
+                                }
+                                finally
+                                {
+                                    stream = null;
+                                }
+                            }
+                            catch (final Throwable throwable)
+                            {
+                                // - ignore
+                            }
+                            if (normalizedUri != null)
+                            {
+                                this.normalizedUris.put(
+                                    uri,
+                                    normalizedUri);
+                                break;
+                            }
                         }
                     }
-                    catch (final Throwable throwable)
+                    else
                     {
-                        // - ignore
-                    }
-                    if (normalizedUri != null)
-                    {
-                        break;
+                        normalizedUri = (URI)this.normalizedUris.get(uri);
                     }
                 }
             }
+            return normalizedUri;
         }
-        return normalizedUri;
+        catch (Throwable th)
+        {
+            th.printStackTrace();
+            throw new RuntimeException(th);
+        }
     }
 }
