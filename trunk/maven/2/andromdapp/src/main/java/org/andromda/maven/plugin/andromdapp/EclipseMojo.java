0b22bb55bbabbf93bd8c2e9eded9b62433f9d95b
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-11-24 20:06:30 +0000

    add the extra souce directories to the .classpath

--- trunk/maven/2/andromdapp/src/main/java/org/andromda/maven/plugin/andromdapp/EclipseMojo.java
+++ trunk/maven/2/andromdapp/src/main/java/org/andromda/maven/plugin/andromdapp/EclipseMojo.java
@@ -1,16 +1,24 @@
 package org.andromda.maven.plugin.andromdapp;
 
 import java.io.File;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+import org.andromda.core.common.ResourceUtils;
 import org.andromda.maven.plugin.andromdapp.eclipse.ClasspathWriter;
 import org.andromda.maven.plugin.andromdapp.eclipse.ProjectWriter;
+import org.apache.commons.lang.ObjectUtils;
 import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Plugin;
+import org.apache.maven.model.PluginExecution;
+import org.apache.maven.model.PluginManagement;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.profiles.DefaultProfileManager;
@@ -19,6 +27,7 @@ import org.apache.maven.project.MavenProjectBuilder;
 import org.apache.maven.project.ProjectBuildingException;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 import org.codehaus.plexus.util.DirectoryScanner;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
 
 
 /**
@@ -46,14 +55,14 @@ public class EclipseMojo
 
     /**
      * Defines the POMs to include when generating the eclipse files.
-     * 
+     *
      * @parameter
      */
     private String[] includes = new String[] {"*/pom.xml"};
 
     /**
      * Defines the POMs to exclude when generating the eclipse files.
-     * 
+     *
      * @parameter
      */
     private String[] excludes = new String[0];
@@ -101,9 +110,11 @@ public class EclipseMojo
             final List projects = this.collectProjects();
             if (!projects.isEmpty())
             {
-                final ProjectWriter projectWriter = new ProjectWriter(this.project, this.getLog());
+                final ProjectWriter projectWriter = new ProjectWriter(this.project,
+                        this.getLog());
                 projectWriter.write();
-                final ClasspathWriter classpathWriter = new ClasspathWriter(this.project, this.getLog());
+                final ClasspathWriter classpathWriter = new ClasspathWriter(this.project,
+                        this.getLog());
                 classpathWriter.write(
                     this.collectProjects(),
                     this.repositoryVariableName,
@@ -150,6 +161,7 @@ public class EclipseMojo
                         pom,
                         this.session.getLocalRepository(),
                         new DefaultProfileManager(this.session.getContainer()));
+                project.getCompileSourceRoots().addAll(this.getExtraSourceDirectories(project));
                 if (this.getLog().isDebugEnabled())
                 {
                     this.getLog().debug("Adding project " + project.getId());
@@ -163,6 +175,75 @@ public class EclipseMojo
         }
 
         return projects;
+    }
+
+    /**
+     * The artifact id for the multi source plugin.
+     */
+    private static final String MULTI_SOURCE_PLUGIN_ARTIFACT_ID = "andromda-multi-source-plugin";
+
+    /**
+     * Retrieves any additional source directories which are defined within the andromda-multi-source-plugin.
+     *
+     * @param project the maven project from which to retrieve the extra source directories.
+     * @return the list of extra source directories.
+     */
+    private List getExtraSourceDirectories(final MavenProject project)
+    {
+        final List sourceDirectories = new ArrayList();
+        final Build build = project.getBuild();
+        if (build != null)
+        {
+            final PluginManagement pluginManagement = build.getPluginManagement();
+            if (pluginManagement != null && !pluginManagement.getPlugins().isEmpty())
+            {
+                Plugin multiSourcePlugin = null;
+                for (final Iterator iterator = pluginManagement.getPlugins().iterator(); iterator.hasNext();)
+                {
+                    final Plugin plugin = (Plugin)iterator.next();
+                    if (MULTI_SOURCE_PLUGIN_ARTIFACT_ID.equals(plugin.getArtifactId()))
+                    {
+                        multiSourcePlugin = plugin;
+                        break;
+                    }
+                }
+                if (multiSourcePlugin != null && !multiSourcePlugin.getExecutions().isEmpty())
+                {
+                    // - there should only be one execution so we get the first
+                    final PluginExecution execution =
+                        (PluginExecution)multiSourcePlugin.getExecutions().iterator().next();
+                    final Xpp3Dom configuration = (Xpp3Dom)execution.getConfiguration();
+                    if (configuration != null && configuration.getChildCount() > 0)
+                    {
+                        final Xpp3Dom directories = configuration.getChild(0);
+                        if (directories != null)
+                        {
+                            final int childCount = directories.getChildCount();
+                            if (childCount > 0)
+                            {
+                                final Xpp3Dom[] children = directories.getChildren();
+                                for (int ctr = 0; ctr < childCount; ctr++)
+                                {
+                                    final Xpp3Dom child = children[ctr];
+                                    if (child != null)
+                                    {
+                                        final String directoryValue = child.getValue();
+                                        if (directoryValue != null)
+                                        {
+                                            sourceDirectories.add(
+                                                ResourceUtils.normalizePath(
+                                                    ObjectUtils.toString(project.getBasedir()) + '/' +
+                                                    directoryValue.trim()));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return sourceDirectories;
     }
 
     /**
