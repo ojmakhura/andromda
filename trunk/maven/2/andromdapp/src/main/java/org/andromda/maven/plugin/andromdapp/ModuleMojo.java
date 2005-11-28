dc696c8416b37a887c3cf35e94924a12a101b716
cwbrandon <cwbrandon@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-11-28 18:49:00 +0000

    provide the ability to execute goals in sub modules.

--- trunk/maven/2/andromdapp/src/main/java/org/andromda/maven/plugin/andromdapp/ModuleMojo.java
+++ trunk/maven/2/andromdapp/src/main/java/org/andromda/maven/plugin/andromdapp/ModuleMojo.java
@@ -3,8 +3,12 @@ package org.andromda.maven.plugin.andromdapp;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.maven.BuildFailureException;
@@ -21,10 +25,9 @@ import org.apache.maven.project.ProjectBuildingException;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 import org.codehaus.plexus.util.dag.CycleDetectedException;
 
-
 /**
  * A Mojo used for execution modules within a given project.
- *
+ * 
  * @goal module
  * @author Chad Brandon
  */
@@ -48,8 +51,9 @@ public class ModuleMojo
 
     /**
      * A comma seperated list of modules to execute in the form:
-     * <em>-Dmodules=mda,core,common</em> or if you want to specify the
-     * goals to execute as well: <em>-Dmodules=mda=[goal1 goal2],core=[goal1]<em>.
+     * <em>-Dmodules=mda,core,common</em> or if you want to specify the goals
+     * to execute as well:
+     * <em>-Dmodules=mda:[goal1+goal2+goal3],core:[goal1]<em>.
      *
      * @parameter expression="${modules}"
      */
@@ -57,16 +61,18 @@ public class ModuleMojo
 
     /**
      * The default module goals to execute.
-     *
+     * 
      * @parameter
      */
-    private List goals = new ArrayList(Arrays.asList(new String[] {"install"}));
+    private List goals = new ArrayList(Arrays.asList(new String[]
+    {
+        "install"
+    }));
 
     /**
      * @see org.apache.maven.plugin.Mojo#execute()
      */
-    public void execute()
-        throws MojoExecutionException
+    public void execute() throws MojoExecutionException
     {
         try
         {
@@ -79,79 +85,86 @@ public class ModuleMojo
     }
 
     private void executeModules()
-        throws CycleDetectedException, LifecycleExecutionException, MojoExecutionException, BuildFailureException
+        throws CycleDetectedException,
+            LifecycleExecutionException,
+            MojoExecutionException,
+            BuildFailureException
     {
-        final List projects = this.collectProjects();
+        final Map projects = this.collectProjects();
 
         // - only execute if we have some projects
         if (!projects.isEmpty())
         {
-            final ReactorManager reactorManager = new ReactorManager(projects);
-            MavenSession projectSession =
-                new MavenSession(
+            for (final Iterator iterator = projects.keySet().iterator(); iterator.hasNext();)
+            {
+                final MavenProject project = (MavenProject)iterator.next();
+                final List goals = (List)projects.get(project);
+                if (goals.isEmpty())
+                {
+                    goals.addAll(this.goals);
+                }
+                final ReactorManager reactorManager = new ReactorManager(Collections
+                    .singletonList(project));
+                final MavenSession projectSession = new MavenSession(
                     this.session.getContainer(),
                     this.session.getSettings(),
                     this.session.getLocalRepository(),
                     this.session.getEventDispatcher(),
                     reactorManager,
                     goals,
-                    baseDirectory.toString(),
+                    this.baseDirectory.toString(),
                     new Properties(),
                     this.session.getStartTime());
 
-            projectSession.setUsingPOMsFromFilesystem(true);
-            this.lifecycleExecutor.execute(
-                projectSession,
-                reactorManager,
-                projectSession.getEventDispatcher());
+                projectSession.setUsingPOMsFromFilesystem(true);
+                this.lifecycleExecutor.execute(projectSession, reactorManager, projectSession
+                    .getEventDispatcher());
+            }
         }
     }
 
     /**
      * Collects all project modules to execute.
-     *
-     * @return the list of collected projects.
-     *
+     * 
+     * @return the Map of collected projects (the key is the project, the value
+     *         the goals).
      * @throws MojoExecutionException
      */
-    private List collectProjects()
-        throws MojoExecutionException
+    private Map collectProjects() throws MojoExecutionException
     {
-        final List projects = new ArrayList();
+        final Map projects = new LinkedHashMap();
         MavenProjectBuilder projectBuilder;
 
         try
         {
-            projectBuilder = (MavenProjectBuilder)session.getContainer().lookup(MavenProjectBuilder.ROLE);
+            projectBuilder = (MavenProjectBuilder)session.getContainer().lookup(
+                MavenProjectBuilder.ROLE);
         }
         catch (ComponentLookupException exception)
         {
             throw new MojoExecutionException("Cannot get a MavenProjectBuilder", exception);
         }
 
-        final List poms = getModulePoms();
+        final Map poms = getModulePoms();
 
         if (!poms.isEmpty())
         {
-            for (final Iterator iterator = poms.iterator(); iterator.hasNext();)
+            for (final Iterator iterator = poms.keySet().iterator(); iterator.hasNext();)
             {
                 File pom = (File)iterator.next();
 
                 try
                 {
-                    MavenProject project =
-                        projectBuilder.build(
-                            pom,
-                            session.getLocalRepository(),
-                            new DefaultProfileManager(session.getContainer()));
-
+                    final MavenProject project = projectBuilder.build(pom, session
+                        .getLocalRepository(), new DefaultProfileManager(session.getContainer()));
                     getLog().debug("Adding project " + project.getId());
-
-                    projects.add(project);
+                    projects.put(project, poms.get(pom));
                 }
                 catch (ProjectBuildingException exception)
                 {
-                    throw new MojoExecutionException("Error loading POM --> '" + pom + "'", exception);
+                    throw new MojoExecutionException(
+                        "Error loading POM --> '" + pom + "'",
+                        exception);
                 }
             }
         }
@@ -160,24 +173,40 @@ public class ModuleMojo
 
     /**
      * Gets all POMs for the modules specified.
-     *
+     * 
      * @return the list of module poms
      */
-    private List getModulePoms()
+    private Map getModulePoms()
     {
-        final List poms = new ArrayList();
+        final Map poms = new LinkedHashMap();
         final String[] modules = this.modules != null ? this.modules.split(",") : null;
 
+        final String goalPrefix = ":";
         if (modules != null)
         {
             final int numberOfModules = modules.length;
             for (int ctr = 0; ctr < numberOfModules; ctr++)
             {
-                final String module = modules[ctr].trim();
+                String module = modules[ctr].trim();
+                final List goalsList = new ArrayList();
+                if (module.indexOf(goalPrefix) != -1)
+                {
+                    String[] goals = module.replaceAll(".*(:\\[)|(\\])", "").split("\\+");
+                    if (goals != null)
+                    {
+                        final int numberOfGoals = goals.length;
+                        for (int ctr2 = 0; ctr2 < numberOfGoals; ctr2++)
+                        {
+                            final String goal = goals[ctr2].trim();
+                            goalsList.add(goal);
+                        }
+                    }
+                }
+                module = module.replaceAll(goalPrefix + "\\[.*\\]", "");
                 final File pom = new File(this.baseDirectory, module + "/pom.xml");
                 if (pom.isFile())
                 {
-                    poms.add(pom);
+                    poms.put(pom, goalsList);
                 }
             }
         }
