e38322a5479fe56a3496436b0c0a8020cee63694
freter <freter@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-12-13 08:07:14 +0000

    Suport for styles.

--- android/org.andromda.android.ui/src/org/andromda/android/ui/internal/editor/BaseSectionPart.java
+++ android/org.andromda.android.ui/src/org/andromda/android/ui/internal/editor/BaseSectionPart.java
@@ -7,6 +7,7 @@ import org.eclipse.ui.forms.editor.FormPage;
 import org.eclipse.ui.forms.widgets.Section;
 
 /**
+ * A basic section part hat features easy access to the editor and the hosting editor page.
  *
  * @author Peter Friese
  * @since 09.12.2005
@@ -18,20 +19,52 @@ public class BaseSectionPart
     /** The editor page owning this section. */
     private final FormPage page;
 
+    /**
+     * Creates a new section part.
+     *
+     * @param parent The parent composite.
+     * @param page The page that hosts this section.
+     * @param style The SWT style for the section.
+     */
     public BaseSectionPart(Composite parent,
-        FormPage page)
+        FormPage page, int style)
     {
-        super(parent, page.getManagedForm().getToolkit(), Section.DESCRIPTION
-                | Section.TITLE_BAR);
+        super(parent, page.getManagedForm().getToolkit(), style);
         this.page = page;
 
     }
 
+    /**
+     * Creates a new section part.
+     *
+     * @param parent The parent composite.
+     * @param page The page that hosts this section.
+     */
+    public BaseSectionPart(Composite parent,
+        FormPage page)
+    {
+        this(parent, page, Section.DESCRIPTION | Section.TITLE_BAR);
+    }
+
+    /**
+     * Creates a new section part.
+     *
+     * @param page The page that hosts this section.
+     * @param style The SWT style of the section.
+     */
+    public BaseSectionPart(FormPage page, int style)
+    {
+        this(page.getManagedForm().getForm().getBody(), page, style);
+    }
+
+    /**
+     * Creates a new section part.
+     *
+     * @param page The page that hosts this section.
+     */
     public BaseSectionPart(FormPage page)
     {
-        super(page.getManagedForm().getForm().getBody(), page.getManagedForm().getToolkit(), Section.DESCRIPTION
-                | Section.TITLE_BAR);
-        this.page = page;
+        this(page.getManagedForm().getForm().getBody(), page);
     }
 
     /**
@@ -43,7 +76,7 @@ public class BaseSectionPart
     }
 
     /**
-     * @return
+     * @return The editor.
      */
     protected FormEditor getEditor()
     {
