ee6401d665c93a3246ffcd48691cc8ec2c748f80
nbhatia <nbhatia@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2005-12-13 14:02:02 +0000

    Simplified handling of one-to-one associations by simplifying isOneToOnePrimary() and dropping isOneToOneSecondary(). Also added the generation of foreign key constraints at the appropriate end.

--- trunk/contrib/cartridges/andromda-nhibernate/src/java/org/andromda/cartridges/nhibernate/metafacades/HibernateAssociationEndLogicImpl.java
+++ trunk/contrib/cartridges/andromda-nhibernate/src/java/org/andromda/cartridges/nhibernate/metafacades/HibernateAssociationEndLogicImpl.java
@@ -105,21 +105,7 @@ public class HibernateAssociationEndLogicImpl
      */
     protected boolean handleIsOne2OnePrimary()
     {
-        boolean primaryOne2One = super.isOne2One();
-        HibernateAssociationEnd otherEnd = (HibernateAssociationEnd)this.getOtherEnd();
-
-        if (primaryOne2One)
-        {
-            primaryOne2One = super.isAggregation() || this.isComposition();
-        }
-
-        // if the flag is false delegate to the super class
-        if (!primaryOne2One)
-        {
-            primaryOne2One = super.isOne2One() && !otherEnd.isAggregation() && !otherEnd.isComposition();
-        }
-
-        return primaryOne2One;
+        return ( this.isOne2One() && (this.isAggregation() || this.isComposition()) );
     }
 
     /**
@@ -212,29 +198,6 @@ public class HibernateAssociationEndLogicImpl
         }
 
         return lazy;
-    }
-
-    /**
-     * @see org.andromda.cartridges.nhibernate.metafacades.HibernateEntityAssociationEnd#isOne2OneSecondary()
-     */
-    protected boolean handleIsOne2OneSecondary()
-    {
-        boolean secondary = false;
-        Object type = this.getType();
-        Object otherType = this.getOtherEnd().getType();
-
-        if ((type != null) && HibernateEntity.class.isAssignableFrom(type.getClass()) && (otherType != null) &&
-            HibernateEntity.class.isAssignableFrom(otherType.getClass()))
-        {
-            HibernateEntity entity = (HibernateEntity)type;
-            HibernateEntity otherEntity = (HibernateEntity)otherType;
-            secondary =
-                (this.isChild() && entity.isForeignHibernateGeneratorClass()) ||
-                otherEntity.isForeignHibernateGeneratorClass() ||
-                (!this.isNavigable() && this.getOtherEnd().isNavigable() && !this.isOne2OnePrimary());
-        }
-
-        return secondary;
     }
 
     /**
