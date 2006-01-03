4b6fd2c42a62fa9a3c0aaeff3e68f6b8ce8a7753
vancek <vancek@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2006-01-03 01:33:44 +0000

    refactored getAttributesAsList out to the session facade

--- cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/EJB3ScriptHelper.java
+++ cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/EJB3ScriptHelper.java
@@ -1,6 +1,7 @@
 package org.andromda.cartridges.ejb3;
 
 import org.andromda.cartridges.ejb3.metafacades.EJB3EntityAttributeFacade;
+import org.andromda.cartridges.ejb3.metafacades.EJB3SessionFacade;
 import org.andromda.metafacades.uml.AttributeFacade;
 import org.andromda.metafacades.uml.ModelElementFacade;
 
@@ -18,50 +19,6 @@ import java.util.StringTokenizer;
  */
 public class EJB3ScriptHelper
 {
-
-    /**
-     * Create a comma seperated list of attributes.
-     * <p/>
-     * This method can be used to generated e.g. argument lists for constructors, method calls etc.
-     * <p/>
-     * It will not return attributes tagged to be optimistic lock values.
-     *
-     * @param attributes   a collection of {@link Attribute} objects
-     * @param includeTypes if <code>true</code>, the type names of the attributes are included.
-     * @param includeNames if <code>true</code>, the names of the attributes are included
-     */
-    public String getAttributesAsList(Collection attributes, boolean includeTypes, boolean includeNames)
-    {
-        if (!includeNames && !includeTypes || attributes == null)
-        {
-            return "";
-        }
-
-        StringBuffer sb = new StringBuffer();
-        String separator = "";
-
-        for (final Iterator it = attributes.iterator(); it.hasNext();)
-        {
-            // AttributeFacade attr = (AttributeFacade)it.next();
-        	EJB3EntityAttributeFacade attr = (EJB3EntityAttributeFacade)it.next();
-            if (!attr.isVersion())
-            {
-	            sb.append(separator);
-	            separator = ", ";
-	            if (includeTypes)
-	            {
-	                sb.append(attr.getType().getFullyQualifiedName());
-	                sb.append(" ");
-	            }
-	            if (includeNames)
-	            {
-	                sb.append(attr.getName());
-	            }
-            }
-        }
-        return sb.toString();
-    }
-
     /**
      * Create a collection of String objects representing the argument names.
      * 
