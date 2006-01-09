af59c2520a62ae37ec83652022930c34dea114d5
freter <freter@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2006-01-09 06:31:22 +0000

    Better handling in case WTP is not available.

--- android/org.andromda.android.ui.editor/src/org/andromda/android/ui/editor/AbstractMultipageSourceEditor.java
+++ android/org.andromda.android.ui.editor/src/org/andromda/android/ui/editor/AbstractMultipageSourceEditor.java
@@ -1,11 +1,15 @@
 package org.andromda.android.ui.editor;
 
+import org.andromda.android.ui.EditorUIPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.editor.IFormPage;
 import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.osgi.framework.Bundle;
 
 /**
  * Abstract forms editor with a source page.
@@ -17,17 +21,11 @@ public abstract class AbstractMultipageSourceEditor
         extends FormEditor
 {
 
+    /** The source editor. */
     private TextEditor editor;
 
+    /** The page index of the source editor. */
     private int sourcePageIndex;
-
-    /**
-     *
-     */
-    public AbstractMultipageSourceEditor()
-    {
-        super();
-    }
 
     /**
      * @see org.eclipse.ui.forms.editor.FormEditor#addPages()
@@ -41,17 +39,19 @@ public abstract class AbstractMultipageSourceEditor
         }
         catch (PartInitException e)
         {
-            // TODO: handle exception
+            EditorUIPlugin.log(e);
         }
     }
 
     /**
-     * @throws PartInitException
-     *
+     * @throws PartInitException if one or more pages could not be added.
      */
     protected abstract void addFormPages() throws PartInitException;
 
-    public int addPage(IFormPage page) throws PartInitException
+    /**
+     * @see org.eclipse.ui.forms.editor.FormEditor#addPage(org.eclipse.ui.forms.editor.IFormPage)
+     */
+    public int addPage(final IFormPage page) throws PartInitException
     {
         int index = getPageCount() - 1;
         addPage(index, page);
@@ -61,7 +61,7 @@ public abstract class AbstractMultipageSourceEditor
     /**
      * Creates the source page.
      *
-     * @throws PartInitException
+     * @throws PartInitException in case the source page could not be initialized correctly
      */
     private void addSourcePage() throws PartInitException
     {
@@ -72,7 +72,7 @@ public abstract class AbstractMultipageSourceEditor
     }
 
     /**
-     * @return
+     * @return the source editor
      */
     private TextEditor getSourceEditor()
     {
@@ -83,17 +83,75 @@ public abstract class AbstractMultipageSourceEditor
         return editor;
     }
 
+    /**
+     * Finds out whether the XML source editor plug-in is available.
+     *
+     * @return <code>true</code> if the XML editor plug-in is abailable, <code>false</code> if not.
+     */
+    private boolean isXMLEditorAvailable()
+    {
+        Bundle bundle = Platform.getBundle("org.eclipse.wst.sse.ui");
+        return bundle != null;
+    }
+
+    /**
+     * Instantiates the source editor. If the XML source editor is available, it will be instantiated. If no XML source
+     * editor is available, the default TextEditor will be instantiated.
+     *
+     * @return either the XML source editor or the TextEditor.
+     */
     protected TextEditor instantiateSourceEditor()
     {
-        return new TextEditor();
+        TextEditor result = null;
+        if (isXMLEditorAvailable())
+        {
+            result = instantiateXMLEditor();
+        }
+
+        if (result == null)
+        {
+            result = new TextEditor();
+        }
+
+        return result;
+    }
+
+    /**
+     * Instantiates the XML source editor. This is done using reflection in order to avoid importing the needed type. If
+     * the type cannot be found, <code>null</code> will bed returned.
+     *
+     * @return an instance of {@link org.eclipse.wst.sse.ui.StructuredTextEditor}
+     */
+    protected TextEditor instantiateXMLEditor()
+    {
+        TextEditor result = null;
+        try
+        {
+            Class clazz = Class.forName("org.eclipse.wst.sse.ui.StructuredTextEditor");
+            result = (TextEditor)clazz.newInstance();
+        }
+        catch (ClassNotFoundException e)
+        {
+            EditorUIPlugin.log(e);
+        }
+        catch (InstantiationException e)
+        {
+            EditorUIPlugin.log(e);
+        }
+        catch (IllegalAccessException e)
+        {
+            EditorUIPlugin.log(e);
+        }
+        return result;
     }
 
     /**
      * @see org.eclipse.ui.part.EditorPart#doSave(org.eclipse.core.runtime.IProgressMonitor)
      */
-    public void doSave(IProgressMonitor monitor)
+    public void doSave(final IProgressMonitor monitor)
     {
         getSourceEditor().doSave(monitor);
+        editorDirtyStateChanged();
     }
 
     /**
@@ -102,6 +160,7 @@ public abstract class AbstractMultipageSourceEditor
     public void doSaveAs()
     {
         getSourceEditor().doSaveAs();
+        editorDirtyStateChanged();
     }
 
     /**
@@ -115,13 +174,40 @@ public abstract class AbstractMultipageSourceEditor
     /**
      * @see org.eclipse.ui.part.WorkbenchPart#getAdapter(java.lang.Class)
      */
-    public Object getAdapter(Class adapter)
+    public Object getAdapter(final Class adapter)
     {
+        // get the document provider by asking the source editor
         if (IDocumentProvider.class.equals(adapter))
         {
-            return getSourceEditor().getDocumentProvider();
+            TextEditor sourceEditor = getSourceEditor();
+            if (sourceEditor != null)
+            {
+                return sourceEditor.getDocumentProvider();
+            }
+            return null;
+        }
+
+        // get the document by asking the source editor
+        if (IDocument.class.equals(adapter))
+        {
+
+            IDocumentProvider documentProvider = (IDocumentProvider)getAdapter(IDocumentProvider.class);
+            if (documentProvider != null)
+            {
+                return documentProvider.getDocument(getEditorInput());
+            }
         }
         return super.getAdapter(adapter);
+    }
+
+    /**
+     * Retrieves the document.
+     *
+     * @return the document being edited.
+     */
+    public IDocument getDocument()
+    {
+        return (IDocument)getAdapter(IDocument.class);
     }
 
 }
