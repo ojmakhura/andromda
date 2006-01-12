0d407050c7c017f7c28f67e00847f8b1f1c98b63
freter <freter@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2006-01-12 23:22:31 +0000

    Synchronisation between editor and model.

--- trunk/android/org.andromda.android.core/src/org/andromda/android/core/internal/model/configuration/AndromdaDocumentEditorModel.java
+++ trunk/android/org.andromda.android.core/src/org/andromda/android/core/internal/model/configuration/AndromdaDocumentEditorModel.java
@@ -1,8 +1,15 @@
 package org.andromda.android.core.internal.model.configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.andromda.android.core.AndroidCore;
 import org.andromda.android.core.internal.model.AbstractEditorModel;
 import org.andromda.android.core.model.configuration.IAndromdaDocumentEditorModel;
 import org.andromda.core.configuration.AndromdaDocument;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlOptions;
+import org.eclipse.jface.text.IDocument;
 
 /**
  * A model wrapper for the AndroMDA configuration document.
@@ -16,18 +23,74 @@ public class AndromdaDocumentEditorModel
 {
 
     /** The AndroMDA configuration document wrapper by this model wrapper. */
-    private final AndromdaDocument andromdaDocument;
+    private AndromdaDocument andromdaDocument;
+
+    /** The number of spaces rendered in the XML file for one indentation. */
+    private static final int NUMBER_OF_SPACES = 4;
 
     /**
      * Creates a new model wrapper for the given AndroMDA configuration document.
+     * 
+     * @param document the document to wrap.
      */
-    public AndromdaDocumentEditorModel(final AndromdaDocument andromdaDocument)
+    public AndromdaDocumentEditorModel(final IDocument document)
     {
-        this.andromdaDocument = andromdaDocument;
+        super(document);
     }
 
     /**
-     * @see org.andromda.android.core.model.configuration.IAndromdaDocumentEditorModel#getAndromdaDocument()
+     * Parses the document and updates the model.
+     * 
+     * @param document the document to parse.
+     */
+    protected void parseDocument(final IDocument document)
+    {
+        try
+        {
+            XmlOptions options = setupXmlOptions();
+            andromdaDocument = AndromdaDocument.Factory.parse(document.get(), options);
+        }
+        catch (XmlException e)
+        {
+            AndroidCore.log(e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected void updateDocument()
+    {
+        IDocument document = getDocument();
+        if (document != null)
+        {
+            if (andromdaDocument != null)
+            {
+                document.set(andromdaDocument.toString());
+            }
+        }
+    }
+
+    /**
+     * Setup an XmlOptions instance so the parser will assume the default namespace for the config document even if is
+     * has no namespace set.
+     * 
+     * @return an XmlOptions instance suitable for parsing AndroMDA configuration documents.
+     */
+    private XmlOptions setupXmlOptions()
+    {
+        XmlOptions options = new XmlOptions();
+        Map namespaceMapping = new HashMap();
+        namespaceMapping.put("", "http://andromda.org/core/configuration");
+        options.setLoadSubstituteNamespaces(namespaceMapping);
+
+        options.setUseDefaultNamespace();
+        options.setSavePrettyPrint().setSavePrettyPrintIndent(NUMBER_OF_SPACES);
+        return options;
+    }
+
+    /**
+     * {@inheritDoc}
      */
     public AndromdaDocument getAndromdaDocument()
     {
