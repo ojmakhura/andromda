130c409aa5ae6a2f0b428fcf38ad306822af6be5
vancek <vancek@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2006-01-25 03:01:00 +0000

    initial revision

---
+++ cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/metafacades/EJB3EnumerationFacadeLogicImpl.java
@@ -0,0 +1,17 @@
+package org.andromda.cartridges.ejb3.metafacades;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.ejb3.metafacades.EJB3EnumerationFacade.
+ *
+ * @see org.andromda.cartridges.ejb3.metafacades.EJB3EnumerationFacade
+ */
+public class EJB3EnumerationFacadeLogicImpl
+    extends EJB3EnumerationFacadeLogic
+{
+
+    public EJB3EnumerationFacadeLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+}
