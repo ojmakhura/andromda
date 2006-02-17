340582de78a32178c87747e2989c5d6ad3ceaa84
vancek <vancek@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2006-02-17 02:38:22 +0000

    added getTemporalType - metafacade engine currently uses EJB3EntityAttributeFacade rather than EJB3ManageableEntityAttributeFacade

--- trunk/cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/metafacades/EJB3ManageableEntityAttributeFacadeLogicImpl.java
+++ trunk/cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/metafacades/EJB3ManageableEntityAttributeFacadeLogicImpl.java
@@ -1,5 +1,9 @@
 package org.andromda.cartridges.ejb3.metafacades;
 
+import org.andromda.cartridges.ejb3.EJB3Globals;
+import org.andromda.cartridges.ejb3.EJB3Profile;
+import org.apache.commons.lang.StringUtils;
+
 
 /**
  * MetafacadeLogic implementation for org.andromda.cartridges.ejb3.metafacades.EJB3ManageableEntityAttributeFacade.
@@ -9,9 +13,26 @@ package org.andromda.cartridges.ejb3.metafacades;
 public class EJB3ManageableEntityAttributeFacadeLogicImpl
     extends EJB3ManageableEntityAttributeFacadeLogic
 {
-
+    /**
+     * The property that stores the defuult temporal type for date based attributes
+     */
+    public static final String ENTITY_DEFAULT_TEMPORAL_TYPE = "entityDefaultTemporalType";
+    
     public EJB3ManageableEntityAttributeFacadeLogicImpl (Object metaObject, String context)
     {
         super (metaObject, context);
+    }
+
+    /**
+     * @see org.andromda.cartridges.ejb3.metafacades.EJB3ManageableEntityAttributeFacadeLogic#handleGetTemporalType()
+     */
+    protected String handleGetTemporalType()
+    {
+        String temporalType = (String)this.findTaggedValue(EJB3Profile.TAGGEDVALUE_PERSISTENCE_TEMPORAL_TYPE);
+        if (StringUtils.isBlank(temporalType))
+        {
+            temporalType = String.valueOf(this.getConfiguredProperty(ENTITY_DEFAULT_TEMPORAL_TYPE));
+        }
+        return temporalType;
     }
 }
