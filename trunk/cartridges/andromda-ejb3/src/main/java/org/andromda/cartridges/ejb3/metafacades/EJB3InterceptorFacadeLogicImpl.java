5acbd1544851958092e73459a72ce02efbd5df19
vancek <vancek@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2006-03-09 04:41:41 +0000

    implemented isDefaultInterceptor and getInterceptorReferences

--- trunk/cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/metafacades/EJB3InterceptorFacadeLogicImpl.java
+++ trunk/cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/metafacades/EJB3InterceptorFacadeLogicImpl.java
@@ -1,7 +1,18 @@
 package org.andromda.cartridges.ejb3.metafacades;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 
+import org.andromda.cartridges.ejb3.EJB3Profile;
+import org.andromda.metafacades.uml.DependencyFacade;
+import org.andromda.metafacades.uml.ModelElementFacade;
+import org.apache.commons.collections.Closure;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+import org.apache.commons.collections.Transformer;
 import org.apache.commons.lang.StringUtils;
 
 
@@ -44,6 +55,56 @@ public class EJB3InterceptorFacadeLogicImpl
                 this.getPackageName(),
                 this.getInterceptorName(),
                 null);
+    }
+
+    /**
+     * @see org.andromda.cartridges.ejb3.metafacades.EJB3InterceptorFacadeLogic#handleGetInterceptorReferences()
+     */
+    protected Collection handleGetInterceptorReferences()
+    {
+        Collection references = this.getSourceDependencies();
+        CollectionUtils.filter(
+            references, 
+            new Predicate()
+            {
+                public boolean evaluate(Object object)
+                {
+                    DependencyFacade dependency = (DependencyFacade)object;
+                    ModelElementFacade targetElement = dependency.getTargetElement();
+                    return (targetElement != null && targetElement.hasStereotype(EJB3Profile.STEREOTYPE_INTERCEPTOR));
+                }
+            });
+        CollectionUtils.transform(
+            references, 
+            new Transformer()
+            {
+                public Object transform(final Object object)
+                {
+                    return ((DependencyFacade)object).getTargetElement();
+                }
+            });
+        final Collection interceptors = new LinkedHashSet(references);
+        CollectionUtils.forAllDo(
+            references,
+            new Closure()
+            {
+                public void execute(Object object)
+                {
+                    if (object instanceof EJB3InterceptorFacade)
+                    {
+                        interceptors.addAll(((EJB3InterceptorFacade)object).getInterceptorReferences());
+                    }
+                }
+            });
+        return interceptors;
+    }
+
+    /**
+     * @see org.andromda.cartridges.ejb3.metafacades.EJB3InterceptorFacadeLogic#handleIsDefaultInterceptor()
+     */
+    protected boolean handleIsDefaultInterceptor()
+    {
+        return this.hasStereotype(EJB3Profile.STEREOTYPE_DEFAULT_INTERCEPTOR);
     }
 
 }
