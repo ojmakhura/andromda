5dc9f7ab47189c8872af863056a3d0ed6ac9250a
vancek <vancek@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2006-04-24 02:54:56 +0000

    refactored handleGetQuery - extracted translation from OCL to method and incorporated hibernate query implementation

--- trunk/cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/metafacades/EJB3FinderMethodFacadeLogicImpl.java
+++ trunk/cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/metafacades/EJB3FinderMethodFacadeLogicImpl.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 
 import org.andromda.cartridges.ejb3.EJB3Globals;
 import org.andromda.cartridges.ejb3.EJB3Profile;
+import org.andromda.metafacades.uml.ModelElementFacade;
 import org.andromda.metafacades.uml.ParameterFacade;
 import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
@@ -37,65 +38,28 @@ public class EJB3FinderMethodFacadeLogicImpl
     // --------------- methods ---------------------
     
     /**
+     * Stores the translated query so that its only translated once.
+     */
+    private String translatedQuery = null;
+
+    /**
+     * Retrieves the translated query.
+     */
+    private String getTranslatedQuery()
+    {
+        if (this.translatedQuery == null)
+        {
+            this.translatedQuery = super.getQuery("query.Hibernate-QL");
+        }
+        return this.translatedQuery;
+    }
+    
+    /**
      * @see org.andromda.cartridges.ejb3.metafacades.EJB3FinderMethodFacade#getQuery()
      */
     protected java.lang.String handleGetQuery()
     {
-        // first see if there is a query stored as a constraint
-        String queryString = super.getQuery("query.Hibernate-QL");
-
-        // otherwise see if there is a query stored as a tagged value
-        if (StringUtils.isEmpty(queryString))
-        {
-            Object value = this.findTaggedValue(EJB3Profile.TAGGEDVALUE_EJB_QUERY);
-            queryString = (String)value;
-            if (queryString != null)
-            {
-                // remove any excess whitespace
-                queryString = queryString.replaceAll("[$\\s]+", " ");
-            }
-        }
-
-        // if there wasn't any stored query, create one by default.
-        if (StringUtils.isEmpty(queryString))
-        {
-            String variableName = StringUtils.uncapitalize(this.getOwner().getName());
-            queryString = "FROM " + this.getOwner().getName() + " AS " + variableName;
-            if (this.getArguments().size() > 0)
-            {
-                final Collection parameters = this.getArguments();
-                if (parameters != null && !parameters.isEmpty())
-                {
-                    Iterator parameterIt = parameters.iterator();
-                    for (int ctr = 0; parameterIt.hasNext(); ctr++)
-                    {
-                        EJB3FinderMethodArgumentFacade param = (EJB3FinderMethodArgumentFacade)parameterIt.next();
-                        if (!param.isFirstResult() && !param.isMaxResults()) 
-                        {
-                            if (ctr == 0)
-                            {
-                                queryString = queryString + " WHERE";
-                            }
-                            String parameter = "?";
-                            if (this.isUseNamedParameters()) 
-                            {
-                                parameter = ":" + param.getName();
-                            }
-                            else
-                            {
-                                parameter = parameter + ctr;
-                            }
-                            queryString = queryString + " " + variableName + "." + param.getName() + " = " + parameter;
-                            if (parameterIt.hasNext())
-                            {
-                                queryString = queryString + " AND";
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return queryString;
+        return this.getQuery((EJB3EntityFacade)null);
     }
 
     /**
@@ -127,6 +91,68 @@ public class EJB3FinderMethodFacadeLogicImpl
             queryCacheEnabled = BooleanUtils.toBoolean(queryCacheEnabledStr);
         }
         return queryCacheEnabled;
+    }
+
+    /**
+     * @see org.andromda.cartridges.ejb3.metafacades.EJB3FinderMethodFacadeLogic#handleGetQuery(org.andromda.cartridges.ejb3.metafacades.EJB3EntityFacade)
+     */
+    protected String handleGetQuery(EJB3EntityFacade entity)
+    {
+        // first see if we can retrieve the query from the super class as an OCL
+        // translation
+        String queryString = this.getTranslatedQuery();
+
+        // otherwise see if there is a query stored as a tagged value
+        if (StringUtils.isEmpty(queryString))
+        {
+            Object value = this.findTaggedValue(EJB3Profile.TAGGEDVALUE_EJB_QUERY);
+            queryString = (String)value;
+            if (queryString != null)
+            {
+                // remove any excess whitespace
+                queryString = queryString.replaceAll("[$\\s]+", " ");
+            }
+        }
+
+        // if there wasn't any stored query, create one by default.
+        if (StringUtils.isEmpty(queryString))
+        {
+            ModelElementFacade owner;
+            if (entity == null)
+            {
+                owner = this.getOwner();
+            }
+            else
+            {
+                owner = entity;
+            }
+            String variableName = StringUtils.uncapitalize(owner.getName());
+            queryString = "from " + owner.getName() + " as " + variableName;
+            if (this.getArguments().size() > 0)
+            {
+                queryString = queryString + " where";
+                Collection arguments = this.getArguments();
+                if (arguments != null && !arguments.isEmpty())
+                {
+                    Iterator argumentIt = arguments.iterator();
+                    for (int ctr = 0; argumentIt.hasNext(); ctr++)
+                    {
+                        ParameterFacade argument = (ParameterFacade)argumentIt.next();
+                        String parameter = "?";
+                        if (this.isUseNamedParameters())
+                        {
+                            parameter = ":" + argument.getName();
+                        }
+                        queryString = queryString + " " + variableName + "." + argument.getName() + " = " + parameter;
+                        if (argumentIt.hasNext())
+                        {
+                            queryString = queryString + " and";
+                        }
+                    }
+                }
+            }
+        }
+        return queryString;
     }
 
 }
