8788a9ff92e006b4f72e0d27d0e10e592a77c751
vancek <vancek@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2006-07-10 05:29:37 +0000

    reset EJB3ManageableEntityAttributeFacade metafacade mapping to use context element refactored getTemporalType in EJB3EntityAttributeFacade and removed overriding instance in EJB3ManageableEntityAttributeFacade

--- trunk/cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/metafacades/EJB3ManageableEntityAttributeFacadeLogicImpl.java
+++ trunk/cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/metafacades/EJB3ManageableEntityAttributeFacadeLogicImpl.java
@@ -13,26 +13,8 @@ import org.apache.commons.lang.StringUtils;
 public class EJB3ManageableEntityAttributeFacadeLogicImpl
     extends EJB3ManageableEntityAttributeFacadeLogic
 {
-    /**
-     * The property that stores the defuult temporal type for date based attributes
-     */
-    public static final String ENTITY_DEFAULT_TEMPORAL_TYPE = "entityDefaultTemporalType";
-    
     public EJB3ManageableEntityAttributeFacadeLogicImpl (Object metaObject, String context)
     {
         super (metaObject, context);
-    }
-
-    /**
-     * @see org.andromda.cartridges.ejb3.metafacades.EJB3ManageableEntityAttributeFacadeLogic#getTemporalType()
-     */
-    public String getTemporalType()
-    {
-        String temporalType = super.getTemporalType();
-        if (StringUtils.isBlank(temporalType))
-        {
-            temporalType = String.valueOf(this.getConfiguredProperty(ENTITY_DEFAULT_TEMPORAL_TYPE));
-        }
-        return temporalType;
     }
 }
