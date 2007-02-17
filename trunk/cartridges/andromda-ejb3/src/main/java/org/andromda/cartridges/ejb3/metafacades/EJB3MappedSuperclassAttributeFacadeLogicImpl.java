68637cd40ef82f228676e96027a8c5add0aeb7f5
vancek <vancek@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2007-02-17 14:09:09 +0000

    add EJB3MappedSuperclassFacade and EJB3MappedSuperclassAttributeFacade metafacades. No longer need to model an Entity stereotype with a MappedSuperclass stereotype on the parent class.  reflect changes in docs and override validation constraints.

---
+++ trunk/cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/metafacades/EJB3MappedSuperclassAttributeFacadeLogicImpl.java
@@ -0,0 +1,17 @@
+package org.andromda.cartridges.ejb3.metafacades;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.ejb3.metafacades.EJB3MappedSuperclassAttributeFacade.
+ *
+ * @see org.andromda.cartridges.ejb3.metafacades.EJB3MappedSuperclassAttributeFacade
+ */
+public class EJB3MappedSuperclassAttributeFacadeLogicImpl
+    extends EJB3MappedSuperclassAttributeFacadeLogic
+{
+
+    public EJB3MappedSuperclassAttributeFacadeLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+}
---
+++ trunk/cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/metafacades/EJB3MappedSuperclassFacadeLogicImpl.java
@@ -0,0 +1,16 @@
+package org.andromda.cartridges.ejb3.metafacades;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.ejb3.metafacades.EJB3MappedSuperclassFacade.
+ *
+ * @see org.andromda.cartridges.ejb3.metafacades.EJB3MappedSuperclassFacade
+ */
+public class EJB3MappedSuperclassFacadeLogicImpl
+    extends EJB3MappedSuperclassFacadeLogic
+{
+    public EJB3MappedSuperclassFacadeLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+}
