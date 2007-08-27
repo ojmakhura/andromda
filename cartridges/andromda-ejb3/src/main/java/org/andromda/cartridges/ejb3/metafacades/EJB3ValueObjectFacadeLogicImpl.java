b86ed6c6ded57dbbfc52867411abcd4270138e67
vancek <vancek@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2007-08-27 14:19:12 +0000

    jira ejb-82 - support value objects as Seam components.  added EJB3ValueObjectFacade.

---
+++ cartridges/andromda-ejb3/src/main/java/org/andromda/cartridges/ejb3/metafacades/EJB3ValueObjectFacadeLogicImpl.java
@@ -0,0 +1,42 @@
+package org.andromda.cartridges.ejb3.metafacades;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.ejb3.metafacades.EJB3ValueObjectFacade.
+ *
+ * @see org.andromda.cartridges.ejb3.metafacades.EJB3ValueObjectFacade
+ */
+public class EJB3ValueObjectFacadeLogicImpl
+    extends EJB3ValueObjectFacadeLogic
+{
+
+    public EJB3ValueObjectFacadeLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+
+    /**
+     * @see org.andromda.cartridges.ejb3.metafacades.EJB3ValueObjectFacade#isSeamComponent()
+     */
+    protected boolean handleIsSeamComponent()
+    {
+        return EJB3MetafacadeUtils.isSeamComponent(this);
+    }
+
+    /**
+     * @see org.andromda.cartridges.ejb3.metafacades.EJB3ValueObjectFacade#getSeamComponentName()
+     */
+    protected java.lang.String handleGetSeamComponentName()
+    {
+        return EJB3MetafacadeUtils.getSeamComponentName(this);
+    }
+
+    /**
+     * @see org.andromda.cartridges.ejb3.metafacades.EJB3ValueObjectFacade#getSeamComponentScopeType()
+     */
+    protected java.lang.String handleGetSeamComponentScopeType()
+    {
+        return EJB3MetafacadeUtils.getSeamComponentScopeType(this, false);
+    }
+
+}
