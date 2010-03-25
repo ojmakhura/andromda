9758ccff4018a4449ed71d961adb6bd2f51c44c3
catforce <catforce@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2010-03-25 22:51:07 +0000

    added second parameter for PostProcessor method (to be used in the feature)

--- trunk/andromda-core/src/main/java/org/andromda/core/common/PostProcessor.java
+++ trunk/andromda-core/src/main/java/org/andromda/core/common/PostProcessor.java
@@ -9,16 +9,18 @@ import java.io.File;
 public interface PostProcessor
 {
     /**
-     * @param pFile
-     * @return file
+     * Determines if this file should be postprocessed in current postprocessor
+     * @param pFile file for postprocessing
+     * @return true if postprocessing should be done, false sonst 
      */
     public boolean acceptFile(File pFile);
 
     /**
      * Postprocess the source
      * @param pSource the Source for postprocessing
+     * @param pPreviousData the Source of existing file, may be null or empty
      * @return postprocessed source
      * @throws Exception on errors occurred
      */
-    public String postProcess(String pSource) throws Exception;
+    public String postProcess(String pSource, String pPreviousData) throws Exception;
 }
