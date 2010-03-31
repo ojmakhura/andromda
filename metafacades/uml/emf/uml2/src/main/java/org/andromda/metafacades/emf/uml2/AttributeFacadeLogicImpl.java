b9732f46742fe1116704ebaa20d85e84a7d40ad0
cissou <cissou@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2010-03-31 16:57:57 +0000

    Fixed the formatting.

--- metafacades/uml/emf/uml2/src/main/java/org/andromda/metafacades/emf/uml2/AttributeFacadeLogicImpl.java
+++ metafacades/uml/emf/uml2/src/main/java/org/andromda/metafacades/emf/uml2/AttributeFacadeLogicImpl.java
@@ -455,8 +455,9 @@ public class AttributeFacadeLogicImpl
     /**
      * @see org.andromda.metafacades.uml.AttributeFacade#isDerived()
      */
-	@Override
-	protected boolean handleIsDerived() {
-		return this.metaObject.isDerived();
-	}
+    @Override
+    protected boolean handleIsDerived()
+    {
+        return this.metaObject.isDerived();
+    }
 }
