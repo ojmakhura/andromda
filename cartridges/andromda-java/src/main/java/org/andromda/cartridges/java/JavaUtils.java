52efead5304b0a5726a88a26502f6470d478bd30
bobfields <bobfields@529e9a02-9b4b-0410-a312-f78cf1ad2bf5>
2011-05-27 18:26:23 +0000

    Comment out Comparable.compareTo @Override - doesn't work under JDK5.

--- cartridges/andromda-java/src/main/java/org/andromda/cartridges/java/JavaUtils.java
+++ cartridges/andromda-java/src/main/java/org/andromda/cartridges/java/JavaUtils.java
@@ -53,4 +53,41 @@ public class JavaUtils
     {
         return df.format(new Date());
     }
+
+    /**
+     * Returns the current JDK version.
+     *
+     * @return the current JDK version (1.4, 1.5, 1.6, 1.7 etc).
+     */
+    public static String getJDKVersion()
+    {
+        // Default JDK version = 1.6
+        String version = "1.6";
+        final String classVersion = System.getProperty("java.class.version","44.0");
+        if ("50.0".compareTo(classVersion) > 0 && "49.0".compareTo(classVersion) <= 0)
+        {
+            version = "1.5";
+        }
+        else if ("52.0".compareTo(classVersion) > 0 && "51.0".compareTo(classVersion) <= 0)
+        {
+            version = "1.7";
+        }
+        else if ("49.0".compareTo(classVersion) > 0 && "48.0".compareTo(classVersion) <= 0)
+        {
+            version = "1.4";
+        }
+        else if ("48.0".compareTo(classVersion) > 0 && "47.0".compareTo(classVersion) <= 0)
+        {
+            version = "1.3";
+        }
+        else if ("47.0".compareTo(classVersion) > 0 && "46.0".compareTo(classVersion) <= 0)
+        {
+            version = "1.2";
+        }
+        else if ("46.0".compareTo(classVersion) > 0 && "45.0".compareTo(classVersion) <= 0)
+        {
+            version = "1.2";
+        }
+        return version;
+    }
 }
