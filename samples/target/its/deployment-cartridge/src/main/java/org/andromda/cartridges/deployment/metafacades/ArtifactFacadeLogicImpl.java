fb9b6acc7eda1e52407e6f9e3785751b746bc694
Onalenna Junior Makhura <ojmakhura@systemsjr.com>
2018-11-29 18:34:37 +0200

    Modified some vsl files

---
+++ samples/target/its/deployment-cartridge/src/main/java/org/andromda/cartridges/deployment/metafacades/ArtifactFacadeLogicImpl.java
@@ -0,0 +1,82 @@
+package org.andromda.cartridges.deployment.metafacades;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import org.andromda.cartridges.deployment.profile.DeploymentProfile;
+import org.andromda.cartridges.deployment.psm.ant.JarTaskCall;
+import org.andromda.metafacades.uml.DependencyFacade;
+import org.omg.uml.foundation.core.Artifact;
+
+/**
+ * MetafacadeLogic implementation for
+ * org.andromda.cartridges.deployment.metafacades.ArtifactFacade.
+ *
+ * @see org.andromda.cartridges.deployment.metafacades.ArtifactFacade
+ */
+public class ArtifactFacadeLogicImpl extends ArtifactFacadeLogic
+{
+    public ArtifactFacadeLogicImpl(
+            Artifact metaObject, String context)
+    {
+        super(metaObject, context);
+    }
+
+    /**
+     * @see org.andromda.cartridges.deployment.metafacades.ArtifactFacade#getWrappedPackages()
+     */
+    protected Collection handleGetWrappedPackages()
+    {
+        ArrayList result = new ArrayList();
+
+        Collection<DependencyFacade> dependencies = getSourceDependencies();
+        for (DependencyFacade element : dependencies)
+        {
+            if (element.hasStereotype(DeploymentProfile.STEREOTYPE_WRAPS))
+            {
+                result.add(element.getTargetElement());
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     *
+     * @see org.andromda.cartridges.deployment.metafacades.ArtifactFacadeLogic#handleGetTasksForCompilationPhase()
+     */
+    protected Collection handleGetTasksForCompilationPhase()
+    {
+        ArrayList result = new ArrayList();
+
+        Collection<DeployedPackageFacade> wrappedPackages = getWrappedPackages();
+        for (DeployedPackageFacade element : wrappedPackages)
+        {
+            result.addAll(element.getTasksForCompilationPhase());
+        }
+
+        return result;
+    }
+
+    /**
+     *
+     * @see org.andromda.cartridges.deployment.metafacades.ArtifactFacadeLogic#handleGetTasksForDeploymentPhase()
+     */
+    protected Collection handleGetTasksForDeploymentPhase()
+    {
+        ArrayList<String> packages = new ArrayList<String>();
+
+        // find all packages which deploy in this artifact
+        Collection<DeployedPackageFacade> wrappedPackages = getWrappedPackages();
+        for (DeployedPackageFacade element : wrappedPackages)
+        {
+            packages.add(element.getFullyQualifiedName());
+        }
+
+        // jar all packages into one jar
+        JarTaskCall taskCall = new JarTaskCall(getName(), packages, "jar");
+        ArrayList result = new ArrayList(1);
+        result.add(taskCall);
+        return result;
+    }
+}
---
+++ samples/target/its/deployment-cartridge/src/main/java/org/andromda/cartridges/deployment/metafacades/NodeFacadeLogicImpl.java
@@ -0,0 +1,57 @@
+package org.andromda.cartridges.deployment.metafacades;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import org.andromda.cartridges.deployment.psm.ant.Project;
+import org.andromda.cartridges.deployment.psm.ant.Target;
+import org.omg.uml.foundation.core.Node;
+
+/**
+ * MetafacadeLogic implementation for
+ * org.andromda.cartridges.deployment.metafacades.NodeFacade.
+ *
+ * @see org.andromda.cartridges.deployment.metafacades.NodeFacade
+ */
+public class NodeFacadeLogicImpl extends NodeFacadeLogic
+{
+    public NodeFacadeLogicImpl(Node metaObject,
+            String context)
+    {
+        super(metaObject, context);
+    }
+
+    /**
+     * @see org.andromda.cartridges.deployment.metafacades.NodeFacade#getDeployedComponents()
+     */
+    protected Collection handleGetDeployedComponents()
+    {
+        return this.metaObject.getDeployedComponent();
+    }
+
+    /**
+     * @see org.andromda.cartridges.deployment.metafacades.NodeFacadeLogic#handleTransformToAntProject()
+     */
+    protected Project handleTransformToAntProject()
+    {
+        ArrayList compilationTasks = new ArrayList();
+        ArrayList deploymentTasks = new ArrayList();
+        for (Iterator iter = getDeployedComponents().iterator(); iter.hasNext();)
+        {
+            ComponentFacade element = (ComponentFacade) iter.next();
+            compilationTasks.addAll(element.getTasksForCompilationPhase());
+            deploymentTasks.addAll(element.getTasksForDeploymentPhase());
+        }
+
+        Target compilationTarget = new Target("compile", null, compilationTasks);
+        Target deploymentTarget = new Target("deploy", "compile",
+                deploymentTasks);
+
+        ArrayList allTargets = new ArrayList(2);
+        allTargets.add(compilationTarget);
+        allTargets.add(deploymentTarget);
+
+        Project p = new Project(getFullyQualifiedName(), "deploy", allTargets);
+        return p;
+    }
+}
