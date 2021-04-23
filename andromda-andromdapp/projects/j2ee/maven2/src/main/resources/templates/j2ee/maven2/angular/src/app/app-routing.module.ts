a7454a6b9b3dadbac647bb896cd66d681c326314
Onalenna Junior Makhura <ojmakhura@roguesystems.co.bw>
2021-04-24 00:40:14 +0200

    Rewire views

--- andromda-andromdapp/projects/j2ee/maven2/src/main/resources/templates/j2ee/maven2/angular/src/app/app-routing.module.ts
+++ andromda-andromdapp/projects/j2ee/maven2/src/main/resources/templates/j2ee/maven2/angular/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Shell } from '@app/shell/shell.service';
 const routes: Routes = [
   Shell.childRoutes([{ path: 'about', loadChildren: () => import('./about/about.module').then((m) => m.AboutModule) }]),
   // Fallback when no prior route is matched
-  { path: '**', redirectTo: '', pathMatch: 'full' },
+  //{ path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
