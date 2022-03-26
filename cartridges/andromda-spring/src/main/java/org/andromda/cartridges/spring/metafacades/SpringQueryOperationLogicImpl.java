0366eaec472359ad03ef98f6d7df2b7eeef1e5e7
Onalenna Junior Makhura <ojmakhura@gmail.com>
2022-03-26 12:30:15 +0200

    Catered for query methods

--- cartridges/andromda-spring/src/main/java/org/andromda/cartridges/spring/metafacades/SpringQueryOperationLogicImpl.java
+++ cartridges/andromda-spring/src/main/java/org/andromda/cartridges/spring/metafacades/SpringQueryOperationLogicImpl.java
@@ -114,165 +114,165 @@ public class SpringQueryOperationLogicImpl
     {
         // first see if we can retrieve the query from the super class as an OCL
         // translation
-        // String queryString = this.getTranslatedQuery();
+        String queryString = this.getTranslatedQuery();
         StringBuilder builder = new StringBuilder();
 
         // otherwise see if there is a query stored as a tagged value
-        if (StringUtils.isEmpty(this.getTranslatedQuery())) {
+        if (StringUtils.isEmpty(queryString)) {
             Object value = this.findTaggedValue(CartridgeHibernateProfile.TAGGEDVALUE_HIBERNATE_QUERY);
             if (value != null) {
                 // remove any excess whitespace
                 builder.append('\"' + ((String) value).replaceAll("[$\\s]+", " ") + '\"');
             }
         } else {
-            builder.append('\"' + this.getTranslatedQuery() + '\"');
+            builder.append('\"' + queryString + '\"');
         }
 
         // if there wasn't any stored query, create one by default.
-        if (builder.length() == 0) {
+        // if (builder.length() == 0) {
 
-            String variableName = StringUtils.uncapitalize(this.getOwner().getName()).substring(0, 1);
-            builder.append("\"SELECT " + variableName + " FROM " + this.getOwner().getName() + " AS " + variableName);
+        //     String variableName = StringUtils.uncapitalize(this.getOwner().getName()).substring(0, 1);
+        //     builder.append("\"SELECT " + variableName + " FROM " + this.getOwner().getName() + " AS " + variableName);
 
-            Collection arguments = this.getArguments();
+        //     Collection arguments = this.getArguments();
 
-            if (arguments != null && !arguments.isEmpty()) {
-                // Check if there is need for inner and add them
-                Iterator argumentIt = arguments.iterator();
+        //     if (arguments != null && !arguments.isEmpty()) {
+        //         // Check if there is need for inner and add them
+        //         Iterator argumentIt = arguments.iterator();
 
-                builder.append(" \" +\n\t\t\t\t\"WHERE");
-                for (; argumentIt.hasNext();) {
-                    ParameterFacade argument = (ParameterFacade) argumentIt.next();
+        //         builder.append(" \" +\n\t\t\t\t\"WHERE");
+        //         for (; argumentIt.hasNext();) {
+        //             ParameterFacade argument = (ParameterFacade) argumentIt.next();
 
-                    if (CollectionUtils.isEmpty(argument.getType().getAttributes())) {
+        //             if (CollectionUtils.isEmpty(argument.getType().getAttributes())) {
 
-                        String parameter = "?";
-                        if (this.isUseNamedParameters()) {
-                            parameter = ':' + argument.getName();
-                        }
-                        builder.append(' ' + variableName + '.' + argument.getName() + " = " + parameter + " \" ");
-                    } else if (argument.getType().getStereotypeNames().toString().contains("Criteria")) { 
+        //                 String parameter = "?";
+        //                 if (this.isUseNamedParameters()) {
+        //                     parameter = ':' + argument.getName();
+        //                 }
+        //                 builder.append(' ' + variableName + '.' + argument.getName() + " = " + parameter + " \" ");
+        //             } else if (argument.getType().getStereotypeNames().toString().contains("Criteria")) { 
                         
-                        Iterator<AttributeFacade> paramIt = argument.getType().getAttributes().iterator();
-                        for (; paramIt.hasNext();) {
-                            AttributeFacade attribute = paramIt.next();
+        //                 Iterator<AttributeFacade> paramIt = argument.getType().getAttributes().iterator();
+        //                 for (; paramIt.hasNext();) {
+        //                     AttributeFacade attribute = paramIt.next();
 
-                            // Do not add complex objects from the WHERE part of the query
-                            // if(!CollectionUtils.isEmpty(attribute.getType().getAttributes())) {
-                            // continue;
-                            // }
-                            boolean insentive = false;
+        //                     // Do not add complex objects from the WHERE part of the query
+        //                     // if(!CollectionUtils.isEmpty(attribute.getType().getAttributes())) {
+        //                     // continue;
+        //                     // }
+        //                     boolean insentive = false;
 
-                            if (attribute instanceof SpringCriteriaAttribute) {
-                                SpringCriteriaAttribute criteriaAttribute = (SpringCriteriaAttribute) attribute;
-                                String columnName = variableName;
+        //                     if (attribute instanceof SpringCriteriaAttribute) {
+        //                         SpringCriteriaAttribute criteriaAttribute = (SpringCriteriaAttribute) attribute;
+        //                         String columnName = variableName;
 
-                                if (StringUtils.isBlank(criteriaAttribute.getAttributeName())) {
-                                    columnName = columnName + '.' + criteriaAttribute.getName();
-                                } else {
-                                    columnName = columnName + '.' + criteriaAttribute.getAttributeName();
-                                }
+        //                         if (StringUtils.isBlank(criteriaAttribute.getAttributeName())) {
+        //                             columnName = columnName + '.' + criteriaAttribute.getName();
+        //                         } else {
+        //                             columnName = columnName + '.' + criteriaAttribute.getAttributeName();
+        //                         }
 
-                                if (criteriaAttribute.isComparatorPresent() && criteriaAttribute.getComparator()
-                                    .equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_INSENSITIVE_LIKE_COMPARATOR)) {
+        //                         if (criteriaAttribute.isComparatorPresent() && criteriaAttribute.getComparator()
+        //                             .equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_INSENSITIVE_LIKE_COMPARATOR)) {
 
-                                    insentive = true;
-                                }
+        //                             insentive = true;
+        //                         }
                                 
-                                builder.append(" (:");
-                                builder.append(criteriaAttribute.getName());
-                                builder.append(" IS NULL OR ");
-                                builder.append(insentive ? "lower(" + columnName + ")" : columnName);
+        //                         builder.append(" (:");
+        //                         builder.append(criteriaAttribute.getName());
+        //                         builder.append(" IS NULL OR ");
+        //                         builder.append(insentive ? "lower(" + columnName + ")" : columnName);
 
-                                String comparator = " = ";
-                                if (criteriaAttribute.isComparatorPresent()) {
+        //                         String comparator = " = ";
+        //                         if (criteriaAttribute.isComparatorPresent()) {
 
-                                    String tmp = criteriaAttribute.getComparator();
+        //                             String tmp = criteriaAttribute.getComparator();
 
-                                    if (tmp.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_COMPARATOR_GREATER)) {
-                                        comparator = " > ";
-                                    } else if (tmp.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_COMPARATOR_GREATER_OR_EQUAL)) {
-                                        comparator = " >= ";
-                                    } else if (tmp.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_COMPARATOR_IN)) {
-                                        comparator = " IN ";
-                                    } else if (tmp
-                                            .equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_COMPARATOR_LESS)) {
-                                        comparator = " < ";
-                                    } else if (tmp.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_COMPARATOR_LESS_OR_EQUAL)) {
-                                        comparator = " <= ";
-                                    } else if (tmp.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_COMPARATOR_LIKE) || 
-                                            tmp.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_INSENSITIVE_LIKE_COMPARATOR)) {
-                                        comparator = " LIKE ";
-                                    }
-                                }
-                                builder.append(comparator);
-                                String q = ":" + criteriaAttribute.getName();
+        //                             if (tmp.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_COMPARATOR_GREATER)) {
+        //                                 comparator = " > ";
+        //                             } else if (tmp.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_COMPARATOR_GREATER_OR_EQUAL)) {
+        //                                 comparator = " >= ";
+        //                             } else if (tmp.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_COMPARATOR_IN)) {
+        //                                 comparator = " IN ";
+        //                             } else if (tmp
+        //                                     .equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_COMPARATOR_LESS)) {
+        //                                 comparator = " < ";
+        //                             } else if (tmp.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_COMPARATOR_LESS_OR_EQUAL)) {
+        //                                 comparator = " <= ";
+        //                             } else if (tmp.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_COMPARATOR_LIKE) || 
+        //                                     tmp.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_INSENSITIVE_LIKE_COMPARATOR)) {
+        //                                 comparator = " LIKE ";
+        //                             }
+        //                         }
+        //                         builder.append(comparator);
+        //                         String q = ":" + criteriaAttribute.getName();
 
-                                if (criteriaAttribute.isMatchModePresent()) {
+        //                         if (criteriaAttribute.isMatchModePresent()) {
                                     
-                                    String mode = criteriaAttribute.getMatchMode();
+        //                             String mode = criteriaAttribute.getMatchMode();
 
-                                    if (mode.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_MATCHMODE_END)) {
+        //                             if (mode.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_MATCHMODE_END)) {
                                                 
-                                        q = "CONCAT('%', " + q + ')';
+        //                                 q = "CONCAT('%', " + q + ')';
 
-                                    } else if (mode.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_MATCHMODE_ANYWHERE)) {
+        //                             } else if (mode.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_MATCHMODE_ANYWHERE)) {
                                                 
-                                        q = "CONCAT('%', " + q + ", '%')";
-                                    } else if(mode.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_MATCHMODE_START)){
+        //                                 q = "CONCAT('%', " + q + ", '%')";
+        //                             } else if(mode.equals(CartridgeHibernateProfile.TAGGEDVALUEVALUE_MATCHMODE_START)){
                                                 
-                                        q = "CONCAT(" + q + ", '%')";
-                                    }
-                                }
+        //                                 q = "CONCAT(" + q + ", '%')";
+        //                             }
+        //                         }
 
-                                if(insentive) {
-                                    q = "lower(" + q + ")";
-                                }
+        //                         if(insentive) {
+        //                             q = "lower(" + q + ")";
+        //                         }
 
-                                builder.append(q);
+        //                         builder.append(q);
 
-                                builder.append(") \"");
+        //                         builder.append(") \"");
 
-                            } else {
-                                String parameter = "?";
-                                if (this.isUseNamedParameters()) {
-                                    parameter = ':' + attribute.getName();
-                                }
+        //                     } else {
+        //                         String parameter = "?";
+        //                         if (this.isUseNamedParameters()) {
+        //                             parameter = ':' + attribute.getName();
+        //                         }
 
-                                if(attribute.getType().isStringType()) {
-                                    builder.append(" lower(" + variableName + '.' + attribute.getName() + ") = lower(" + parameter + ") \"");
-                                } else {
-                                    builder.append(' ' + variableName + '.' + attribute.getName() + " = " + parameter + " \"");
-                                }
-                            }
+        //                         if(attribute.getType().isStringType()) {
+        //                             builder.append(" lower(" + variableName + '.' + attribute.getName() + ") = lower(" + parameter + ") \"");
+        //                         } else {
+        //                             builder.append(' ' + variableName + '.' + attribute.getName() + " = " + parameter + " \"");
+        //                         }
+        //                     }
 
-                            if (paramIt.hasNext()) {
-                                builder.append(" +\n\t\t\t\t\"AND");
-                            }
-                        }
-                    } else if (argument.getType().getStereotypeNames().toString().contains("Entity")) { // We are dealing with an entity
+        //                     if (paramIt.hasNext()) {
+        //                         builder.append(" +\n\t\t\t\t\"AND");
+        //                     }
+        //                 }
+        //             } else if (argument.getType().getStereotypeNames().toString().contains("Entity")) { // We are dealing with an entity
 
-                        // We get all the identifiers from the entity
-                        SpringEntity ent = (SpringEntity) argument.getType();
-                        Iterator<ModelElementFacade> it = ent.getIdentifiers().iterator();
+        //                 // We get all the identifiers from the entity
+        //                 SpringEntity ent = (SpringEntity) argument.getType();
+        //                 Iterator<ModelElementFacade> it = ent.getIdentifiers().iterator();
 
-                        while(it.hasNext()) {
-                            ModelElementFacade element = it.next();
-                            builder.append(' ' + variableName + '.' + argument.getName() + '.' + element.getName() + " = :" + argument.getName() + StringUtils.capitalize(element.getName()) + " \"");
+        //                 while(it.hasNext()) {
+        //                     ModelElementFacade element = it.next();
+        //                     builder.append(' ' + variableName + '.' + argument.getName() + '.' + element.getName() + " = :" + argument.getName() + StringUtils.capitalize(element.getName()) + " \"");
 
-                            if (it.hasNext()) {
-                                builder.append("+\n\t\t\t\t\"AND");
-                            }
+        //                     if (it.hasNext()) {
+        //                         builder.append("+\n\t\t\t\t\"AND");
+        //                     }
 
-                        }
-                    }
+        //                 }
+        //             }
 
-                    if (argumentIt.hasNext()) {
-                        builder.append("+\n\t\t\t\t\"AND");
-                    }
-                }
-            }
-        }
+        //             if (argumentIt.hasNext()) {
+        //                 builder.append("+\n\t\t\t\t\"AND");
+        //             }
+        //         }
+        //     }
+        // }
 
         return builder.toString();
     }
