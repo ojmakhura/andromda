452737c6ed03c09f97dfc98042d1c2b7b3f4cf56
Onalenna J. Makhura <ojmakhura@roguesystems.co.bw>
2024-09-26 22:42:05 +0200

    table rendering

--- andromda-andromdapp/projects/j2ee/maven2/src/main/resources/templates/j2ee/maven2/angular/src/app/app.config.ts
+++ andromda-andromdapp/projects/j2ee/maven2/src/main/resources/templates/j2ee/maven2/angular/src/app/app.config.ts
@@ -1,39 +1,40 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, isDevMode, importProvidersFrom } from '@angular/core';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreDevtoolsModule, provideStoreDevtools } from '@ngrx/store-devtools';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '@env/environment';
-import { EffectsModule } from '@ngrx/effects';
 import { UseCaseScope } from './utils/use-case-scope';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { withInterceptors, provideHttpClient } from '@angular/common/http';
 import { RouteReusableStrategy } from './@shared';
-import { apiPrefixInterceptor, errorHandlerInterceptor } from './@core';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
+import { errorHandlerInterceptor } from './@core/http/error-handler.interceptor';
+import { apiPrefixInterceptor } from './@core/http/api-prefix.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     UseCaseScope,
     provideRouter(routes),
-    provideAnimations(),
-    provideHttpClient(withInterceptors([
+    provideAnimations(),provideHttpClient(withInterceptors([
       apiPrefixInterceptor,
       errorHandlerInterceptor,
     ])),
+    provideHttpClient(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
     importProvidersFrom(
-      TranslateModule.forRoot(),
-      StoreModule.forRoot({}),
       StoreDevtoolsModule.instrument({}),
       TranslateModule.forRoot(),
-      EffectsModule.forRoot([]),
       ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }),
     ),
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}},
     {
       provide: RouteReuseStrategy,
       useClass: RouteReusableStrategy,
     },
   ],
 };
+
+
