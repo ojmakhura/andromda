c975261ff5821d5d2cf6948dc14a355128d791df
Onalenna J. Makhura <ojmakhura@roguesystems.co.bw>
2024-12-20 04:36:11 +0200

    FIxed conflicts ins renderAngularActionFormInput.vm

--- andromda-andromdapp/projects/j2ee/maven2/src/main/resources/templates/j2ee/maven2/angular/src/app/model/column.model.ts
+++ andromda-andromdapp/projects/j2ee/maven2/src/main/resources/templates/j2ee/maven2/angular/src/app/model/column.model.ts
@@ -1,12 +1,13 @@
 export class ColumnModel {
-    id: string;
-    label: string;
-    actionId?: string;
-    link: boolean = false;
-    
-    constructor(id: string, label: string, link: boolean | false) {
-        this.id = id;
-        this.label = label;
-        this.link = link;
-    }
+  id: string;
+  label: string;
+  actionId?: string;
+  link: boolean = false;
+
+  constructor(id: string, label: string, link: boolean | false, actionId?: string) {
+      this.id = id;
+      this.label = label;
+      this.link = link;
+      this.actionId = actionId;
+  }
 }
