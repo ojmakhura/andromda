4f71ef53de32ffd3ee653b31be1e3e9ca8b0ad4c
Onalenna Junior Makhura <omakhura@bitri.co.bw>
2025-12-12 08:10:35 +0200

    package update for angular

---
+++ andromda-andromdapp/projects/j2ee/maven2/src/main/resources/templates/j2ee/maven2/angular/src/app/auth/authentication.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { Logger } from '@app/@shared';
+
+const log = new Logger('AuthenticationGuard');
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate { 
+
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    return true;
+  }
+
+}
