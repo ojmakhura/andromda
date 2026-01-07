f0bb723714cea94e88a9834442526ee85a2afe11
Onalenna Junior Makhura <omakhura@bitri.co.bw>
2026-01-07 17:40:33 +0200

    added fancy table component

---
+++ andromda-andromdapp/projects/j2ee/maven2/src/main/resources/templates/j2ee/maven2/angular/src/app/components/fancy-table/fancy-table.component.ts
@@ -0,0 +1,150 @@
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  effect,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  signal,
+  Signal,
+  ViewChild,
+} from '@angular/core';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatRadioChange, MatRadioModule } from '@angular/material/radio';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '@app/@shared/shared.module';
+import { MaterialModule } from '@app/material.module';
+import { ActionTemplate } from '@app/model/action-template';
+import { ColumnModel } from '@app/model/column.model';
+import { IdLabel } from '@app/model/id-label.model';
+import { Page } from '@app/model/page.model';
+import { SelectionType } from '@app/model/selection-type.model';
+import { DeepSignal } from '@ngrx/signals';
+import { TranslateModule } from '@ngx-translate/core';
+
+interface HasId {
+  id?: string | number;
+  [key: string]: any;
+}
+
+@Component({
+  selector: 'app-fancy-table',
+  templateUrl: './fancy-table.component.html',
+  styleUrls: ['./fancy-table.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    TranslateModule,
+    SharedModule,
+    MaterialModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatPaginatorModule,
+    MatTooltipModule,
+    MatButtonModule,
+  ],
+})
+export class FancyTableComponent<T extends HasId> implements OnInit, OnDestroy, AfterViewInit, AfterViewChecked {
+  @Input() paged: boolean = false;
+  @Input() showActions: boolean = true;
+  @Input() actions: ActionTemplate[] = [];
+  @Input({ required: true }) mainColumns: ColumnModel[] = [];
+  @Input({ required: true }) dataColumns: ColumnModel[] = [];
+  @Input() dataSignal: Signal<T[]> | DeepSignal<Page<T>> | Signal<undefined>;
+  dataItems = signal([] as T[]);
+  @Input() selectionType = SelectionType.NONE;
+
+  @Output() actionClicked: EventEmitter<any> = new EventEmitter<any>();
+  @Output() pageChanged: EventEmitter<any> = new EventEmitter<any>();
+  @Input() selectionFilter: any;
+
+  @ViewChild(MatPaginator) set paginator(paginator: MatPaginator | null) {
+    if (paginator) {
+      paginator.page.subscribe({
+        next: (paginator: MatPaginator) => {
+          console.log('Paginator event:', paginator);
+          this.pageChanged.emit({
+            pageNumber: paginator.pageIndex,
+            pageSize: paginator.pageSize,
+          });
+        },
+      });
+    }
+  }
+
+  totalElements = 0;
+
+  allColumns: string[] = [];
+
+  s = SelectionType;
+  selectedItems: any[] = [];
+
+  constructor() {
+    effect(() => {
+      if (!this.dataSignal) {
+        return;
+      }
+
+      if (!this.paged) {
+        const data: T[] = <T[]>this.dataSignal();
+        this.dataItems.set(data || []);
+        this.totalElements = data?.length || 0;
+      } else {
+        const page = <Page<T>>this.dataSignal();
+        this.dataItems.set(page?.content || []);
+        this.totalElements = page?.totalElements || 0;
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.allColumns = this.dataColumns.map((column) => column.id);
+    if (this.showActions) {
+      this.allColumns.push('actions');
+    }
+
+    if (this.selectionType !== SelectionType.NONE) {
+      this.allColumns.unshift('selection');
+    }
+  }
+
+  ngAfterViewChecked(): void {}
+
+  ngOnDestroy(): void {}
+
+  ngAfterViewInit() {
+    console.log('Paginator:', this.paginator);
+  }
+
+  onActionClicked(action: string, id: string | number | undefined) {
+    if (this.actionClicked) {
+      this.actionClicked.emit({ action, id });
+    }
+  }
+
+  radioSelected($event: MatRadioChange, row: any) {
+    if ($event.value) {
+      this.selectedItems = [row];
+    }
+  }
+
+  checkboxSelected($event: MatCheckboxChange, row: any) {
+    if ($event.checked) {
+      this.selectedItems.push(row);
+    } else {
+      this.selectedItems = this.selectedItems.filter((item) => {
+        let r = this.selectionFilter ? this.selectionFilter(item, row) : item.id !== row.id;
+        return !r;
+      });
+    }
+  }
+}
